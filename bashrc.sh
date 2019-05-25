1ec05fe8411e4775e86c3ad71f737ff5a278cca0
Fredrik Averpil <fredrik@averpil.com>
2019-05-25 10:01:12 +0200

    Add activate alias

--- bashrc.sh
+++ bashrc.sh
@@ -2,7 +2,8 @@
 alias ll='ls -alhF'
 alias tree='tree -C'
 alias wrk='docker run --rm skandyla/wrk'
-alias venv='echo "venv" >> .gitignore && python3 -m venv venv && source venv/bin/activate && pip install -U pip pylint black pep8 pydocstyle && pip list && python --version'
+alias venv='echo "venv" >> .gitignore && python3 -m venv --copies venv && source venv/bin/activate && pip install -U pip pylint black pep8 pydocstyle && pip list && python --version'
+alias activate='source venv/bin/activate'
 
 # Environment variables
 # export PATH="$PATH:~/miniconda3/bin"
