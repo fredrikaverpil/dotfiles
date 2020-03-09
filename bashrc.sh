69709e74315e5b84b74ced6a400d038a266a9f81
Fredrik Averpil <fredrik@averpil.com>
2020-03-09 08:56:36 +0100

    Move envs to os-specific area

--- bashrc.sh
+++ bashrc.sh
@@ -5,18 +5,19 @@ alias wrk='docker run --rm skandyla/wrk'
 alias venv='echo "venv" >> .gitignore && python3 -m venv --copies venv && source venv/bin/activate && pip install -U pip pylint black pep8 pydocstyle && pip list && python --version'
 alias activate='source venv/bin/activate'
 
-# Environment variables
-# export PATH="$PATH:~/miniconda3/bin"
-export HOMEBREW_NO_ANALYTICS=1
-export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-export PATH="$PATH:$HOME/.cargo/bin"  # Rust
-
 # Per-platform specifics
 if [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
     # Assume Bash
 
   if [ "$(uname -s)" == "Darwin" ]; then
     # Assume macOS
+
+    # Environment variables
+    # export PATH="$PATH:~/miniconda3/bin"
+    export HOMEBREW_NO_ANALYTICS=1
+    export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+    export PATH="$PATH:$HOME/.cargo/bin"  # Rust
+
 
     # Enable colors
     export CLICOLOR=1
