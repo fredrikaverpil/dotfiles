2fe658573284dde40eb2767065894569e096d394
Fredrik Averpil <fredrik@averpil.com>
2020-03-15 22:31:36 +0100

    Use temporary python path

--- bash_exports.sh
+++ bash_exports.sh
@@ -3,6 +3,7 @@ case `uname` in
     Darwin)
         # export PATH="$PATH:~/miniconda3/bin"
         export HOMEBREW_NO_ANALYTICS=1
+        export PATH="/usr/local/opt/python@3.8/bin:$PATH"  # temporary until 'brew install python3' installs latest version
         export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
         export PATH="$PATH:$HOME/.cargo/bin"  # Rust
         export CLICOLOR=1  # Enable colors
