03b429aa2bdf33c2271fb861dcd57ec78dbc9867
Fredrik Averpil <fredrik@averpil.com>
2020-03-25 08:48:25 +0100

    Update aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -3,9 +3,14 @@ case `uname` in
     Darwin)
         alias ll='ls -alhF'
         alias tree='tree -C'
+        alias gg='git grep'
+        alias rebase='git pull origin master'
         alias wrk='docker run --rm skandyla/wrk'
-        alias venv='echo "venv" >> .gitignore && python3 -m venv venv && source venv/bin/activate && pip install -U pip pylint black pep8 pydocstyle && pip list && python --version'
+
+        alias venv='echo "venv" >> .gitignore && python3 -m venv venv && source venv/bin/activate && pip install -U pip black flake8 pydocstyle && pip list && python --version'
         alias activate='source venv/bin/activate'
+        alias pyclean='find . -name "*.py[co]" -o -name __pycache__ -exec rm -rf {}'
+        alias pip-purge='pip freeze | xargs pip uninstall -y'
 
     ;;
     Linux)
