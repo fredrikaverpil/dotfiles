e6ca79f15639a01f45ba7cb8b5850c3666cbfc4a
Fredrik Averpil <fredrik@averpil.com>
2020-03-25 08:49:38 +0100

    Update aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,17 +1,7 @@
 # Per-platform settings
 case `uname` in
     Darwin)
-        alias ll='ls -alhF'
-        alias tree='tree -C'
-        alias gg='git grep'
-        alias rebase='git pull origin master'
-        alias wrk='docker run --rm skandyla/wrk'
-
-        alias venv='echo "venv" >> .gitignore && python3 -m venv venv && source venv/bin/activate && pip install -U pip black flake8 pydocstyle && pip list && python --version'
-        alias activate='source venv/bin/activate'
-        alias pyclean='find . -name "*.py[co]" -o -name __pycache__ -exec rm -rf {}'
-        alias pip-purge='pip freeze | xargs pip uninstall -y'
-
+        # commands for macOS go here
     ;;
     Linux)
         # commands for Linux go here
@@ -20,3 +10,16 @@ case `uname` in
         # commands for FreeBSD go here
     ;;
 esac
+
+
+# Global settings
+alias ll='ls -alhF'
+alias tree='tree -C'
+alias gg='git grep'
+alias rebase='git pull origin master'
+alias wrk='docker run --rm skandyla/wrk'
+
+alias venv='echo "venv" >> .gitignore && python3 -m venv venv && source venv/bin/activate && pip install -U pip black flake8 pydocstyle && pip list && python --version'
+alias activate='source venv/bin/activate'
+alias pyclean='find . -name "*.py[co]" -o -name __pycache__ -exec rm -rf {}'
+alias pip-purge='pip freeze | xargs pip uninstall -y'
