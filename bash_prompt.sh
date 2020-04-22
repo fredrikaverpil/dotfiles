79048b675e7d0defba518c53ae32445d7bbe1bd7
Fredrik Averpil <fredrik@averpil.com>
2020-04-22 17:17:15 +0200

    Re-enable calc

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -40,7 +40,7 @@ function timer_start {
 }
 
 function timer_stop {
-    # local delta_us=$((($(timer_now) - $timer_start) / 1000))
+    local delta_us=$((($(timer_now) - $timer_start) / 1000))
     local us=$((delta_us % 1000))
     local ms=$(((delta_us / 1000) % 1000))
     local s=$(((delta_us / 1000000) % 60))
