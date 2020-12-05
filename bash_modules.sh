5f6105761f2d2a7432767c89fda5e79fbd339fc1
Fredrik Averpil <fredrik@averpil.com>
2020-12-05 22:50:41 +0100

    Update modules

--- bash_modules.sh
+++ bash_modules.sh
@@ -8,7 +8,10 @@ case `uname` in
         if [ -f /etc/redhat-release ]; then
             # module avail [app]
             module load git
+            module load python/3.6.6
             module load python/3.7.4
+            module load python/3.8.5
+            module load python/3.9.0
             module load chrome/83.0.4103.116-1
             # module list
         fi
