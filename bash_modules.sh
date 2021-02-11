1aa42ad75918aca213f27694d3480e395cd920a4
Fredrik Averpil <fredrik@averpil.com>
2021-02-11 13:34:22 +0100

    Comment out modules (#18)

--- bash_modules.sh
+++ bash_modules.sh
@@ -9,13 +9,13 @@ case `uname` in
             # https://modules.readthedocs.io/en/latest/index.html
             # module avail [app]
             # module load git
-            module load $(module avail git --latest | sed -nre 's/^git\/[^0-9]*(([0-9]+\.)*[0-9]+).*/\0/p')
+            # module load $(module avail git --latest | sed -nre 's/^git\/[^0-9]*(([0-9]+\.)*[0-9]+).*/\0/p')
             module load python/3.6.6
             module load python/3.7.4
             module load python/3.8.5
             module load python/3.9.0
             # module load chrome/83.0.4103.116-1
-            module load $(module avail chrome --latest | sed -nre 's/^chrome\/[^0-9]*(([0-9]+\.)*[0-9]+).*/\0/p')
+            # module load $(module avail chrome --latest | sed -nre 's/^chrome\/[^0-9]*(([0-9]+\.)*[0-9]+).*/\0/p')
             module load $(module avail firefox --latest | sed -nre 's/^firefox\/[^0-9]*(([0-9]+\.)*[0-9]+).*/\0/p')
             # module list
         fi
