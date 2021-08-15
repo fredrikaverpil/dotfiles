02cf6806aa7266e97e333ff87ad89c60a8509342
Fredrik Averpil <fredrik@averpil.com>
2021-08-15 19:38:36 +0200

    Fix syntax error

--- bashrc.sh
+++ bashrc.sh
@@ -1,6 +1,6 @@
 
 # Source file if it exists and have a size greater than zero
-[[ -s ~/.bash_exports ]] && source ~/.bash_exports ]]
-[[ -s ~/.bash_aliases ]] && source ~/.bash_aliases ]]
-[[ -s ~/.bash_prompt ]] && source ~/.bash_prompt ]]
-[[ -s ~/.bash_venv ]] && source ~/.bash_venv ]]
+[[ -s ~/.bash_exports ]] && source ~/.bash_exports
+[[ -s ~/.bash_aliases ]] && source ~/.bash_aliases
+[[ -s ~/.bash_prompt ]] && source ~/.bash_prompt
+[[ -s ~/.bash_venv ]] && source ~/.bash_venv
