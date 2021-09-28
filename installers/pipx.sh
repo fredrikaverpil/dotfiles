e162d74e0c1eb6918bda2e2414520caf7f035dc4
Fredrik Averpil <fredrik.averpil@gmail.com>
2021-09-28 10:41:52 +0000

    Simplify

--- installers/pipx.sh
+++ installers/pipx.sh
@@ -6,21 +6,11 @@
 case `uname` in
     Darwin)
         # commands for Linux go here
-        if [ ! -d ~/.pyenv ]; then
-            brew install openssl readline sqlite3 xz zlib
-
-            curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-        fi
     ;;
     Linux)
         # commands for Linux go here
-        echo "kaka"
         if ! command -v pipx &> /dev/null; then
-            echo "jkaak"
-            pyenv global 3.9.7
-            PIP_REQUIRE_VIRTUALENV=false python -m pip install -U pipx
-            pyenv global system
-
+            PIP_REQUIRE_VIRTUALENV=false $HOME/.pyenv/versions/3.9.7/bin/python -m pip install -U pipx
             sudo ln -s $HOME/.pyenv/versions/3.9.7/bin/pipx /usr/bin/pipx
         fi
     ;;
