69642dd897e0aae75518ae5646f88f311e6d4ab7
Fredrik Averpil <fredrik@averpil.com>
2021-09-28 15:03:54 +0000

    Combine entire python setup (#51)

--- installers/python.sh
+++ installers/python.sh
@@ -2,6 +2,7 @@
 
 # https://github.com/pyenv/pyenv/wiki
 # https://github.com/pyenv/pyenv-installer
+# https://github.com/pypa/pipx
 
 # Per-platform settings
 case `uname` in
@@ -15,14 +16,28 @@ case `uname` in
     ;;
     Linux)
         # commands for Linux go here
+
+        # install python via pyenv
         if [ ! -d ~/.pyenv ]; then
             if command -v apt-get &> /dev/null; then
+
+            BASE_PY_VER=`cat .python-version`
+
             sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
                 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
                 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-            # install 
+            # install
             curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+            # install version
+            $HOME/.pyenv/bin/pyenv install $BASE_PY_VER
             fi
+        fi
+
+        # install pipx
+        if ! command -v pipx &> /dev/null; then
+            PIP_REQUIRE_VIRTUALENV=false $HOME/.pyenv/versions/$BASE_PY_VER/bin/python -m pip install -U pipx
+            sudo ln -s $HOME/.pyenv/versions/$BASE_PY_VER/bin/pipx /usr/bin/pipx
         fi
     ;;
     FreeBSD)
