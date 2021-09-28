2174ddc03ab9df00e996e34d41314b29785c0450
Fredrik Averpil <fredrik@averpil.com>
2021-09-28 10:00:27 +0000

    Use capital D (#47)

--- installers/nerdfont.sh
+++ installers/nerdfont.sh
@@ -14,9 +14,9 @@ case `uname` in
         if ! compgen -G "${HOME}/.fonts/Fira*" > /dev/null; then
             if command -v fc-cache &> /dev/null; then
                 # Ubuntu
-                curl --location --output ~/downloads/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-                unzip -o ~/downloads/FiraCode.zip -d ~/.fonts
-                rm ~/downloads/FiraCode.zip
+                curl --location --output ~/Downloads/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+                unzip -o ~/Downloads/FiraCode.zip -d ~/.fonts
+                rm ~/Downloads/FiraCode.zip
                 fc-cache -fv
             fi
         fi
