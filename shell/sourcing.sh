710a19e3715aab60ff1418eb4003ee2e5b27fbd0
Fredrik Averpil <fredrik.averpil@gmail.com>
2021-10-07 08:06:19 +0200

    Update

--- shell/sourcing.sh
+++ shell/sourcing.sh
@@ -9,3 +9,29 @@ if [ -f ~/.linuxbrew/bin/brew ]; then
     # eval "$(/home/fredrik/.linuxbrew/bin/brew shellenv)"
     # eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
+
+
+if [ -d ~/.pyenv ]; then
+
+    eval "$(pyenv init --path)"
+    eval "$(pyenv virtualenv-init -)"
+
+    function cd() {
+    builtin cd "$@"
+
+    if [[ -z "$VIRTUAL_ENV" ]] ; then
+        ## If env folder is found then activate the vitualenv
+        if [[ -d ./.venv ]] ; then
+            source ./.venv/bin/activate
+        fi
+    else
+        ## check the current folder belong to earlier VIRTUAL_ENV folder
+        # if yes then do nothing
+        # else deactivate
+        parentdir="$(dirname "$VIRTUAL_ENV")"
+        if [[ "$PWD"/ != "$parentdir"/* ]] ; then
+            deactivate
+        fi
+    fi
+    }
+fi
