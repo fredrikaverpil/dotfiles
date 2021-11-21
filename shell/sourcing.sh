e744a0593bc12205e116e5fa9d7ee1bfaf431c3f
Fredrik Averpil <fredrik.averpil@gmail.com>
2021-11-21 16:50:53 +0100

    Add nvm sourcing on arm64

--- shell/sourcing.sh
+++ shell/sourcing.sh
@@ -5,8 +5,12 @@ fi
 
 
 # Node version manager
-if [ -d /usr/local/opt/nvm ]; then
-    # brew-installed nvm, macOS
+if [ -d /opt/homebrew/opt/nvm ]; then
+  # brew-installed nvm, macOS arm64
+    [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+    [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+elif [ -d /usr/local/opt/nvm ]; then
+    # brew-installed nvm, macOS x86_64
     [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
     [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 elif [ -d ~/.nvm ]; then
