ebc97c044442007321e0a8486b19c3340e0ef437
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-01-01 17:08:16 +0100

    Revert changes

--- installers/python.sh
+++ installers/python.sh
@@ -11,34 +11,33 @@ Darwin)
 
     base_python_version=$(cat .python-version)
 
-    if [ ! -d "$HOME/.pyenv" ]; then
-        mkdir -p "$HOME/.pyenv"
+    if [ ! -d $HOME/.pyenv ]; then
         curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
         git clone https://github.com/s1341/pyenv-alias.git ~/.pyenv/plugins/pyenv-alias
     fi
 
-    if [ ! -d "$HOME/.pyenv/versions/${base_python_version}" ]; then
+    if [ ! -d $HOME/.pyenv/versions/${base_python_version} ]; then
         brew install openssl readline sqlite3 xz zlib # required to build python
-        ~/.pyenv/bin/pyenv install "$base_python_version"
+        ~/.pyenv/bin/pyenv install $base_python_version
     fi
 
-    if [ ! -d "$(brew --prefix)/bin/pipx" ]; then
+    if [ ! -d $(brew --prefix)/bin/pipx ]; then
         brew install pipx
     fi
 
     # arm64
-    if [ ! -f "$HOME/.local/bin/ipython" ]; then "$(brew --prefix)/bin/pipx" install ipython --pip-args rich; fi
-    if [ ! -f "$HOME/.local/bin/ipython" ]; then "$(brew --prefix)/bin/pipx" install bpython; fi
-    if [ ! -f "$HOME/.local/bin/black" ]; then "$(brew --prefix)/bin/pipx" install black; fi
-    if [ ! -f "$HOME/.local/bin/poetry" ]; then "$(brew --prefix)/bin/pipx" install poetry; fi
-    if [ ! -f "$HOME/.local/bin/pre-commit" ]; then "$(brew --prefix)/bin/pipx" install pre-commit; fi
+    if [ ! -f $HOME/.local/bin/ipython ]; then $(brew --prefix)/bin/pipx install ipython --pip-args rich; fi
+    if [ ! -f $HOME/.local/bin/ipython ]; then $(brew --prefix)/bin/pipx install bpython; fi
+    if [ ! -f $HOME/.local/bin/black ]; then $(brew --prefix)/bin/pipx install black; fi
+    if [ ! -f $HOME/.local/bin/poetry ]; then $(brew --prefix)/bin/pipx install poetry; fi
+    if [ ! -f $HOME/.local/bin/pre-commit ]; then $(brew --prefix)/bin/pipx install pre-commit; fi
 
     # x86_64
-    if [ "$(uname -m)" == "arm64" ] && [ ! -d "$HOME/.pyenv/versions/${base_python_version}_x86" ]; then
+    if [ "$(uname -m)" == "arm64" ] && [ ! -d ~/.pyenv/versions/${base_python_version}_x86 ]; then
         # http://sixty-north.com/blog/pyenv-apple-silicon.html
         brew86 install openssl readline sqlite3 xz zlib # required to build python
         VERSION_ALIAS="${base_python_version}_x86" \
-            pyenv86 install -v "$base_python_version"
+            pyenv86 install -v $base_python_version
 
         brew86 install pipx
 
@@ -66,7 +65,7 @@ Linux)
             curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 
             # install python version
-            "$HOME/.pyenv/bin/pyenv" install "$base_python_version"
+            $HOME/.pyenv/bin/pyenv install $base_python_version
         fi
     fi
 
@@ -78,8 +77,8 @@ Linux)
 
     # install pipx
     if [ ! -f /usr/bin/pipx ]; then
-        PIP_REQUIRE_VIRTUALENV=false "$HOME/.pyenv/versions/$base_python_version/bin/python" -m pip install -U pipx
-        sudo ln -s "$pipx_target_path" /usr/bin/pipx
+        PIP_REQUIRE_VIRTUALENV=false $HOME/.pyenv/versions/$base_python_version/bin/python -m pip install -U pipx
+        sudo ln -s $pipx_target_path /usr/bin/pipx
     fi
 
     # pipx-installations
