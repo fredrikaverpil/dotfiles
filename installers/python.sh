4daba25e0317af53ef0937c5ef4d78dc99ea9f61
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-01-01 18:19:18 +0100

    Revert "Refactor"
    This reverts commit 42151f46fd37f2805ed516075e867d9adeda6a3a.

--- installers/python.sh
+++ installers/python.sh
@@ -4,40 +4,35 @@
 # https://github.com/pyenv/pyenv-installer
 # https://github.com/pypa/pipx
 
-base_python_version=$(cat .python-version)
-
 # Per-platform settings
 case $(uname) in
 Darwin)
     # commands for macOS go here
 
-    # install pyenv
+    base_python_version=$(cat .python-version)
+
     if [ ! -d $HOME/.pyenv ]; then
         curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
         git clone https://github.com/s1341/pyenv-alias.git ~/.pyenv/plugins/pyenv-alias
     fi
 
-    # install python
     if [ ! -d $HOME/.pyenv/versions/${base_python_version} ]; then
         brew install openssl readline sqlite3 xz zlib # required to build python
         ~/.pyenv/bin/pyenv install $base_python_version
     fi
 
-    # install pipx
     if [ ! -d $(brew --prefix)/bin/pipx ]; then
         brew install pipx
     fi
 
-    # install pipx-managed tools
+    # arm64
     if [ ! -f $HOME/.local/bin/ipython ]; then $(brew --prefix)/bin/pipx install ipython --pip-args rich; fi
     if [ ! -f $HOME/.local/bin/ipython ]; then $(brew --prefix)/bin/pipx install bpython; fi
     if [ ! -f $HOME/.local/bin/black ]; then $(brew --prefix)/bin/pipx install black; fi
-    if [ ! -f $HOME/.local/bin/ipython ]; then $(brew --prefix)/bin/pipx install flake8; fi
-    if [ ! -f $HOME/.local/bin/ipython ]; then $(brew --prefix)/bin/pipx install bandit; fi
     if [ ! -f $HOME/.local/bin/poetry ]; then $(brew --prefix)/bin/pipx install poetry; fi
     if [ ! -f $HOME/.local/bin/pre-commit ]; then $(brew --prefix)/bin/pipx install pre-commit; fi
 
-    # install python, pipx and pipx-managed tools for x86_64
+    # x86_64
     if [ "$(uname -m)" == "arm64" ] && [ ! -d ~/.pyenv/versions/${base_python_version}_x86 ]; then
         # http://sixty-north.com/blog/pyenv-apple-silicon.html
         brew86 install openssl readline sqlite3 xz zlib # required to build python
@@ -52,22 +47,26 @@ Darwin)
 Linux)
     # commands for Linux go here
 
+    base_python_version=$(cat .python-version)
     pipx_target_path=$HOME/.pyenv/versions/$base_python_version/bin/pipx
 
-    if [ ! -d $HOME/.pyenv ]; then
-        curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-    fi
-
-    if [ ! -d $HOME/.pyenv/versions/${base_python_version} ]; then
+    # install python via pyenv
+    if [ ! -d ~/.pyenv ]; then
         if command -v apt-get &>/dev/null; then
+
             sudo apt-get install -y gcc
 
             sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
                 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
                 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-        fi
 
-        $HOME/.pyenv/bin/pyenv install $base_python_version
+            # install pyenv
+            echo "Installing pyenv into ${HOME}/.pyenv ..."
+            curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+            # install python version
+            $HOME/.pyenv/bin/pyenv install $base_python_version
+        fi
     fi
 
     # delete symlink and pipx if it is pointing to the wrong python installation
