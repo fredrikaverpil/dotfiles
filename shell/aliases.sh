b1082c9ef522e7604f4ba90571ee9bfac84fb2ff
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-01-01 15:05:42 +0100

    Remove old nvm shims, update scripts

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,10 +1,12 @@
+# shellcheck shell=bash
+
 # Aliases; runs in the same shell you are already in
 # See bin folder for scripts which will run in their own shell
 
 # Global settings
 alias repos='cd ~/code/repos'
 # alias ll='ls -alhF'
-alias ll='exa --long --header --group-directories-first --git --group'
+alias ll='exa --long --header --group-directories-first --git --group --all'
 alias tree='tree -C'
 
 # Doktor
@@ -13,14 +15,14 @@ alias mux='cd ~/code/repos/doktor && tmuxinator'
 
 # Git
 alias git-purge='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d'
-alias git-grep='git rev-list --all | xargs git --no-pager grep --extended-regexp --ignore-case'  # usage: gg <regexp>
+alias git-grep='git rev-list --all | xargs git --no-pager grep --extended-regexp --ignore-case' # usage: gg <regexp>
 alias glog='git log --graph --decorate --pretty=oneline --abbrev-commit --all'
 alias submodule-reset='git submodule deinit -f . && git submodule update --init --recursive'
 
 # Docker
 alias wrk='docker run --interactive --tty --rm skandyla/wrk'
 alias ubuntu='docker run --interactive --tty --rm --volume $(pwd):/host:ro ubuntu:20.04 bash'
-alias docker-ip="docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'"  # append container id
+alias docker-ip="docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'" # append container id
 alias k9s='docker run --rm -it -v $KUBECONFIG:/root/.kube/config quay.io/derailed/k9s'
 
 # Python
@@ -39,34 +41,31 @@ alias venv='PIP_REQUIRE_VIRTUALENV=false python -m pip install --upgrade --user 
 # alias gp='gerrit-amend && gerrit-push'
 # alias gp-draft='gerrit-amend && gerrit-draft'
 
-
 # Per-platform settings, will override the above commands
-case `uname` in
-    Darwin)
-        # commands for macOS go here
+case $(uname) in
+Darwin)
+    # commands for macOS go here
     ;;
-    Linux)
-        # commands for Linux go here
-        if [ -f /etc/redhat-release ]; then
-            # vscode fix
-            # https://code.visualstudio.com/updates/v1_53#_electron-11-update
-            # https://github.com/microsoft/vscode/issues/81358
-            alias code='LD_LIBRARY_PATH=/app/vbuild/RHEL7-x86_64/gcc/9.3.0/lib64/:$LD_LIBRARY_PATH $HOME/apps/vscode/bin/code --no-sandbox'
-        fi
+Linux)
+    # commands for Linux go here
+    if [ -f /etc/redhat-release ]; then
+        # vscode fix
+        # https://code.visualstudio.com/updates/v1_53#_electron-11-update
+        # https://github.com/microsoft/vscode/issues/81358
+        alias code='LD_LIBRARY_PATH=/app/vbuild/RHEL7-x86_64/gcc/9.3.0/lib64/:$LD_LIBRARY_PATH $HOME/apps/vscode/bin/code --no-sandbox'
+    fi
 
     ;;
-    FreeBSD)
-        # commands for FreeBSD go here
+FreeBSD)
+    # commands for FreeBSD go here
     ;;
-    MINGW64_NT-*)
-        # commands for Git bash in Windows go here
+MINGW64_NT-*)
+    # commands for Git bash in Windows go here
 
-        # Python
-        alias activate='source .venv/Scripts/activate'
-        # assuming pyenv
-        alias venv='PIP_REQUIRE_VIRTUALENV=false python -m pip install --upgrade --user pip virtualenv && python -m virtualenv .venv && source .venv/Scripts/activate && python -m pip install --upgrade pip && which pip && pip list && pip --version && python --version'
+    # Python
+    alias activate='source .venv/Scripts/activate'
+    # assuming pyenv
+    alias venv='PIP_REQUIRE_VIRTUALENV=false python -m pip install --upgrade --user pip virtualenv && python -m virtualenv .venv && source .venv/Scripts/activate && python -m pip install --upgrade pip && which pip && pip list && pip --version && python --version'
 
     ;;
 esac
-
-
--- shell/exports.sh
+++ shell/exports.sh
@@ -1,34 +1,36 @@
+# shellcheck shell=bash
+
 # Global settings
-export PIP_REQUIRE_VIRTUALENV=true  # use --isolated to bypass
-export PATH="$HOME/.local/bin:$PATH"  # pipx-installed binaries
-export PATH="$PATH:$HOME/code/repos/dotfiles/shell/bin"  # dotfiles-bin
+export PIP_REQUIRE_VIRTUALENV=true                      # use --isolated to bypass
+export PATH="$HOME/.local/bin:$PATH"                    # pipx-installed binaries
+export PYENV_ROOT="$HOME/.pyenv"                        # pyenv
+export PATH="$PYENV_ROOT/bin:$PATH"                     # pyenv
+export PATH="$PATH:$HOME/code/repos/dotfiles/shell/bin" # dotfiles-bin
+export PATH="$PATH:$HOME/.cargo/bin"                    # rust
 
 #Per-platform settings
-case `uname` in
-    Darwin)
-        export PYENV_ROOT="$HOME/.pyenv"
-        export PATH="$PYENV_ROOT/bin:$PATH"
-        export HOMEBREW_NO_ANALYTICS=1
-        export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-        export PATH="$PATH:$HOME/.cargo/bin"
-        export CLICOLOR=1  # Enable colors
+case $(uname) in
+Darwin)
+    # commands for macOS go here
+    export HOMEBREW_NO_ANALYTICS=1
+    export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+    export CLICOLOR=1 # Enable colors
 
+    # nvm
+    if [ "$(uname -m)" = "arm64" ]; then
+        export NVM_DIR="$HOME/.nvm"
+    elif [ "$(uname -m)" = "x86_64" ]; then
+        export NVM_DIR="$HOME/.nvm_x86"
+    fi
     ;;
-    Linux)
-        # commands for Linux go here
-        export PATH="$HOME/apps/vscode/bin:$PATH"
-        export PYENV_ROOT="$HOME/.pyenv"
-        export PATH="$PYENV_ROOT/bin:$PATH"
-        export KUBECONFIG="$HOME/.kube/config"
-        export PATH=$PATH:/usr/local/go/bin
-        export PATH=$PATH:$HOME/.cargo/bin
 
+Linux)
+    # commands for Linux go here
+    export NVM_DIR="$HOME/.nvm" # nvmexport NVM_DIR="$HOME/.nvm"                             # nvm
     ;;
-    FreeBSD)
-        # commands for FreeBSD go here
+
+MINGW64_NT-*)
+    # commands for Git bash in Windows go here
     ;;
-    MINGW64_NT-*)
-        # commands for Git bash in Windows go here
-    ;;
-    *)
+*) ;;
 esac
