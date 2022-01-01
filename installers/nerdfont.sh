210c22262953a740120ace2336b57b2bd57958bf
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-01-01 16:00:31 +0100

    Copy from find cmd

--- installers/nerdfont.sh
+++ installers/nerdfont.sh
@@ -9,7 +9,7 @@ Darwin)
     if ! ls /Users/${USER}/Library/Fonts/Fira* 1>/dev/null 2>&1; then
         curl --location --output $HOME/Downloads/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
         unzip -o $HOME/Downloads/FiraCode.zip -d $HOME/Downloads/FiraCode
-        find $HOME/Downloads/FiraCode/ -name "*.otf" | xargs -0 cp -v "{}" $HOME/Library/Fonts/
+        find $HOME/Downloads/FiraCode/ -name "*.otf" -exec cp -v {} $HOME/Library/Fonts/ \;
         # cp $HOME/Downloads/FiraCode/*.otf $HOME/Library/Fonts/
         rm $HOME/Downloads/FiraCode.zip
         rm -r $HOME/Downloads/FiraCode
