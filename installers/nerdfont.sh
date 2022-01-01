36b58824aa3d095a23153e39589ab4dfd73c8140
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-01-01 15:48:54 +0100

    Update linux path, use otf

--- installers/nerdfont.sh
+++ installers/nerdfont.sh
@@ -16,11 +16,12 @@ Darwin)
     ;;
 Linux)
     # commands for Linux go here
-    if ! ls /usr/local/share/fonts/Fira* 1>/dev/null 2>&1; then
+    if ! ls ~/.local/share/fonts/Fira* 1>/dev/null 2>&1; then
         # Ubuntu
         curl --location --output ~/Downloads/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
         unzip -o ~/Downloads/FiraCode.zip -d ~/Downloads/FiraCode
-        sudo cp -v ~/Downloads/FiraCode/*.ttf /usr/local/share/fonts
+        mkdir -p ~/.local/share/fonts
+        sudo cp -v ~/Downloads/FiraCode/*.otf ~/.local/share/fonts
         rm ~/Downloads/FiraCode.zip
         rm -r ~/Downloads/FiraCode
 
