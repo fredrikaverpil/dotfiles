b5359a8c5f3fab20bda2e715af0e1e73e7c2d118
Fredrik Averpil <fredrik.averpil@gmail.com>
2022-01-11 21:53:24 +0100

    Remove old nvm installation

--- installers/nvm.sh
+++ installers/nvm.sh
@@ -8,17 +8,6 @@ Darwin)
     # commands for macOS go here
     brew install nvm
 
-    # nvm install 12
-
-    # pyenv global 3.9.9
-    # nvm install 14
-    # pyenv global system
-
-    # x86_64
-    if [ "$(uname -m)" == "arm64" ]; then
-        NVM_DIR=$HOME/.nvm_x86 brew86 install nvm
-    fi
-
     ;;
 Linux)
     # commands for Linux go here
