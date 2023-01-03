1aa8779bc37f7e599442d09624974704a858ad1a
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-01-03 23:52:03 +0100

    feat: add nvim

---
+++ nvim/lua/fredrik/remap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
---
+++ nvim/lua/fredrik/set.lua
@@ -0,0 +1,30 @@
+-- line numbers
+vim.opt.nu = true
+
+-- relative line numbers
+vim.opt.relativenumber = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- faster update time
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "88"
+
+
