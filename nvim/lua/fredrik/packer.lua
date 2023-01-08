9b2a28257468cc79db00a2a13a15b09d527bbf89
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-01-08 11:39:27 +0100

    fix: formatting

--- nvim/lua/fredrik/packer.lua
+++ nvim/lua/fredrik/packer.lua
@@ -4,13 +4,11 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-
+    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', tag = '0.1.0', -- or branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
