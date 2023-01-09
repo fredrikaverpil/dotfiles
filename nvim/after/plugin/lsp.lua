e51f3f9ee73e38e0d9608d23ece164c2a37d86c0
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-01-09 19:37:08 +0100

    feat: use lsp-zero defaults to show diagnostic warnings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,3 +2,12 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
