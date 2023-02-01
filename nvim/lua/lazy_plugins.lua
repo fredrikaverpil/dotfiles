b693505d866e3308790ebe5ea23b4cca062cf2f3
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-02-01 07:59:51 +0100

    chore: cleanup

--- nvim/lua/lazy_plugins.lua
+++ nvim/lua/lazy_plugins.lua
@@ -1,4 +1,4 @@
--- clone down project if it doesn't exist
+-- clone plugin manager (lazy.nvim) if it doesn't exist
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -12,7 +12,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- plugins
+-- plugins to lazy load, and their settings
 rose = {
   'rose-pine/neovim',
   name = 'rose-pine',
@@ -29,46 +29,13 @@ treesitter = {
   run = ':TSUpdate',
   config = function()
     require'nvim-treesitter.configs'.setup {
-      -- A list of parser names, or "all" (the four listed parsers should always be installed)
       ensure_installed = {
           "c", "lua", "vim", "help", "python", "javascript", "typescript", "rust"
       },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
-
-      -- List of parsers to ignore installing (for "all")
-      -- ignore_install = { "javascript" },
-
-      ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-      -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
       highlight = {
-        -- `false` will disable the whole extension
         enable = true,
-
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        -- disable = { "c", "rust" },
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        -- disable = function(lang, buf)
-        --     local max_filesize = 100 * 1024 -- 100 KB
-        --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        --     if ok and stats and stats.size > max_filesize then
-        --         return true
-        --     end
-        -- end,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
       },
     }
@@ -139,23 +106,23 @@ nvimtree = {
     'nvim-tree/nvim-tree.lua',
     tag = 'nightly', -- optional, updated every week. (see issue #1193)
     config = function()
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+      require("nvim-tree").setup({
+        sort_by = "case_sensitive",
+        view = {
+          width = 30,
+          mappings = {
+            list = {
+              { key = "u", action = "dir_up" },
+            },
+          },
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+        },
+      })
     end,
     dependencies = {
         {'nvim-tree/nvim-web-devicons'}, -- optional, for file icons
@@ -163,7 +130,7 @@ require("nvim-tree").setup({
 }
 
 
-
+-- list of plugins to load
 enabled_plugins = {
   rose,
   treesitter,
@@ -173,4 +140,5 @@ enabled_plugins = {
   nvimtree
 }
 
+-- load plugins
 require("lazy").setup(enabled_plugins)
