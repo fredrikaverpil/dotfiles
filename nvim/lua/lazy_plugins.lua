fe3fe29fb018f05da2999d02bc9464cb07817330
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-02-01 07:52:47 +0100

    feat: add nvim-tree

--- nvim/lua/lazy_plugins.lua
+++ nvim/lua/lazy_plugins.lua
@@ -135,12 +135,42 @@ lspzero = {
   }
 }
 
+nvimtree = {
+    'nvim-tree/nvim-tree.lua',
+    tag = 'nightly', -- optional, updated every week. (see issue #1193)
+    config = function()
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+    end,
+    dependencies = {
+        {'nvim-tree/nvim-web-devicons'}, -- optional, for file icons
+    },
+}
+
+
+
 enabled_plugins = {
   rose,
   treesitter,
   telescope,
   fugitive,
-  lspzero
+  lspzero,
+  nvimtree
 }
 
 require("lazy").setup(enabled_plugins)
