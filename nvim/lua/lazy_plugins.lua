49efaac4f6e3b3e511da316eae948582cb2755f2
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-02-04 18:16:35 +0100

    feat: add move.nvim

--- nvim/lua/lazy_plugins.lua
+++ nvim/lua/lazy_plugins.lua
@@ -204,6 +204,28 @@ local trouble = {
     }
 }
 
+move = {
+    'fedepujol/move.nvim',
+    config = function()
+        local opts = { noremap = true, silent = true }
+        -- If using iTerm2, go into Settings -> Profile -> Keys
+        -- and set left option key to send Esc+
+
+        -- Normal-mode commands
+        vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
+        vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
+        vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+        vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+
+        -- Visual-mode commands
+        vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
+        vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
+        vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
+        vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
+    end,
+}
+
+
 -- list of plugins to load
 local enabled_plugins = {
   rose,
@@ -218,6 +240,7 @@ local enabled_plugins = {
   todo_comments,
   which_key,
   trouble,
+  move,
 }
 
 -- load plugins
