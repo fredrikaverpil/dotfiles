4b130ef3c25dc6a70652c664fe3a7ce3f9e3d878
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-02-05 17:32:43 +0100

    feat: configure black, unconfigure flake8

--- nvim/lua/plugins/fredrik.lua
+++ nvim/lua/plugins/fredrik.lua
@@ -3,6 +3,7 @@ return {
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
+    -- opts will be merged with the parent spec
     opts = {
         ensure_installed = {
         "bash",
@@ -27,6 +28,7 @@ return {
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
+    -- opts will be merged with the parent spec
     opts = {
         ensure_installed = {
         -- python
@@ -69,11 +71,25 @@ return {
         }
   },
 
+
+  -- git signs
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require('gitsigns').setup()
+    end,
+  },
+
   -- change trouble config
   {
     "folke/trouble.nvim",
     -- opts will be merged with the parent spec
-    opts = { use_diagnostic_signs = true },
+    opts = {
+      -- auto_open = false, -- automatically open the list when you have diagnostics
+      -- auto_close = false, -- automatically close the list when you have no diagnostics
+      -- use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
+      -- auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+    },
   },
 
   -- change neo-tree config
@@ -91,6 +107,23 @@ return {
     },
   },
 
+
+  -- change null-ls config
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+
+      -- add black as formatter
+      opts.sources = vim.list_extend(opts.sources, { nls.builtins.formatting.black })
+
+      -- remove flake8 from opts.sources
+      opts.sources = vim.tbl_filter(function(source)
+        return source.name ~= "flake8"
+      end, opts.sources)
+
+    end,
+  },
 
 
 }
