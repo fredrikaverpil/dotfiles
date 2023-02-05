0b16e074522b5fe308262bc1f83b9d8eaa7643ca
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-02-05 17:32:24 +0100

    fix: set python3 version

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,6 +6,7 @@
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "python" },
     callback = function()
+        vim.g.python3_host_prog = vim.fn.expand(".venv/bin/python")
         vim.opt_local.colorcolumn = "88"  -- Ruler at column number
         vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
         vim.opt_local.shiftwidth = 4 -- Size of an indent
