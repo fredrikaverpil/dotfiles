2998190812692a4bc9e853a369da9553e2c83e07
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-02-12 19:47:53 +0100

    feat: add pytest config

--- nvim/lua/plugins/fredrik.lua
+++ nvim/lua/plugins/fredrik.lua
@@ -177,19 +177,35 @@ return {
   -- neotest
   {
     "nvim-neotest/neotest",
+
+    keys = {
+      { "<leader>rr", ":lua require('neotest').run.run()<CR>", desc = "Run nearest test" },
+      { "<leader>rl", ":lua require('neotest').run.run_last()<CR>", desc = "Run last test" },
+      { "<leader>rf", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", desc = "Run tests in file" },
+      { "<leader>rs", ":lua require('neotest').summary.toggle()<CR>", desc = "Run test summary" },
+      { "<leader>ro", ":lua require('neotest').output.open({ enter = true })<CR>", desc = "Run test output" },
+      { "<leader>rp", ":lua require('neotest').output_panel.toggle()<CR>", desc = "Run test output panel" },
+
+      -- debugging via nvim-dap
+      -- { "<leader>rd", ":lua require('neotest').run.run({ strategy = 'dap' })<CR>", desc = "Debug nearest test" },
+    },
+
     config = function()
       require("neotest").setup({
         adapters = {
           require("neotest-python")({
             dap = { justMyCode = false },
+            -- runner = "pytest",
           }),
           require("neotest-plenary"),
           require("neotest-vim-test")({
+            -- ignore file types for installed adapters (or use allow_file_types)
             ignore_file_types = { "python", "vim", "lua" },
           }),
         },
       })
     end,
+
     dependencies = {
       { "nvim-lua/plenary.nvim" },
       { "nvim-treesitter/nvim-treesitter" },
