210f7d68215f5a765bdcfd3b6214fd3973ab9469
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-03-14 09:07:03 +0100

    refactor: move editor stuff

--- nvim/lua/plugins/fredrik.lua
+++ nvim/lua/plugins/fredrik.lua
@@ -67,21 +67,6 @@ return {
     },
   },
 
-  -- change neo-tree config
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    -- opts will be merged with the parent spec
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true, -- when true, they will just be displayed differently than normal items
-          hide_dotfiles = false,
-          hide_gitignored = true,
-        },
-      },
-    },
-  },
-
   -- change null-ls config
   {
     "jose-elias-alvarez/null-ls.nvim",
@@ -107,9 +92,6 @@ return {
       })
     end,
   },
-
-  -- disable tabs
-  { "akinsho/bufferline.nvim", enabled = false },
 
   -- Use <tab> for completion via supertab
   {
