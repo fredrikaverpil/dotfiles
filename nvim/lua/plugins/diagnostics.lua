601f8e7ab345227634068faa4f311a4f54639c41
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-03-14 09:55:59 +0100

    chore: format

--- nvim/lua/plugins/diagnostics.lua
+++ nvim/lua/plugins/diagnostics.lua
@@ -1,5 +1,7 @@
 return {
 
+  -- NOTE: see null_ls.lua for diagnostics config via null-ls
+
   -- change trouble config
   {
     "folke/trouble.nvim",
@@ -10,28 +12,5 @@ return {
       -- use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
       -- auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
     },
-  },
-
-  -- change null-ls config
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = { "mason.nvim" },
-    event = { "BufReadPre", "BufNewFile" },
-    opts = function(_, opts)
-      local null_ls = require("null-ls")
-      local diagnostics = null_ls.builtins.diagnostics
-
-      null_ls.setup({
-        -- debug = true, -- Turn on debug for :NullLsLog
-        debug = false,
-        diagnostics_format = "#{m} #{s}[#{c}]",
-        sources = {
-          -- list of supported sources:
-          -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
-          diagnostics.ruff,
-          diagnostics.mypy,
-        },
-      })
-    end,
   },
 }
--- nvim/lua/plugins/fredrik.lua
+++ nvim/lua/plugins/fredrik.lua
@@ -1,6 +1,5 @@
 return {
 
-
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
@@ -38,7 +37,7 @@ return {
       local cmp = require("cmp")
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
-            ["<Tab>"] = cmp.mapping(function(fallback)
+        ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
             -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
@@ -51,7 +50,7 @@ return {
             fallback()
           end
         end, { "i", "s" }),
-            ["<S-Tab>"] = cmp.mapping(function(fallback)
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
