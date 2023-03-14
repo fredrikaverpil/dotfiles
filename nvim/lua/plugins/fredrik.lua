af5aa08409b1b338653618b694dfd51b5de2d915
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-03-14 08:00:25 +0100

    fix: theme

--- nvim/lua/plugins/fredrik.lua
+++ nvim/lua/plugins/fredrik.lua
@@ -1,5 +1,21 @@
 return {
 
+  -- tokyonight, style: night
+  {
+    "folke/tokyonight.nvim",
+    -- opts will be merged with the parent spec
+    opts = {
+      -- style
+      style = "night", -- can be: storm, night, moon, day
+      -- disable the background color
+      transparent = false,
+      -- change the default "italic_comment" style to be underlined
+      styles = { italic_comment = "underline" },
+      -- disable bolding and italicizing keywords (if you are using another plugin that bolds keywords, you may want to disable this)
+      disable = { bold = true, italic = true },
+    },
+  },
+
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
