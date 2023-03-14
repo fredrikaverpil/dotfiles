72209bb2f5f366253506a9e1adb6a821560ea287
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-03-14 08:07:24 +0100

    feat: add diffview and octo

--- nvim/lua/plugins/fredrik.lua
+++ nvim/lua/plugins/fredrik.lua
@@ -250,6 +250,32 @@ return {
     },
   },
 
+  -- diffview
+  {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+    },
+    config = function()
+      require("diffview").setup({
+        enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+      })
+    end,
+  },
+
+  -- octo
+  {
+    "pwntester/octo.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "kyazdani42/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup()
+    end,
+  },
+
   -- leap
   {
     "ggandor/leap.nvim",
