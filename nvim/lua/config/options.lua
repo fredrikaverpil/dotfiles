c83db00502a17d2749e54a5c3f2192184f679905
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-03-14 10:04:59 +0100

    chore: enable swap

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,7 +9,7 @@
 vim.opt.listchars = "tab:▸ ,trail:·,nbsp:␣,extends:❯,precedes:❮"
 
 -- disable swap files
-vim.opt.swapfile = false
+-- vim.opt.swapfile = false
 
 -- fill character for diff: /
 vim.opt.fillchars = "diff:/"
