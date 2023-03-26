9973e6df473370eb99660bc707c0c47b63464fad
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-03-26 09:58:21 +0200

    fix: temporary fix where prompt cant be sent
    https://github.com/jackMort/ChatGPT.nvim/issues/99

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -17,6 +17,9 @@ return {
     config = function()
       require("chatgpt").setup({
         -- optional configuration
+        keymaps = {
+          submit = "<C-a>", -- temporary solution, because of https://github.com/jackMort/ChatGPT.nvim/issues/99
+        },
       })
     end,
     dependencies = {
