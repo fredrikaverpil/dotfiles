4db509a562042e1b4d22bc4bfc9561737870fef2
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-04-03 23:32:06 +0200

    chore: cannot load plugins

--- nvim-nvchad/lua/custom/plugins.lua
+++ nvim-nvchad/lua/custom/plugins.lua
@@ -79,28 +79,10 @@ local plugins = {
   },
 
 
-  {
-    "folke/trouble.nvim",
-    -- opts will be merged with the parent spec
-    opts = {
-      -- auto_open = false, -- automatically open the list when you have diagnostics
-      -- auto_close = false, -- automatically close the list when you have no diagnostics
-      -- use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
-      -- auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-    },
-  },
 
-	-- git signs
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
+
+
 
 }
 
-
-
-
-  return plugins
+return plugins
