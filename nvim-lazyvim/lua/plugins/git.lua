74fab643d7ed90c021e8735b91f72071aaa78970
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-04-03 09:26:12 +0200

    fix: errors

--- nvim-lazyvim/lua/plugins/git.lua
+++ nvim-lazyvim/lua/plugins/git.lua
@@ -1,36 +1,36 @@
 return {
 
-  -- git signs
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  },
+	-- git signs
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
 
-  -- diffview
-  {
-    "sindrets/diffview.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-    },
-    config = function()
-      require("diffview").setup({
-        enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
-      })
-    end,
-  },
+	-- diffview
+	-- {
+	--   "sindrets/diffview.nvim",
+	--   dependencies = {
+	--     { "nvim-lua/plenary.nvim" },
+	--   },
+	--   config = function()
+	--     require("diffview").setup({
+	--     enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+	--     })
+	--   end,
+	-- },
 
-  -- octo
-  {
-    "pwntester/octo.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "kyazdani42/nvim-web-devicons",
-    },
-    config = function()
-      require("octo").setup()
-    end,
-  },
+	-- octo
+	{
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"kyazdani42/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup()
+		end,
+	},
 }
--- nvim-lazyvim/lua/plugins/treesitter.lua
+++ nvim-lazyvim/lua/plugins/treesitter.lua
@@ -1,27 +1,26 @@
 return {
 
-  -- add more treesitter parsers, used for e.g. theming and other plugins
-  {
-    "nvim-treesitter/nvim-treesitter",
-    -- opts will be merged with the parent spec
-    opts = {
-      ensure_installed = {
-        "bash",
-        "help",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      },
-    },
-  },
+	-- add more treesitter parsers, used for e.g. theming and other plugins
+	{
+		"nvim-treesitter/nvim-treesitter",
+		-- opts will be merged with the parent spec
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+		},
+	},
 }
