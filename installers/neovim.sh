e3d8d90f5d49bc483e5b14d3587b794b6c989ff1
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-04-03 23:31:42 +0200

    fix: install only if folder does not exist

--- installers/neovim.sh
+++ installers/neovim.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -ex
 
-git clone https://github.com/NvChad/NvChad ~/.config/NvChad --depth 1
-git clone --recursive https://github.com/LazyVim/starter.git ~/.config/LazyVim
+if [ ! -d "$HOME/.config/NvChad" ]; then
+    git clone https://github.com/NvChad/NvChad ~/.config/NvChad --depth 1
+fi
+
+if [ ! -d "$HOME/.config/LazyVim" ]; then
+    git clone --recursive https://github.com/LazyVim/starter.git ~/.config/LazyVim
+fi
