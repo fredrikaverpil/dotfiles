9d09c7758eaf9fe99bca9def6a0a4b82c3e37067
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-04-06 15:50:29 +0200

    feat: add cht.sh

---
+++ shell/bin/cht.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+languages=$(echo "python typescript rust golang c" | tr " " "\n")
+core_utls=$(echo "find xargs sed awk" | tr " " "\n")
+selected=$(echo -e "$languages\n$core_utls" | fzf )
+
+read -p "Enter the search term: " query
+
+if echo "$languages" | grep -q "$selected" ; then
+  curl cht.sh/$selected/$(echo $query | tr " " "+")
+else
+    curl cht.sh/$selected-$query
+fi
