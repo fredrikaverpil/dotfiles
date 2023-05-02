7c0f03fe61643f05257a5f1f605eae8d9d50b627
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-02 21:54:05 +0200

    fix: formatting

--- nvim-lazyvim/lua/plugins/mason.lua
+++ nvim-lazyvim/lua/plugins/mason.lua
@@ -1,23 +1,23 @@
 return {
 
-  -- NOTE: also see ':h Lazy' or run :Lazy to lazy-load all plugins
+	-- NOTE: also see ':h Lazy' or run :Lazy to lazy-load all plugins
 
-  -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    -- opts will be merged with the parent spec
-    opts = {
-      ensure_installed = {
-        -- python
-        "debugpy",
-        -- lua
-        "stylua",
-        -- shell
-        "shellcheck",
-        "shfmt",
-        -- javascript/typescript
-        "prettier",
-      },
-    },
-  },
+	-- add any tools you want to have installed below
+	{
+		"williamboman/mason.nvim",
+		-- opts will be merged with the parent spec
+		opts = {
+			ensure_installed = {
+				-- python
+				"debugpy",
+				-- lua
+				"stylua",
+				-- shell
+				"shellcheck",
+				"shfmt",
+				-- javascript/typescript
+				"prettier",
+			},
+		},
+	},
 }
--- nvim-lazyvim/lua/plugins/null_ls.lua
+++ nvim-lazyvim/lua/plugins/null_ls.lua
@@ -1,28 +1,30 @@
 return {
 
-  -- change null-ls config
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = { "mason.nvim" },
-    event = { "BufReadPre", "BufNewFile" },
-    opts = function()
-      local null_ls = require("null-ls")
-      local formatting = null_ls.builtins.formatting
-      local diagnostics = null_ls.builtins.diagnostics
+	-- change null-ls config
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		dependencies = { "mason.nvim" },
+		event = { "BufReadPre", "BufNewFile" },
+		opts = function()
+			local null_ls = require("null-ls")
+			local formatting = null_ls.builtins.formatting
+			local diagnostics = null_ls.builtins.diagnostics
 
-      null_ls.setup({
-        -- debug = true, -- Turn on debug for :NullLsLog
-        debug = false,
-        -- diagnostics_format = "#{m} #{s}[#{c}]",
-        sources = {
-          -- list of supported sources:
-          -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
-          formatting.stylua,
-          formatting.black, -- causes crash on multiple file save with neovim 0.8.3
-          diagnostics.ruff,
-          diagnostics.mypy,
-        },
-      })
-    end,
-  },
+			null_ls.setup({
+				-- debug = true, -- Turn on debug for :NullLsLog
+				debug = false,
+				-- diagnostics_format = "#{m} #{s}[#{c}]",
+				sources = {
+					-- list of supported sources:
+					-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+					formatting.stylua,
+					formatting.black,
+					formatting.prettier,
+					diagnostics.ruff,
+					diagnostics.mypy,
+					diagnostics.eslint,
+				},
+			})
+		end,
+	},
 }
