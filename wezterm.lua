e75c2795bdaef8f455ef505db8cbb3c537e6e5ea
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-08 21:12:21 +0200

    feat: add theme

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,13 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 -- https://wezfurlong.org/wezterm/config/files.html
 
-config.font = wezterm.font_with_fallback {
-    'JetBrains Mono',
-    'JetBrains Mono Nerd Font'
-}
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"JetBrains Mono Nerd Font",
+})
+
+config.color_scheme = "Catppuccin Mocha"
 
 return config
