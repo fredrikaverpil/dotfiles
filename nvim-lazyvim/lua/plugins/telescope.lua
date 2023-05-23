841b6919bc6fa7e1d980fe6af8f16d748042dc2c
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-23 20:44:27 +0200

    fix: show hidden files

--- nvim-lazyvim/lua/plugins/telescope.lua
+++ nvim-lazyvim/lua/plugins/telescope.lua
@@ -1,14 +1,20 @@
 return {
 
-  -- change telescope config
-  {
-    "nvim-telescope/telescope.nvim",
-    -- opts will be merged with the parent spec
-    opts = {
-      defaults = {
-        file_ignore_patterns = { "^./.git/", "^node_modules/", "^poetry.lock" },
-      },
-    },
-  },
-
+	-- change telescope config
+	{
+		"nvim-telescope/telescope.nvim",
+		-- opts will be merged with the parent spec
+		opts = {
+			defaults = {
+				file_ignore_patterns = { "^.git/", "^node_modules/", "^poetry.lock" },
+			},
+			pickers = {
+				live_grep = {
+					additional_args = function()
+						return { "--hidden" }
+					end,
+				},
+			},
+		},
+	},
 }
