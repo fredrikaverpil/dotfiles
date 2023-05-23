ab4785555d43550e1a01c2da49c4f0c2d25757c8
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-23 20:43:42 +0200

    feat: add ruler etc for ts

--- nvim-lazyvim/lua/config/autocmds.lua
+++ nvim-lazyvim/lua/config/autocmds.lua
@@ -22,3 +22,12 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.shiftwidth = 4 -- Size of an indent
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "typescript",
+	callback = function()
+		vim.opt_local.colorcolumn = "79" -- Ruler at column number
+		vim.opt_local.tabstop = 4
+		vim.opt_local.shiftwidth = 4
+	end,
+})
