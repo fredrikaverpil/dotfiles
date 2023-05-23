e861115a2f48fcf4e7aba651e8383bfda227bda5
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-23 21:33:33 +0200

    feat: add ripgrep search

--- nvim-lazyvim/lua/plugins/telescope.lua
+++ nvim-lazyvim/lua/plugins/telescope.lua
@@ -3,6 +3,24 @@ return {
 	-- change telescope config
 	{
 		"nvim-telescope/telescope.nvim",
+
+		dependencies = {
+			"nvim-telescope/telescope-live-grep-args.nvim",
+		},
+		config = function()
+			-- https://github.com/nvim-telescope/telescope-live-grep-args.nvim
+			require("telescope").load_extension("live_grep_args")
+		end,
+		keys = {
+			{
+				"<leader>fs",
+				function()
+					require("telescope.").extensions.live_grep_args.live_grep_args()
+				end,
+				desc = "Find Plugin File",
+			},
+		},
+
 		-- opts will be merged with the parent spec
 		opts = {
 			defaults = {
