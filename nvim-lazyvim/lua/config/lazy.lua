d915e7c789ed67e111ae3b325e163a71a2342259
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-24 17:11:43 +0200

    feat: enable extra modules

--- nvim-lazyvim/lua/config/lazy.lua
+++ nvim-lazyvim/lua/config/lazy.lua
@@ -12,12 +12,11 @@ require("lazy").setup({
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
 		-- import any extras modules here
-		-- { import = "lazyvim.plugins.extras.lang.typescript" },
-		-- { import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.lang.json" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-		-- import/override with your plugins
 
-		-- added by Fredrik
+		-- import/override with your plugins
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.dap.nlua" },
