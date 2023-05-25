cf228b2fea2c62ca9f87a23ac7c6720e154f4695
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-25 14:34:36 +0200

    feat: add debugging for python

---
+++ nvim-lazyvim/lua/plugins/debug.lua
@@ -0,0 +1,46 @@
+-- See https://www.lazyvim.org/plugins/extras/dap.core
+-- and test.lua for keymaps
+
+return {
+
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			-- "theHamsta/nvim-dap-virtual-text",
+		},
+
+		config = function()
+			-- vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
+			local dap = require("dap")
+
+			-- Python DAP
+			-- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Python
+			dap.adapters.python = {
+				type = "executable",
+				command = "python",
+				args = { "-m", "debugpy.adapter" },
+			}
+
+			dap.configurations.python = {
+				{
+					-- The first three options are required by nvim-dap
+					type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+					request = "launch",
+					name = "Launch file",
+					justMyCode = false,
+
+					-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+					program = "${file}", -- This configuration will launch the current file if used.
+					pythonPath = function()
+						-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+						-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+						-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+						return os.getenv("VIRTUAL_ENV") .. "/bin/python" or "/usr/bin/env python"
+					end,
+				},
+			}
+		end,
+	},
+}
--- nvim-lazyvim/lua/plugins/test.lua
+++ nvim-lazyvim/lua/plugins/test.lua
@@ -1,6 +1,5 @@
 return {
 
-	-- neotest
 	{
 		"nvim-neotest/neotest",
 		dependencies = {
@@ -11,11 +10,6 @@ return {
 			-- adapters
 			{
 				"nvim-neotest/neotest-python",
-				-- i'm not sure where I got this from, and not sure why it would be needed... commenting out for now.
-				-- dependencies = {
-				-- 	{ "nvim-neotest/neotest-plenary" },
-				-- 	{ "nvim-neotest/neotest-vim-test" },
-				-- },
 			},
 		},
 		keys = {
@@ -26,25 +20,17 @@ return {
 			{ "<leader>to", ":lua require('neotest').output.open({ enter = true })<CR>", desc = "Run test output" },
 			{ "<leader>tp", ":lua require('neotest').output_panel.toggle()<CR>", desc = "Run test output panel" },
 
-			-- debugging via nvim-dap
-			-- { "<leader>ctd", ":lua require('neotest').run.run({ strategy = 'dap' })<CR>", desc = "Debug nearest test" },
+			-- debugging via nvim-dap, see debug.lua
+			{ "<leader>td", ":lua require('neotest').run.run({ strategy = 'dap' })<CR>", desc = "Debug nearest test" },
 		},
 		config = function()
 			-- Setup neotest adapter for python
 			require("neotest").setup({
 				adapters = {
 					require("neotest-python")({
-						dap = { justMyCode = false },
 						runner = "pytest",
 						args = { "--log-level", "DEBUG", "--color", "yes" },
 					}),
-
-					-- i'm not sure where I got this from, and not sure why it would be needed... commenting out for now.
-					-- require("neotest-plenary"),
-					-- require("neotest-vim-test")({
-					-- 	-- ignore file types for installed adapters (or use allow_file_types)
-					-- 	ignore_file_types = { "python", "vim", "lua" },
-					-- }),
 				},
 			})
 		end,
