b300c68883021c554a44318b3d111e6c9c442a78
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-25 15:25:20 +0200

    fix: use spaces

--- nvim-lazyvim/lua/plugins/debug.lua
+++ nvim-lazyvim/lua/plugins/debug.lua
@@ -3,44 +3,54 @@
 
 return {
 
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-			-- "theHamsta/nvim-dap-virtual-text",
-		},
+  -- fix error seen in https://github.com/LazyVim/LazyVim/discussions/821
+  -- {
+  --   "theHamsta/nvim-dap-virtual-text",
+  --   config = function()
+  --     require("nvim-dap-virtual-text").setup({
+  --       display_callback = function(variable, buf, stackframe, node, options) end,
+  --     })
+  --   end,
+  -- },
 
-		config = function()
-			-- vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
-			local dap = require("dap")
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      -- "theHamsta/nvim-dap-virtual-text",
+    },
 
-			-- Python DAP
-			-- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Python
-			dap.adapters.python = {
-				type = "executable",
-				command = "python",
-				args = { "-m", "debugpy.adapter" },
-			}
+    config = function()
+      -- vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
+      local dap = require("dap")
 
-			dap.configurations.python = {
-				{
-					-- The first three options are required by nvim-dap
-					type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
-					request = "launch",
-					name = "Launch file",
-					justMyCode = false,
+      -- Python DAP
+      -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Python
+      dap.adapters.python = {
+        type = "executable",
+        command = "python",
+        args = { "-m", "debugpy.adapter" },
+      }
 
-					-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+      dap.configurations.python = {
+        {
+          -- The first three options are required by nvim-dap
+          type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+          request = "launch",
+          name = "Launch file",
+          justMyCode = false,
 
-					program = "${file}", -- This configuration will launch the current file if used.
-					pythonPath = function()
-						-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-						-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-						-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-						return os.getenv("VIRTUAL_ENV") .. "/bin/python" or "/usr/bin/env python"
-					end,
-				},
-			}
-		end,
-	},
+          -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+          program = "${file}", -- This configuration will launch the current file if used.
+          pythonPath = function()
+            -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+            -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+            -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+            return os.getenv("VIRTUAL_ENV") .. "/bin/python" or "/usr/bin/env python"
+          end,
+        },
+      }
+    end,
+  },
 }
--- nvim-lazyvim/lua/plugins/test.lua
+++ nvim-lazyvim/lua/plugins/test.lua
@@ -1,38 +1,38 @@
 return {
 
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-treesitter/nvim-treesitter" },
-			{ "antoinemadec/FixCursorHold.nvim" },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-treesitter/nvim-treesitter" },
+      { "antoinemadec/FixCursorHold.nvim" },
 
-			-- adapters
-			{
-				"nvim-neotest/neotest-python",
-			},
-		},
-		keys = {
-			{ "<leader>tt", ":lua require('neotest').run.run()<CR>", desc = "Run nearest test" },
-			{ "<leader>tl", ":lua require('neotest').run.run_last()<CR>", desc = "Run last test" },
-			{ "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", desc = "Run tests in file" },
-			{ "<leader>ts", ":lua require('neotest').summary.toggle()<CR>", desc = "Run test summary" },
-			{ "<leader>to", ":lua require('neotest').output.open({ enter = true })<CR>", desc = "Run test output" },
-			{ "<leader>tp", ":lua require('neotest').output_panel.toggle()<CR>", desc = "Run test output panel" },
+      -- adapters
+      {
+        "nvim-neotest/neotest-python",
+      },
+    },
+    keys = {
+      { "<leader>tt", ":lua require('neotest').run.run()<CR>", desc = "Run nearest test" },
+      { "<leader>tl", ":lua require('neotest').run.run_last()<CR>", desc = "Run last test" },
+      { "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", desc = "Run tests in file" },
+      { "<leader>ts", ":lua require('neotest').summary.toggle()<CR>", desc = "Run test summary" },
+      { "<leader>to", ":lua require('neotest').output.open({ enter = true })<CR>", desc = "Run test output" },
+      { "<leader>tp", ":lua require('neotest').output_panel.toggle()<CR>", desc = "Run test output panel" },
 
-			-- debugging via nvim-dap, see debug.lua
-			{ "<leader>td", ":lua require('neotest').run.run({ strategy = 'dap' })<CR>", desc = "Debug nearest test" },
-		},
-		config = function()
-			-- Setup neotest adapter for python
-			require("neotest").setup({
-				adapters = {
-					require("neotest-python")({
-						runner = "pytest",
-						args = { "--log-level", "DEBUG", "--color", "yes" },
-					}),
-				},
-			})
-		end,
-	},
+      -- debugging via nvim-dap, see debug.lua
+      { "<leader>td", ":lua require('neotest').run.run({ strategy = 'dap' })<CR>", desc = "Debug nearest test" },
+    },
+    config = function()
+      -- Setup neotest adapter for python
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python")({
+            runner = "pytest",
+            args = { "--log-level", "DEBUG", "--color", "yes" },
+          }),
+        },
+      })
+    end,
+  },
 }
