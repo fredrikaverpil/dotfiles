c54f9df052a774f09bb64b41be8f1e768d040f61
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-25 12:26:06 +0200

    feat: add prettier

--- nvim-lazyvim/lua/config/lazy.lua
+++ nvim-lazyvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "lazyvim.plugins.extras.dap.nlua" },
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 
 		{ import = "plugins" },
 	},
