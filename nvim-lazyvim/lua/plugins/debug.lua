f2a7f83372493acf6c5fed43032ec241f9ef66aa
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-25 15:27:10 +0200

    fix: cleanup

--- nvim-lazyvim/lua/plugins/debug.lua
+++ nvim-lazyvim/lua/plugins/debug.lua
@@ -38,10 +38,8 @@ return {
           type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
           request = "launch",
           name = "Launch file",
-          justMyCode = false,
 
           -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
           program = "${file}", -- This configuration will launch the current file if used.
           pythonPath = function()
             -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
@@ -49,6 +47,7 @@ return {
             -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
             return os.getenv("VIRTUAL_ENV") .. "/bin/python" or "/usr/bin/env python"
           end,
+          justMyCode = false,
         },
       }
     end,
