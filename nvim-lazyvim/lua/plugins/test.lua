da2eca04abe4c5d6ef167234120ad434d87e0b9b
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-26 20:44:14 +0200

    feat: enable testing for rust

--- nvim-lazyvim/lua/plugins/test.lua
+++ nvim-lazyvim/lua/plugins/test.lua
@@ -1,6 +1,17 @@
 return {
 
   {
+    "folke/neodev.nvim",
+    config = function()
+      require("neodev").setup({
+        library = { plugins = { "neotest" }, types = true },
+      })
+    end,
+  },
+
+  -- neotest, also set up via lazy.lua
+  -- https://www.lazyvim.org/plugins/extras/test.core
+  {
     "nvim-neotest/neotest",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
@@ -8,21 +19,22 @@ return {
       { "antoinemadec/FixCursorHold.nvim" },
 
       -- adapters
-      {
-        "nvim-neotest/neotest-python",
-      },
+      { "nvim-neotest/neotest-python" },
+      { "rouge8/neotest-rust" },
     },
-    keys = {
-      { "<leader>tt", ":lua require('neotest').run.run()<CR>", desc = "Run nearest test" },
-      { "<leader>tl", ":lua require('neotest').run.run_last()<CR>", desc = "Run last test" },
-      { "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", desc = "Run tests in file" },
-      { "<leader>ts", ":lua require('neotest').summary.toggle()<CR>", desc = "Run test summary" },
-      { "<leader>to", ":lua require('neotest').output.open({ enter = true })<CR>", desc = "Run test output" },
-      { "<leader>tp", ":lua require('neotest').output_panel.toggle()<CR>", desc = "Run test output panel" },
 
-      -- debugging via nvim-dap, see debug.lua
-      { "<leader>td", ":lua require('neotest').run.run({ strategy = 'dap' })<CR>", desc = "Debug nearest test" },
-    },
+    -- keys = {
+    --   { "<leader>tt", ":lua require('neotest').run.run()<CR>", desc = "Run nearest test" },
+    --   { "<leader>tl", ":lua require('neotest').run.run_last()<CR>", desc = "Run last test" },
+    --   { "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", desc = "Run tests in file" },
+    --   { "<leader>ts", ":lua require('neotest').summary.toggle()<CR>", desc = "Run test summary" },
+    --   { "<leader>to", ":lua require('neotest').output.open({ enter = true })<CR>", desc = "Run test output" },
+    --   { "<leader>tp", ":lua require('neotest').output_panel.toggle()<CR>", desc = "Run test output panel" },
+    --
+    --   -- debugging via nvim-dap, see debug.lua
+    --   { "<leader>td", ":lua require('neotest').run.run({ strategy = 'dap' })<CR>", desc = "Debug nearest test" },
+    -- },
+
     config = function()
       -- Setup neotest adapter for python
       require("neotest").setup({
@@ -30,6 +42,13 @@ return {
           require("neotest-python")({
             runner = "pytest",
             args = { "--log-level", "DEBUG", "--color", "yes" },
+          }),
+
+          require("neotest-rust")({
+            -- requires nextest, which can be installed via "cargo binstall:
+            -- https://github.com/cargo-bins/cargo-binstall
+            -- https://nexte.st/book/pre-built-binaries.html
+            args = { "--no-capture" },
           }),
         },
       })
--- nvim-lazyvim/lua/plugins/treesitter.lua
+++ nvim-lazyvim/lua/plugins/treesitter.lua
@@ -1,26 +1,27 @@
 return {
 
-	-- add more treesitter parsers, used for e.g. theming and other plugins
-	{
-		"nvim-treesitter/nvim-treesitter",
-		-- opts will be merged with the parent spec
-		opts = {
-			ensure_installed = {
-				"bash",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"tsx",
-				"typescript",
-				"vim",
-				"yaml",
-			},
-		},
-	},
+  -- add more treesitter parsers, used for e.g. theming and other plugins
+  {
+    "nvim-treesitter/nvim-treesitter",
+    -- opts will be merged with the parent spec
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
 }
