85f0eeb2128a80fa73a7b3dbc338289ad008390c
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-27 08:59:57 +0200

    feat: use tokyo night

--- wezterm.lua
+++ wezterm.lua
@@ -4,10 +4,13 @@ local config = {}
 -- https://wezfurlong.org/wezterm/config/files.html
 
 config.font = wezterm.font_with_fallback({
-	"JetBrains Mono",
-	"JetBrains Mono Nerd Font",
+  "JetBrains Mono",
+  "JetBrains Mono Nerd Font",
 })
 
-config.color_scheme = "Catppuccin Mocha"
+-- https://wezfurlong.org/wezterm/colorschemes/
+-- config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "tokyonight_night"
+config.color_scheme = "Tokyo Night (Gogh)"
 
 return config
