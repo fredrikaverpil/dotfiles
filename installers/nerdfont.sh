a457ebfd89f457740c7da9fff02e8d5da7ec8cdf
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-28 15:00:43 +0200

    fix: use jetbrains

--- installers/nerdfont.sh
+++ installers/nerdfont.sh
@@ -5,34 +5,34 @@
 # Per-platform settings
 case $(uname) in
 Darwin)
-    # commands for macOS go here
-    if ! ls "/Users/${USER}/Library/Fonts/Fira"* 1>/dev/null 2>&1; then
-        curl --location --output ~/Downloads/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-        unzip -o ~/Downloads/FiraCode.zip -d ~/Downloads/FiraCode
-        mkdir -p ~/Library/Fonts/
-        cp -v ~/Downloads/FiraCode/*.otf ~/Library/Fonts/
-        rm ~/Downloads/FiraCode.zip
-        rm -r ~/Downloads/FiraCode
-    fi
-    ;;
+	# commands for macOS go here
+	if ! ls "/Users/${USER}/Library/Fonts/JetBrainsMono"* 1>/dev/null 2>&1; then
+		curl --location --output ~/Downloads/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+		unzip -o ~/Downloads/JetBrainsMono.zip -d ~/Downloads/JetBrainsMono
+		mkdir -p ~/Library/Fonts/
+		cp -v ~/Downloads/JetBrainsMono/* ~/Library/Fonts/
+		rm ~/Downloads/JetBrainsMono.zip
+		rm -r ~/Downloads/JetBrainsMono
+	fi
+	;;
 Linux)
-    # commands for Linux go here
-    if ! ls ~/.local/share/fonts/Fira* 1>/dev/null 2>&1; then
-        # Ubuntu
-        curl --location --output ~/Downloads/FiraCode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-        unzip -o ~/Downloads/FiraCode.zip -d ~/Downloads/FiraCode
-        mkdir -p ~/.local/share/fonts
-        sudo cp -v ~/Downloads/FiraCode/*.otf ~/.local/share/fonts
-        rm ~/Downloads/FiraCode.zip
-        rm -r ~/Downloads/FiraCode
+	# commands for Linux go here
+	if ! ls ~/.local/share/fonts/JetBrainsMono* 1>/dev/null 2>&1; then
+		# Ubuntu
+		curl --location --output ~/Downloads/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+		unzip -o ~/Downloads/JetBrainsMono.zip -d ~/Downloads/JetBrainsMono
+		mkdir -p ~/.local/share/fonts
+		sudo cp -v ~/Downloads/JetBrainsMono/* ~/.local/share/fonts
+		rm ~/Downloads/JetBrainsMono.zip
+		rm -r ~/Downloads/JetBrainsMono
 
-    fi
-    ;;
+	fi
+	;;
 FreeBSD)
-    # commands for FreeBSD go here
-    ;;
+	# commands for FreeBSD go here
+	;;
 MINGW64_NT-*)
-    # commands for Git bash in Windows go here
-    ;;
+	# commands for Git bash in Windows go here
+	;;
 *) ;;
 esac
