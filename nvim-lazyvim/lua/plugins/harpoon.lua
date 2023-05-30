dae2d13873d1c3e171b6c6a48898034585ae8a08
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-30 16:01:05 +0200

    feat: add harpoon

---
+++ nvim-lazyvim/lua/plugins/harpoon.lua
@@ -0,0 +1,19 @@
+return {
+
+  {
+    "ThePrimeagen/harpoon",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>hh", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Harpoon menu" },
+      -- { "<leader>ht", ":Telescope harpoon marks<CR>", desc = "Telescope menu" },
+
+      { "<leader>ha", ":lua require('harpoon.mark').add_file()<CR>", desc = "Add file as marked" },
+      { "<leader>hn", ":lua require('harpoon.ui').nav_next()<CR>", desc = "Next file" },
+      { "<leader>hp", ":lua require('harpoon.ui').nav_prev()<CR>", desc = "Previous file" },
+      { "<leader>ht", ":lua require('harpoon.term').gotoTerminal(1)<CR>", desc = "Terminal" },
+    },
+    config = function()
+      require("telescope").load_extension("harpoon")
+    end,
+  },
+}
