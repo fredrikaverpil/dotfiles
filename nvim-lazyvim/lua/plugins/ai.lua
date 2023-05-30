35651be8fb7681689daf0b97f98bb834efb63bf1
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-30 16:00:38 +0200

    fix: remove unused

--- nvim-lazyvim/lua/plugins/ai.lua
+++ nvim-lazyvim/lua/plugins/ai.lua
@@ -1,20 +1,17 @@
 return {
 
-	-- chatgpt
-	{
-		"jackMort/ChatGPT.nvim",
-		config = function()
-			require("chatgpt").setup({
-				-- optional configuration
-				keymaps = {
-					-- submit = "<C-a>", -- temporary solution, because of https://github.com/jackMort/ChatGPT.nvim/issues/99
-				},
-			})
-		end,
-		dependencies = {
-			{ "MunifTanjim/nui.nvim" },
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-telescope/telescope.nvim" },
-		},
-	},
+  -- chatgpt
+  {
+    "jackMort/ChatGPT.nvim",
+    config = function()
+      require("chatgpt").setup({
+        -- optional configuration
+      })
+    end,
+    dependencies = {
+      { "MunifTanjim/nui.nvim" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+  },
 }
