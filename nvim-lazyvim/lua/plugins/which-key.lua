74a851109b39e0d275edff501a25e1850f61b96a
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-30 16:02:08 +0200

    fix: which-keys

--- nvim-lazyvim/lua/plugins/which-key.lua
+++ nvim-lazyvim/lua/plugins/which-key.lua
@@ -1,19 +1,22 @@
 return {
-	-- Modify which-key keys
-	{
-		"folke/which-key.nvim",
-		opts = function()
-			require("which-key").register({
-				["<leader>t"] = {
-					name = "+test",
-				},
-				["<leader>gb"] = {
-					name = "+blame",
-				},
-				["<leader>gd"] = {
-					name = "+diffview",
-				},
-			})
-		end,
-	},
+  -- Modify which-key keys
+  {
+    "folke/which-key.nvim",
+    opts = function()
+      require("which-key").register({
+        ["<leader>t"] = {
+          name = "+test",
+        },
+        ["<leader>gb"] = {
+          name = "+blame",
+        },
+        ["<leader>gd"] = {
+          name = "+diffview",
+        },
+        ["<leader>h"] = {
+          name = "+harpoon",
+        },
+      })
+    end,
+  },
 }
