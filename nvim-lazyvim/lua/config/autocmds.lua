7f17825b696bf3c7941742ea24395f21719453db
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-05-30 16:00:25 +0200

    fix: rulers

--- nvim-lazyvim/lua/config/autocmds.lua
+++ nvim-lazyvim/lua/config/autocmds.lua
@@ -5,29 +5,29 @@
 
 -- Python specific options
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "python" },
-	callback = function()
-		vim.g.python3_host_prog = vim.fn.expand(".venv/bin/python")
-		vim.opt_local.colorcolumn = "88" -- Ruler at column number
-		vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
-		vim.opt_local.shiftwidth = 4 -- Size of an indent
-	end,
+  pattern = { "python" },
+  callback = function()
+    vim.g.python3_host_prog = vim.fn.expand(".venv/bin/python")
+    vim.opt_local.colorcolumn = "72,88" -- Ruler at column number
+    vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
+    vim.opt_local.shiftwidth = 4 -- Size of an indent
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "rust" },
-	callback = function()
-		vim.opt_local.colorcolumn = "79" -- Ruler at column number
-		vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
-		vim.opt_local.shiftwidth = 4 -- Size of an indent
-	end,
+  pattern = { "rust" },
+  callback = function()
+    vim.opt_local.colorcolumn = "79" -- Ruler at column number
+    vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
+    vim.opt_local.shiftwidth = 4 -- Size of an indent
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "typescript",
-	callback = function()
-		vim.opt_local.colorcolumn = "79" -- Ruler at column number
-		vim.opt_local.tabstop = 4
-		vim.opt_local.shiftwidth = 4
-	end,
+  pattern = "typescript",
+  callback = function()
+    vim.opt_local.colorcolumn = "79" -- Ruler at column number
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
 })
