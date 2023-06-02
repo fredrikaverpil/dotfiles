8bcea99149c2c59bf3f7e5dfeb86bf1aa8b68be3
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-02 08:57:34 +0200

    fix: indendations

--- nvim-lazyvim/lua/plugins/telescope.lua
+++ nvim-lazyvim/lua/plugins/telescope.lua
@@ -1,45 +1,45 @@
 return {
 
-	-- change telescope config
-	{
-		"nvim-telescope/telescope.nvim",
+  -- change telescope config
+  {
+    "nvim-telescope/telescope.nvim",
 
-		dependencies = {
-			"nvim-telescope/telescope-live-grep-args.nvim",
-		},
-		config = function()
-			-- https://github.com/nvim-telescope/telescope-live-grep-args.nvim
-			-- Uses ripgrep args (rg) for live_grep
-			-- Command examples:
-			-- -i "Data"  # case insensitive
-			-- -g "!*.md" # ignore md files
-			-- -w # whole word
-			-- -e # regex
-			-- see 'man rg' for more
-			require("telescope").load_extension("live_grep_args")
-		end,
-		keys = {
-			{
-				"<leader>/",
-				function()
-					require("telescope.").extensions.live_grep_args.live_grep_args()
-				end,
-				desc = "Find Plugin File",
-			},
-		},
+    dependencies = {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+    },
+    config = function()
+      -- https://github.com/nvim-telescope/telescope-live-grep-args.nvim
+      -- Uses ripgrep args (rg) for live_grep
+      -- Command examples:
+      -- -i "Data"  # case insensitive
+      -- -g "!*.md" # ignore md files
+      -- -w # whole word
+      -- -e # regex
+      -- see 'man rg' for more
+      require("telescope").load_extension("live_grep_args")
+    end,
+    keys = {
+      {
+        "<leader>/",
+        function()
+          require("telescope.").extensions.live_grep_args.live_grep_args()
+        end,
+        desc = "Find Plugin File",
+      },
+    },
 
-		-- opts will be merged with the parent spec
-		opts = {
-			defaults = {
-				file_ignore_patterns = { "^.git/", "^node_modules/", "^poetry.lock" },
-			},
-			pickers = {
-				live_grep = {
-					additional_args = function()
-						return { "--hidden" }
-					end,
-				},
-			},
-		},
-	},
+    -- opts will be merged with the parent spec
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "^.git/", "^node_modules/", "^poetry.lock" },
+      },
+      pickers = {
+        live_grep = {
+          additional_args = function()
+            return { "--hidden" }
+          end,
+        },
+      },
+    },
+  },
 }
