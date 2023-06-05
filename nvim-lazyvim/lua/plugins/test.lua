bf02c0e62fe6a2ccf33df65085120bd9b9809b35
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-05 10:54:59 +0200

    chore: various updates

--- nvim-lazyvim/lua/plugins/test.lua
+++ nvim-lazyvim/lua/plugins/test.lua
@@ -9,8 +9,8 @@ return {
     end,
   },
 
-  -- neotest, also set up via lazy.lua
-  -- https://www.lazyvim.org/plugins/extras/test.core
+  -- neotest, also set up via lazy.lua.
+  -- go here for full config, including keymaps: https://www.lazyvim.org/plugins/extras/test.core
   {
     "nvim-neotest/neotest",
     dependencies = {
@@ -19,46 +19,49 @@ return {
       { "antoinemadec/FixCursorHold.nvim" },
 
       -- adapters
+      { "nvim-neotest/neotest-vim-test" },
       { "nvim-neotest/neotest-python" },
       { "rouge8/neotest-rust" },
       { "adrigzr/neotest-mocha" },
+      { "vim-test/vim-test" },
     },
 
-    -- keys = {
-    --   { "<leader>tt", ":lua require('neotest').run.run()<CR>", desc = "Run nearest test" },
-    --   { "<leader>tl", ":lua require('neotest').run.run_last()<CR>", desc = "Run last test" },
-    --   { "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand('%'))<CR>", desc = "Run tests in file" },
-    --   { "<leader>ts", ":lua require('neotest').summary.toggle()<CR>", desc = "Run test summary" },
-    --   { "<leader>to", ":lua require('neotest').output.open({ enter = true })<CR>", desc = "Run test output" },
-    --   { "<leader>tp", ":lua require('neotest').output_panel.toggle()<CR>", desc = "Run test output panel" },
-    --
-    --   -- debugging via nvim-dap, see debug.lua
-    --   { "<leader>td", ":lua require('neotest').run.run({ strategy = 'dap' })<CR>", desc = "Debug nearest test" },
-    -- },
-
     config = function()
-      -- Setup neotest adapter for python
       require("neotest").setup({
+
+        -- https://github.com/nvim-neotest/neotest-python
         adapters = {
           require("neotest-python")({
             runner = "pytest",
-            args = { "--log-level", "DEBUG", "--color", "yes" },
+            args = { "--log-level", "INFO", "--color", "yes", "-vv", "-s" },
           }),
 
           require("neotest-rust")({
+            -- https://github.com/rouge8/neotest-rust
+            --
             -- requires nextest, which can be installed via "cargo binstall:
             -- https://github.com/cargo-bins/cargo-binstall
             -- https://nexte.st/book/pre-built-binaries.html
             args = { "--no-capture" },
           }),
 
+          -- TODO: there is an issue where neotest-mocha attempts to run tests for Python:
+          -- https://github.com/adrigzr/neotest-mocha/issues/7
+          --
           -- require("neotest-mocha")({
+          --   -- https://github.com/adrigzr/neotest-mocha
+          --   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
           --   command = "npm test --",
           --   env = { CI = true },
           --   cwd = function(path)
           --     return vim.fn.getcwd()
           --   end,
           -- }),
+
+          require("neotest-vim-test")({
+            -- https://github.com/nvim-neotest/neotest-vim-test
+            ignore_file_types = { "python", "vim", "lua", "rust" },
+          }),
         },
       })
     end,
