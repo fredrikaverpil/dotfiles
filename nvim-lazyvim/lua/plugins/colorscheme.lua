bca0c9439808923d7730e68db6a4497875bdf38f
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-06 15:07:22 +0200

    feat: no need to repeat configs

--- nvim-lazyvim/lua/plugins/colorscheme.lua
+++ nvim-lazyvim/lua/plugins/colorscheme.lua
@@ -2,27 +2,13 @@ return {
 
   -- NOTE: also see treesitter.lua for languages with improved syntax highlighting
 
-  -- tokyonight, style: night
-  {
-    "folke/tokyonight.nvim",
-    -- opts will be merged with the parent spec
-    opts = {
-      -- style
-      style = "night", -- can be: storm, night, moon, day
-      -- transparent = true,
-      -- styles = {
-      --   sidebars = "transparent",
-      --   floats = "transparent",
-      -- },
-    },
-  },
-
   -- set the colorscheme
   {
     "LazyVim/LazyVim",
     -- lazy = false,
     opts = {
-      colorscheme = "tokyonight",
+      -- colorscheme = "tokyonight-night",
+      -- colorscheme = "catppuccin-mocha",
     },
   },
 }
