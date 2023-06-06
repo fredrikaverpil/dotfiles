31804c76131cc8f294286893a2b9d5f6948dcfad
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-06 14:09:54 +0200

    feat: add window picker

--- nvim-lazyvim/lua/plugins/filetree.lua
+++ nvim-lazyvim/lua/plugins/filetree.lua
@@ -18,4 +18,17 @@ return {
       },
     },
   },
+
+  {
+    "s1n7ax/nvim-window-picker",
+    dependencies = {
+      { "nvim-neo-tree/neo-tree.nvim" },
+    },
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
 }
