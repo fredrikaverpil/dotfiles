317331dbba656d3d2379d34a8f7bdc49f41a2387
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-06 15:16:12 +0200

    feat: use system to decide color scheme

--- wezterm.lua
+++ wezterm.lua
@@ -13,9 +13,26 @@ config.font = wezterm.font_with_fallback({
   { family = "Noto Color Emoji" },
 })
 
+-- colorschemes
 -- https://wezfurlong.org/wezterm/colorschemes/
--- config.color_scheme = "Catppuccin Mocha"
-config.color_scheme = "tokyonight_night"
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return "Dark"
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "tokyonight_moon"
+  else
+    return "tokyonight_day"
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 -- title bar
 config.window_decorations = "RESIZE"
