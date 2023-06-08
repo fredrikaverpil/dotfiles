79cce3668696e51f2671290a73cca6be71b6fab5
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-08 22:47:45 +0200

    feat: add scrolloff

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -3,13 +3,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- python colod column at black-default line length
--- vim.opt.colorcolumn = "88"
-
--- show symbols for whitespace
-vim.opt.listchars = "tab:▸ ,trail:·,nbsp:␣,extends:❯,precedes:❮"
-
--- disable swap files
--- vim.opt.swapfile = false
-
-vim.opt.relativenumber = false -- Relative line numbers
+vim.opt.listchars = "tab:▸ ,trail:·,nbsp:␣,extends:❯,precedes:❮" -- show symbols for whitespace
+vim.opt.relativenumber = false -- relative line numbers
+vim.opt.scrolloff = 20 -- keep 20 lines above and below the cursor
