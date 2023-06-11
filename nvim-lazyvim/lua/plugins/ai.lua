712eaf3b2df6e5ea49e3fa04de9c64c474636de8
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-11 16:27:24 +0200

    fix: use inline suggestions
    this will use the lazyvim copilot setup, but with inline suggestions enabled

--- nvim-lazyvim/lua/plugins/ai.lua
+++ nvim-lazyvim/lua/plugins/ai.lua
@@ -1,5 +1,36 @@
 return {
 
+  -- custom config which piggybacks on the copilot extras in lazy.lua.
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          accept = false, -- disable built-in keymapping
+        },
+      })
+
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      if cmp_status_ok then
+        cmp.event:on("menu_opened", function()
+          vim.b.copilot_suggestion_hidden = true
+        end)
+
+        cmp.event:on("menu_closed", function()
+          vim.b.copilot_suggestion_hidden = false
+        end)
+      end
+    end,
+  },
+
   -- https://github.com/jackMort/ChatGPT.nvim
   {
     "jackMort/ChatGPT.nvim",
@@ -8,7 +39,6 @@ return {
       { "nvim-lua/plenary.nvim" },
       { "nvim-telescope/telescope.nvim" },
     },
-
     config = function()
       require("chatgpt").setup({
         -- optional configuration
--- nvim-lazyvim/lua/plugins/cmp.lua
+++ nvim-lazyvim/lua/plugins/cmp.lua
@@ -12,33 +12,40 @@ return {
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      -- local has_words_before = function()
-      --   unpack = unpack or table.unpack
-      --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      -- end
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
       local luasnip = require("luasnip")
       local cmp = require("cmp")
 
-      -- opts.mapping = cmp.mapping.preset.insert(vim.tbl_deep_extend("force", opts.mapping, {
-      --   ["<CR>"] = vim.NIL,
-      -- }))
+      -- cmp.event:on("menu_opened", function()
+      --   vim.b.copilot_suggestion_hidden = true
+      -- end)
+      --
+      -- cmp.event:on("menu_closed", function()
+      --   vim.b.copilot_suggestion_hidden = false
+      -- end)
+
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<CR>"] = vim.NIL,
+
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- they way you will only jump inside the snippet region
+          elseif require("copilot.suggestion").is_visible() then
+            require("copilot.suggestion").accept()
           elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
-          -- elseif has_words_before() then
-          --   cmp.complete()
+          elseif has_words_before() then
+            cmp.complete()
           else
             fallback()
           end
         end, { "i", "s" }),
+
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
