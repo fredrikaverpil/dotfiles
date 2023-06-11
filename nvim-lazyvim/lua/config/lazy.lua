9798f019e51616870fc4541a222d41caff1b8f7c
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-11 16:28:21 +0200

    feat: add go support

--- nvim-lazyvim/lua/config/lazy.lua
+++ nvim-lazyvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
