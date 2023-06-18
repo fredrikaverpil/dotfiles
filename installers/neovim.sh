222d337b8e829f7536f7dfa9e87f23aae87ed252
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-18 10:31:01 +0200

    feat: add all the nvims

--- installers/neovim.sh
+++ installers/neovim.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -ex
 
-if [ ! -d "$HOME/.config/NvChad" ]; then
-    git clone https://github.com/NvChad/NvChad ~/.config/NvChad --depth 1
-fi
+# this will install so that nvim can be launched with NVIM_APPNAME, see aliases.sh
+#
+# see shell/bin/nvims how to start the different nvim distros
 
 if [ ! -d "$HOME/.config/LazyVim" ]; then
-    git clone --recursive https://github.com/LazyVim/starter.git ~/.config/LazyVim
+	git clone --recursive https://github.com/LazyVim/starter.git ~/.config/LazyVim
+fi
+
+if [ ! -d "$HOME/.config/NvChad" ]; then
+	git clone https://github.com/NvChad/NvChad ~/.config/NvChad --depth 1
+fi
+
+if [ ! -d "$HOME/.config/AstroNvim" ]; then
+	git clone https://github.com/AstroNvim/AstroNvim.git ~/.config/AstroNvim --depth 1
+fi
+
+if [ ! -d "$HOME/.config/kickstart" ]; then
+	git clone https://github.com/nvim-lua/kickstart.nvim.git ~/.config/kickstart | bash
 fi
