ed8881747d5712ce19bee049846bcf9192a4022a
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-18 10:52:23 +0200

    feat: add questions

--- installers/neovim.sh
+++ installers/neovim.sh
@@ -1,21 +1,34 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 # this will install so that nvim can be launched with NVIM_APPNAME, see aliases.sh
 #
 # see shell/bin/nvims how to start the different nvim distros
 
-if [ ! -d "$HOME/.config/LazyVim" ]; then
-	git clone --recursive https://github.com/LazyVim/starter.git ~/.config/LazyVim
+echo "About to remove default neovim dirs, continue? (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	rm -rf ~/.config/nvim
+	rm -rf ~/.local/share/nvim
+	rm -rf ~/.state/nvim
+	rm -rf ~/.cache/nvim
 fi
 
-if [ ! -d "$HOME/.config/NvChad" ]; then
-	git clone https://github.com/NvChad/NvChad ~/.config/NvChad --depth 1
-fi
+echo "About to install (git clone) neovim distros, continue? (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	if [ ! -d "$HOME/.config/LazyVim" ]; then
+		git clone --recursive https://github.com/LazyVim/starter.git ~/.config/LazyVim
+	fi
 
-if [ ! -d "$HOME/.config/AstroNvim" ]; then
-	git clone https://github.com/AstroNvim/AstroNvim.git ~/.config/AstroNvim --depth 1
-fi
+	if [ ! -d "$HOME/.config/NvChad" ]; then
+		git clone https://github.com/NvChad/NvChad ~/.config/NvChad --depth 1
+	fi
 
-if [ ! -d "$HOME/.config/kickstart" ]; then
-	git clone https://github.com/nvim-lua/kickstart.nvim.git ~/.config/kickstart | bash
+	if [ ! -d "$HOME/.config/AstroNvim" ]; then
+		git clone https://github.com/AstroNvim/AstroNvim.git ~/.config/AstroNvim --depth 1
+	fi
+
+	if [ ! -d "$HOME/.config/kickstart" ]; then
+		git clone https://github.com/nvim-lua/kickstart.nvim.git ~/.config/kickstart | bash
+	fi
 fi
