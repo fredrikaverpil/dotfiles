f9951e16f7c863249c38b9e6f33cff0fff2965c7
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-06-19 01:12:04 +0200

    chore: move to util script

---
+++ utils/public_repos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+
+# clone down all my public repos from github
+
+cd ~/code/repos
+GH_USER=fredrikaverpil
+PAGE=1
+curl "https://api.github.com/users/$GH_USER/repos?page=$PAGE&per_page=100" | grep -e 'clone_url*' | cut -d \" -f 4 | xargs -L1 git clone --recursive
