b32505e84b9ccd793e99971d2a9b399c16d4f096
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-07-01 12:11:25 +0200

    fix: finally fix the loading of sources
    lalalal

--- nvim-lazyvim/lua/plugins/lsp.lua
+++ nvim-lazyvim/lua/plugins/lsp.lua
@@ -9,11 +9,10 @@ return {
     -- opts will be merged with the parent spec
     opts = {
       ensure_installed = {
+
         -- python
         "ruff-lsp",
         "pyright",
-        -- "debugpy",
-        -- "mypy",
 
         -- lua
         "lua-language-server",
@@ -28,16 +27,12 @@ return {
         "dockerfile-language-server",
 
         -- javascript/typescript, see lazy.lua
-        -- "prettierd",
-        -- "typescript-language-server",
-        -- "eslint_d",
 
-        -- rust
+        -- rust, also see lazy.lua
+        "rust-analyzer", -- LSP
         "rustfmt",
-        "rust-analyzer",
 
-        -- go
-        -- handled by lazy.lua
+        -- go, see lazy.lua
       },
     },
   },
@@ -47,64 +42,45 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     dependencies = { "mason.nvim" },
     event = { "BufReadPre", "BufNewFile" },
-    opts = function()
+    opts = function(_, opts)
       local mason_registry = require("mason-registry")
+      mason_registry.refresh()
+
       local null_ls = require("null-ls")
       local formatting = null_ls.builtins.formatting
       local diagnostics = null_ls.builtins.diagnostics
       local code_actions = null_ls.builtins.code_actions
 
-      mason_registry.refresh()
+      -- null_ls.setup({
+      --   debug = false, -- Turn on debug for :NullLsLog
+      --   -- diagnostics_format = "#{m} #{s}[#{c}]",
+      -- })
 
-      null_ls.setup({
-        debug = false, -- Turn on debug for :NullLsLog
-        -- diagnostics_format = "#{m} #{s}[#{c}]",
-        sources = {
-          -- list of supported sources:
-          -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+      local sources = {
+        -- list of supported sources:
+        -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 
-          -- get from $PATH
-          -- diagnostics.ruff,
-          diagnostics.mypy,
-          formatting.black,
+        -- not installed via Mason, expected to be found on $PATH
+        diagnostics.mypy,
+        formatting.black,
 
-          -- get from mason
-          formatting.stylua.with({
-            command = mason_registry.get_package("stylua").path,
-            extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
-          }),
-          formatting.shfmt.with({
-            command = mason_registry.get_package("shfmt").path,
-          }),
-          -- diagnostics.eslint_d.with({
-          --   command = mason_registry.get_package("eslint_d").path,
-          -- }),
-          formatting.prettierd.with({
-            command = mason_registry.get_package("prettierd").path,
-          }),
-          formatting.rustfmt.with({
-            command = mason_registry.get_package("rustfmt").path,
-          }),
-          -- formatting.yamlfmt.with({
-          --   command = mason_registry.get_package("yamlfmt").path,
-          -- }),
-          formatting.yamlfix.with({
-            command = mason_registry.get_package("yamlfix").path, -- requires python
-          }),
+        -- installed via Mason
+        formatting.stylua.with({
+          extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+        }),
+        formatting.shfmt,
+        formatting.rustfmt,
+        formatting.yamlfix, -- requires python
+        diagnostics.yamllint,
+        diagnostics.shellcheck,
+        code_actions.shellcheck,
+        code_actions.gitsigns,
+      }
 
-          diagnostics.yamllint.with({
-            command = mason_registry.get_package("yamllint").path,
-          }),
-          diagnostics.shellcheck.with({
-            command = mason_registry.get_package("shellcheck").path,
-          }),
-
-          code_actions.shellcheck.with({
-            command = mason_registry.get_package("shellcheck").path,
-          }),
-          code_actions.gitsigns,
-        },
-      })
+      -- extend opts.sources
+      for _, source in ipairs(sources) do
+        table.insert(opts.sources, source)
+      end
     end,
   },
 }
