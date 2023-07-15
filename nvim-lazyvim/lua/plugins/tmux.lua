66db0a5ad57825401e2be7d90b5556052032e900
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-07-15 18:18:43 +0200

    fix: move between tmux/neovim windows

--- nvim-lazyvim/lua/plugins/tmux.lua
+++ nvim-lazyvim/lua/plugins/tmux.lua
@@ -1,7 +1,12 @@
 return {
-
-	-- vim tmux navigator
-	{
-		"christoomey/vim-tmux-navigator",
-	},
+  {
+    "numToStr/Navigator.nvim",
+    config = true,
+    keys = {
+      { "<C-h>", "<cmd>NavigatorLeft<cr>", desc = "Move left" },
+      { "<C-l", "<cmd>NavigatorRight<cr>", desc = "Move right" },
+      { "<C-j>", "<cmd>NavigatorDown<cr>", desc = "Move down" },
+      { "<C-k>", "<cmd>NavigatorUp<cr>", desc = "Move up" },
+    },
+  },
 }
