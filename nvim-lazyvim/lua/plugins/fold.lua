691757300179dd5a601c5ed073c6c92251ed660a
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-08-31 08:41:08 +0200

    feat: add ufo for nicer folding

---
+++ nvim-lazyvim/lua/plugins/fold.lua
@@ -0,0 +1,22 @@
+return {
+
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			vim.o.foldcolumn = "1" -- '0' is not bad
+			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end,
+			})
+		end,
+	},
+}
