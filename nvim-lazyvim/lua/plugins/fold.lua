129927a069fe6533f96cc4ef6a4f8bee76cbc74b
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-10-03 19:23:00 +0200

    feat: disable ufo, use tressiter-powered folding from lazyvim

--- nvim-lazyvim/lua/plugins/fold.lua
+++ nvim-lazyvim/lua/plugins/fold.lua
@@ -2,7 +2,7 @@ return {
 
   {
     "kevinhwang91/nvim-ufo",
-    enabled = true,
+    enabled = false, -- lazyvim supports this now out of the box
     dependencies = {
       "kevinhwang91/promise-async",
       "nvim-treesitter/nvim-treesitter",
