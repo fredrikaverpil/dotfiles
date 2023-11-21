6614786a1cd46c21be329bd093d45ff0bf9da5d7
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-11-21 09:22:28 +0100

    fix: provide tooling via brew where Mason provides the wrong arch

--- nvim-lazyvim/lua/plugins/lsp.lua
+++ nvim-lazyvim/lua/plugins/lsp.lua
@@ -72,10 +72,25 @@ return {
         "buf-language-server", -- lsp (prototype, not feature-complete yet, rely on buf for now)
         "buf", -- formatter, linter
         "protolint", -- linter
+
+        -- containers
+        -- "hadolint", -- linter
+        "dockerfile-language-server",
+        "docker-compose-language-service",
       }
 
+      -- extend opts.ensure_installed
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, ensure_installed)
+
+      -- remove from opts.ensure_installed
+      -- NOTE: this removes tooling because Mason provides a version
+      -- built for intel machines on macOS. Instead, these are provided
+      -- with the proper architecture via brew.
+      local ensure_not_installed = { "shellcheck", "hadolint" }
+      opts.ensure_installed = vim.tbl_filter(function(tool)
+        return not vim.tbl_contains(ensure_not_installed, tool)
+      end, opts.ensure_installed)
     end,
   },
 
