4c0359de6aa42574420466d3e4a2d03c05458db6
Fredrik Averpil <fredrik.averpil@gmail.com>
2023-12-28 15:02:41 +0100

    fix: improve removal of dupes

--- shell/exports.sh
+++ shell/exports.sh
@@ -6,13 +6,14 @@
 
 function add_to_path() {
 	# NOTE: zsh only
+
 	# usage:
 	# add_to_path prepend /path/to/prepend
 	# add_to_path append /path/to/append
-	if [ -d "$2" ]; then
 
-		if [[ ! ":$PATH:" =~ .*":$2:.*" ]]; then
-			# echo "Warning: $2 is already in \$PATH"
+	if [ -d "$2" ]; then
+		# If the given path exist, proceed...
+		if [[ ":$PATH:" == *":$2:"* ]]; then
 			remove_from_path "$2"
 		fi
 
@@ -30,10 +31,20 @@ function add_to_path() {
 
 function remove_from_path() {
 	# NOTE: zsh only
+
 	# usage:
 	# remove_from_path /path/to/remove
-	if [ -d "$1" ] && [[ ":$PATH:" =~ .*":$1:.*" ]]; then
-		PATH="${PATH//$1:/}"
+
+	local path_to_remove="$1"
+	if [[ -n "$path_to_remove" && ":$PATH:" == *":$path_to_remove:"* ]]; then
+		while [[ ":$PATH:" == *":$path_to_remove:"* ]]; do
+			# Remove
+			PATH="${PATH/#$path_to_remove:/}"   # If it's at the beginning
+			PATH="${PATH/%:$path_to_remove/}"   # If it's at the end
+			PATH="${PATH//:$path_to_remove:/:}" # If it's in the middle
+		done
+		PATH="${PATH#:}" # Remove leading colon
+		PATH="${PATH%:}" # Remove trailing colon
 		export PATH
 	fi
 }
@@ -83,9 +94,9 @@ add_to_path append "$HOME/go/bin"
 
 # NOTE: the last prepend appears first in $PATH, so make sure the order is correct below
 add_to_path prepend "$HOME/.tmux/plugins/t-smart-tmux-session-manager/bin"
-add_to_path prepend "$PYENV_ROOT/bin"  # pyenv
-add_to_path prepend "$HOME/.local/bin" # user-installed binaries
-add_to_path prepend "$DOTFILES/shell/bin"
+add_to_path prepend "$PYENV_ROOT/bin"     # pyenv
+add_to_path prepend "$HOME/.local/bin"    # user-installed binaries
+add_to_path prepend "$DOTFILES/shell/bin" # personal and custom scripts
 
 # load .env file if it exists
 # shellcheck disable=SC1090
