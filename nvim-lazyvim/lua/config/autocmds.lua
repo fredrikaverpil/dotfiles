4d4af880cbb51c273a554fdadc0ea22d82365d0a
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-01-04 07:54:29 +0100

    chore: format

--- nvim-lazyvim/lua/config/autocmds.lua
+++ nvim-lazyvim/lua/config/autocmds.lua
@@ -4,59 +4,52 @@
 -- Add any additional autocmds here
 
 local function find_python_executable()
-	if vim.env.VIRTUAL_ENV then
-		local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. "/**/bin/python", true, true)
-		local executable_path = table.concat(paths, ", ")
-		if executable_path ~= "" then
-			vim.api.nvim_echo({ { "Using path for python: " .. executable_path, "None" } }, false, {})
-			return executable_path
-		end
-	elseif vim.fn.filereadable(".venv/bin/python") == 1 then
-		local executable_path = vim.fn.expand(".venv/bin/python")
-		vim.api.nvim_echo({ { "Using path for python: " .. executable_path, "None" } }, false, {})
-		return executable_path
-	end
-	vim.api.nvim_echo({ { "No python executable found (see autocmds.lua)", "WarningMsg" } }, false, {})
+  if vim.env.VIRTUAL_ENV then
+    local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. "/**/bin/python", true, true)
+    local executable_path = table.concat(paths, ", ")
+    if executable_path ~= "" then
+      vim.api.nvim_echo({ { "Using path for python: " .. executable_path, "None" } }, false, {})
+      return executable_path
+    end
+  elseif vim.fn.filereadable(".venv/bin/python") == 1 then
+    local executable_path = vim.fn.expand(".venv/bin/python")
+    vim.api.nvim_echo({ { "Using path for python: " .. executable_path, "None" } }, false, {})
+    return executable_path
+  end
+  vim.api.nvim_echo({ { "No python executable found (see autocmds.lua)", "WarningMsg" } }, false, {})
 end
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "python" },
-	callback = function()
-		vim.g.python3_host_prog = find_python_executable() -- python executable
-		vim.opt_local.colorcolumn = "72,88" -- Ruler at column number
-		vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
-		vim.opt_local.shiftwidth = 4 -- Size of an indent
-	end,
+  pattern = { "python" },
+  callback = function()
+    vim.g.python3_host_prog = find_python_executable() -- python executable
+    vim.opt_local.colorcolumn = "72,88" -- Ruler at column number
+    vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
+    vim.opt_local.shiftwidth = 4 -- Size of an indent
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "go" },
-	callback = function()
-		vim.opt_local.colorcolumn = "120" -- Ruler at column number
-	end,
+  pattern = { "go" },
+  callback = function()
+    vim.opt_local.colorcolumn = "120" -- Ruler at column number
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "rust" },
-	callback = function()
-		vim.opt_local.colorcolumn = "79" -- Ruler at column number
-		vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
-		vim.opt_local.shiftwidth = 4 -- Size of an indent
-	end,
+  pattern = { "rust" },
+  callback = function()
+    vim.opt_local.colorcolumn = "79" -- Ruler at column number
+    vim.opt_local.tabstop = 4 -- Number of spaces tabs count for
+    vim.opt_local.shiftwidth = 4 -- Size of an indent
+  end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "typescript",
-	callback = function()
-		vim.opt_local.colorcolumn = "79" -- Ruler at column number
-		vim.opt_local.tabstop = 4
-		vim.opt_local.shiftwidth = 4
-	end,
+  pattern = "typescript",
+  callback = function()
+    vim.opt_local.colorcolumn = "79" -- Ruler at column number
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
 })
-
--- see lint.lua
--- vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
---   callback = function()
---     require("lint").try_lint()
---   end,
--- })
