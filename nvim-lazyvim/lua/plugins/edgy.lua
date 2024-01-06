0fc8d3bb44d3e9a16b45ea446f63b5612da42fb5
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-01-06 18:24:30 +0100

    feat: disable animations

---
+++ nvim-lazyvim/lua/plugins/edgy.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = "screen"
+    end,
+    opts = {
+
+      animate = {
+        enabled = false,
+        fps = 100, -- frames per second
+        cps = 120, -- cells per second
+        on_begin = function()
+          vim.g.minianimate_disable = true
+        end,
+        on_end = function()
+          vim.g.minianimate_disable = false
+        end,
+        -- Spinner for pinned views that are loading.
+        -- if you have noice.nvim installed, you can use any spinner from it, like:
+        -- spinner = require("noice.util.spinners").spinners.circleFull,
+        spinner = {
+          frames = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+          interval = 80,
+        },
+      },
+    },
+  },
+}
