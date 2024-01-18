243b17075d636369028163d4aea41d11b58ba74f
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-01-18 14:26:00 +0100

    fix: vimd-dadbod with vim-dadbod-completion

--- nvim-lazyvim/lua/plugins/db.lua
+++ nvim-lazyvim/lua/plugins/db.lua
@@ -1,14 +1,22 @@
 return {
 
-  -- breaks completion!
-
   {
     "tpope/vim-dadbod",
     dependencies = {
       { "kristijanhusak/vim-dadbod-ui" },
-      -- { "kristijanhusak/vim-dadbod-completion" },
+      { "kristijanhusak/vim-dadbod-completion" },
+      { "hrsh7th/nvim-cmp" }, -- see cmp.lua
     },
     config = function()
+      local cmp = require("cmp")
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "sql", "mysql", "plsql" },
+        callback = function()
+          cmp.setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+        end,
+      })
+
       vim.g.db_ui_save_location = "~/.local/share/db_ui"
     end,
   },
