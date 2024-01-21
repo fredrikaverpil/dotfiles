05e9177a60f5a00f2bfac2afaf7e1077819bd0c5
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-01-21 14:34:04 +0100

    fix: move files saved by dbui

--- nvim-lazyvim/lua/plugins/sql.lua
+++ nvim-lazyvim/lua/plugins/sql.lua
@@ -32,7 +32,8 @@ return {
     },
     cmd = { "DBUI", "DBUIFindBuffer" },
     config = function()
-      vim.g.db_ui_save_location = "~/.local/share/db_ui"
+      vim.g.db_ui_save_location = "~/code/dbui"
+      vim.g.db_ui_tmp_query_location = "~/code/queries"
       vim.g.db_ui_use_nerd_fonts = true
       vim.g.db_ui_execute_on_save = false
       vim.g.db_ui_use_nvim_notify = true
