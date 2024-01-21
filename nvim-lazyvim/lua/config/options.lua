a6d279128e9369562015384d26e597aa78df6d1a
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-01-21 10:27:31 +0100

    feat: add system clipboard sync on wsl

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -13,3 +13,16 @@ vim.opt.secure = true -- disable shell and write commands in local .nvim.lua .vi
 
 -- set autopairs disabled by default
 vim.g.minipairs_disable = true
+
+-- sync with system clipboard
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.has("wsl") == 1 then
+  vim.api.nvim_create_autocmd("TextYankPost", {
+
+    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+
+    callback = function()
+      vim.fn.system("clip.exe", vim.fn.getreg('"'))
+    end,
+  })
+end
