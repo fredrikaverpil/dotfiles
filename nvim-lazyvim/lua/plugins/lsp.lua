9a68eaa63ad86e522c5e8e85d8d4517191195111
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-01-23 21:20:27 +0100

    feat: update golangci-lint setup
    do not use lsp, instead use linter and auto-detect config.

--- nvim-lazyvim/lua/plugins/lsp.lua
+++ nvim-lazyvim/lua/plugins/lsp.lua
@@ -59,7 +59,6 @@ return {
         -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md
         -- https://github.com/golang/tools/blob/master/gopls/internal/lsp/source/options.go
         "gopls", -- lsp
-        "golangci-lint-langserver", -- lsp
         "gofumpt", -- formatter
         -- "goimports", -- formatter
         "gci", -- formatter, replaces goimports
@@ -149,6 +148,34 @@ return {
     enabled = true,
     opts = function(_, opts)
       local linters = require("lint").linters
+
+      local function find_file(filename)
+        -- find file
+        local command = "fd --hidden --no-ignore '" .. filename .. "' " .. vim.fn.getcwd() .. " | head -n 1"
+        local file = io.popen(command):read("*l")
+        return file and file or nil
+      end
+
+      local use_golangci_config_if_available = function()
+        local config_file = find_file(".golangci.yml")
+        if config_file then
+          print("Using golangci-lint config: " .. config_file)
+          return {
+            "run",
+            "--out-format",
+            "json",
+            "--config",
+            config_file,
+            function()
+              return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+            end,
+          }
+        else
+          return linters.golangcilint.args
+        end
+      end
+
+      linters.golangcilint.args = use_golangci_config_if_available()
       linters.mypy.cmd = prefer_bin_from_venv("mypy")
 
       local linters_by_ft = {
@@ -159,6 +186,7 @@ return {
         sh = { "shellcheck" },
         sql = { "sqlfluff" }, -- NOTE: https://github.com/mfussenegger/nvim-lint/blob/master/lua/lint/linters/sqlfluff.lua
         yaml = { "yamllint" },
+        go = { "golangcilint" },
       }
 
       -- extend opts.linters_by_ft
