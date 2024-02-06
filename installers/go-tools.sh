8daca4f7bc1b1bb1dabc11002ff6bf4962e8ba6c
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-02-06 23:18:49 +0100

    chore: add some tooling

--- installers/go-tools.sh
+++ installers/go-tools.sh
@@ -3,3 +3,5 @@
 go install golang.org/dl/gotip@latest
 go install golang.org/x/tools/cmd/godoc@latest
 go install github.com/lotusirous/gostdsym/stdsym@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install golang.org/x/tools/gopls@latest
