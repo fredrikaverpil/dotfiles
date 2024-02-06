f4575e2808e32a99ef949ccf2e7fe50677529fd7
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-02-06 23:18:40 +0100

    chore: increase tokens

--- nvim-lazyvim/lua/plugins/ai.lua
+++ nvim-lazyvim/lua/plugins/ai.lua
@@ -105,6 +105,7 @@ return {
         actions_paths = { "~/code/dotfiles/nvim-lazyvim/chatgpt-actions.json" },
         openai_params = {
           model = "gpt-4",
+          max_tokens = 4000,
         },
         openai_edit_params = {
           model = "gpt-3.5-turbo",
