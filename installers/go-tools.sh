bbb785b67c4fcb7108acd8dbfa8c7956b653f86c
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-02-08 11:32:12 +0100

    feat: add deadcode

--- installers/go-tools.sh
+++ installers/go-tools.sh
@@ -5,3 +5,4 @@ go install golang.org/x/tools/cmd/godoc@latest
 go install github.com/lotusirous/gostdsym/stdsym@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/cmd/deadcode@latest
