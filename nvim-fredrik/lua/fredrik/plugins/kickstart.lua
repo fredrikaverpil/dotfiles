deceaccd3d7af18e130f3eda196128f95aa90b41
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-02-19 23:06:19 +0100

    refactor: move lspconfig to its own file

--- nvim-fredrik/lua/fredrik/plugins/kickstart.lua
+++ nvim-fredrik/lua/fredrik/plugins/kickstart.lua
@@ -7,25 +7,6 @@ return {
   -- Detect tabstop and shiftwidth automatically
   "tpope/vim-sleuth",
 
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
-  {
-    -- LSP Configuration & Plugins
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { "j-hui/fidget.nvim", tag = "legacy", opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
-      "folke/neodev.nvim",
-    },
-  },
-
   {
     -- Autocompletion
     "hrsh7th/nvim-cmp",
