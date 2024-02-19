49557b99b7ce33457867fb7772f76dfbc4b571cf
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-02-19 20:43:05 +0100

    feat: move is_code_private into global variable

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -26,3 +26,19 @@ if vim.fn.has("wsl") == 1 then
     end,
   })
 end
+
+vim.g.is_code_private = function()
+  local current_dir = vim.fn.getcwd()
+  local home_dir = os.getenv("HOME") or os.getenv("USERPROFILE")
+  local code_path = home_dir .. "/code"
+
+  -- if git repo is filed under ~/code/work/private, do not allow AI
+  local private_path = code_path .. "/work/private"
+  local is_code_private = string.find(current_dir, private_path) == 1
+
+  if is_code_private then
+    return false
+  else
+    return true
+  end
+end
--- nvim-lazyvim/lua/plugins/ai.lua
+++ nvim-lazyvim/lua/plugins/ai.lua
@@ -1,25 +1,9 @@
-local enable_ai = function()
-  local current_dir = vim.fn.getcwd()
-  local home_dir = os.getenv("HOME") or os.getenv("USERPROFILE")
-  local code_path = home_dir .. "/code"
-
-  -- if git repo is filed under ~/code/work/private, do not allow AI
-  local private_path = code_path .. "/work/private"
-  local is_code_private = string.find(current_dir, private_path) == 1
-
-  if is_code_private then
-    return false
-  else
-    return true
-  end
-end
-
 return {
 
   -- custom config which piggybacks on the copilot extras in lazy.lua.
   {
     "zbirenbaum/copilot.lua",
-    enabled = enable_ai(),
+    enabled = vim.g.is_code_private(),
     cmd = "Copilot",
     build = ":Copilot auth",
     event = "InsertEnter",
