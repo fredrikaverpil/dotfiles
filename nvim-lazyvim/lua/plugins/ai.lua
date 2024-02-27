fd9c3df6e9f91a9b0f808172d6dfef02fe7d8da0
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-02-27 12:07:28 +0100

    feat: use oatmeal for ollama

--- nvim-lazyvim/lua/plugins/ai.lua
+++ nvim-lazyvim/lua/plugins/ai.lua
@@ -108,11 +108,20 @@ return {
     end,
   },
 
-  -- https://github.com/David-Kunz/gen.nvim
   {
-    "David-Kunz/gen.nvim",
-    config = function()
-      require("gen").model = "codellama"
-    end,
+    -- help:
+    -- /modellist
+    -- /model  <model name from model list>
+    -- /replace <number from code suggestion>
+    -- exit with CTRL+C
+    "dustinblackman/oatmeal.nvim",
+    cmd = { "Oatmeal" },
+    keys = {
+      { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
+    },
+    opts = {
+      backend = "ollama",
+      model = "codellama:latest",
+    },
   },
 }
