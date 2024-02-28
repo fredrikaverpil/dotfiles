f9ff0a237d110434a836fbaddee52a813f46cd61
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-02-28 20:24:01 +0100

    feat(go): golines

--- installers/go-tools.sh
+++ installers/go-tools.sh
@@ -7,3 +7,4 @@ go install github.com/go-delve/delve/cmd/dlv@latest
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/deadcode@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install github.com/segmentio/golines@latest
--- nvim-lazyvim/lua/plugins/lang_go.lua
+++ nvim-lazyvim/lua/plugins/lang_go.lua
@@ -134,11 +134,41 @@ return {
     "stevearc/conform.nvim",
     ft = { "go" },
     optional = true,
-    opts = {
-      formatters_by_ft = {
-        go = { "gofumpt", "goimports", "gci" },
-      },
-    },
+    opts = function(_, opts)
+      local formatters = require("conform.formatters")
+      print(vim.inspect(formatters.golines.args))
+      formatters.golines.args = { "-m", "120" }
+      local remove_from_formatters = {}
+      local extend_formatters_with = {}
+      local replace_formatters_with = {
+        go = { "gofumpt", "goimports", "gci", "golines" },
+      }
+
+      -- NOTE: conform.nvim can use a sub-list to run only the first available formatter (see docs)
+
+      -- remove from opts.formatters_by_ft
+      for ft, formatters_ in pairs(remove_from_formatters) do
+        opts.formatters_by_ft[ft] = vim.tbl_filter(function(formatter)
+          return not vim.tbl_contains(formatters_, formatter)
+        end, opts.formatters_by_ft[ft])
+      end
+      -- extend opts.formatters_by_ft
+      for ft, formatters_ in pairs(extend_formatters_with) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        vim.list_extend(opts.formatters_by_ft[ft], formatters_)
+      end
+      -- replace opts.formatters_by_ft
+      for ft, formatters_ in pairs(replace_formatters_with) do
+        opts.formatters_by_ft[ft] = formatters_
+      end
+
+      -- review opts.formatters_by_ft by uncommenting the below
+      vim.api.nvim_echo(
+        { { "opts.formatters_by_ft", "None" }, { vim.inspect(opts.formatters_by_ft), "None" } },
+        false,
+        {}
+      )
+    end,
   },
   {
     "mfussenegger/nvim-lint",
