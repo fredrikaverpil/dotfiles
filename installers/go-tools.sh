6c2de75090ceeaea82b4663be68392061c0367a0
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-02 09:38:50 +0100

    feat(go): add gofumpt

--- installers/go-tools.sh
+++ installers/go-tools.sh
@@ -5,6 +5,7 @@ go install golang.org/x/tools/cmd/godoc@latest
 go install github.com/lotusirous/gostdsym/stdsym@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install golang.org/x/tools/gopls@latest
+go install mvdan.cc/gofumpt@latest
 go install golang.org/x/tools/cmd/deadcode@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install github.com/segmentio/golines@latest
