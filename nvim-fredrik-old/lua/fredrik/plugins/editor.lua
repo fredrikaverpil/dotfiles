98382eeddad76cbf0340d7d8ea089a7f7b26fef4
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-03 16:52:43 +0100

    refactor(rename): custom neovim config backup

--- nvim-fredrik-old/lua/fredrik/plugins/editor.lua
+++ nvim-fredrik-old/lua/fredrik/plugins/editor.lua
@@ -8,6 +8,9 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      keymaps = {
+        { "n", "<leader>e", ":Neotree toggle<CR>" },
+      },
     },
   },
 }
