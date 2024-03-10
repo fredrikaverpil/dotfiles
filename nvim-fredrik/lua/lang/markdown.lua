f5951403de9db05cca7bb96d31b81b9dfcb27e4e
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-10 21:43:47 +0100

    feat(nvim-fredrik): add markdown

---
+++ nvim-fredrik/lua/lang/markdown.lua
@@ -0,0 +1,12 @@
+return {
+
+  -- install without yarn or npm
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+}
