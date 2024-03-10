fba11eac6d2c98ba3de79b365c2e0ad73995fc45
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-10 22:48:15 +0100

    feat(markdown): add markdownlint

--- nvim-fredrik/lua/lang/markdown.lua
+++ nvim-fredrik/lua/lang/markdown.lua
@@ -1,6 +1,43 @@
 return {
 
-  -- install without yarn or npm
+  {
+    "stevearc/conform.nvim",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "markdownlint" })
+        end,
+      },
+    },
+    ft = { "markdown" },
+    opts = {
+      formatters_by_ft = {
+        markdown = { "markdownlint" },
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "markdownlint" })
+        end,
+      },
+    },
+    ft = { "markdown" },
+    opts = {
+      linters_by_ft = {
+        markdown = { "markdownlint" },
+      },
+    },
+  },
+
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
