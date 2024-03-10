dad18f7ff943093fbff31eb84db3df36de07f66b
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-10 21:44:05 +0100

    fix(conform): perform auto-format on save

--- nvim-fredrik/lua/plugins/conform.lua
+++ nvim-fredrik/lua/plugins/conform.lua
@@ -1,10 +1,24 @@
 return {
   {
+    -- https://github.com/stevearc/conform.nvim
     "stevearc/conform.nvim",
     config = function(_, opts)
-      require("conform").setup({
-        formatters_by_ft = opts.formatters_by_ft,
-      })
+      -- TODO: add toggle keymap for aut-save on/off by leveraging a vim.g variable and:
+      -- vim.api.nvim_create_autocmd("BufWritePre", {
+      --   pattern = "*",
+      --   callback = function(args)
+      --     -- read global variable here...
+      --     require("conform").format({ bufnr = args.buf })
+      --   end,
+      -- })
+
+      opts.format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+      }
+
+      require("conform").setup(opts)
     end,
   },
 }
