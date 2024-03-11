1664d73294ff187dbed44937e7ab269052cd805e
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-11 17:30:14 +0100

    feat(nvim): use biome for json

--- nvim-fredrik/lua/lang/json.lua
+++ nvim-fredrik/lua/lang/json.lua
@@ -16,14 +16,33 @@ return {
         "williamboman/mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "prettier" })
+          vim.list_extend(opts.ensure_installed, { "biome" })
         end,
       },
     },
     ft = { "json", "jsonc", "json5" },
     opts = {
       formatters_by_ft = {
-        json = { "prettier" },
+        json = { "biome" },
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "biome" })
+        end,
+      },
+    },
+    ft = { "json", "jsonc", "json5" },
+    opts = {
+      linters_by_ft = {
+        json = { "biome" },
       },
     },
   },
