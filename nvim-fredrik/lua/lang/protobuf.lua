1ea56bde4fd142acc4672f8ba12e0583c5978e18
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-11 09:16:50 +0100

    feat(nvim): add protobuf settings

---
+++ nvim-fredrik/lua/lang/protobuf.lua
@@ -0,0 +1,64 @@
+return {
+
+  {
+    "stevearc/conform.nvim",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "buf" })
+        end,
+      },
+    },
+    ft = { "proto" },
+    opts = {
+      formatters_by_ft = {
+        proto = { "buf" },
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "buf" })
+        end,
+      },
+    },
+    ft = { "proto" },
+    opts = {
+      linters_by_ft = {
+        proto = { "buf" },
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+          {
+            "williamboman/mason.nvim",
+          },
+        },
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "bufls" })
+        end,
+      },
+    },
+    ft = { "proto" },
+    opts = {
+      servers = {
+        bufls = {},
+      },
+    },
+  },
+}
