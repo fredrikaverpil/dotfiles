522abfaf180edb985aaef9658fc0f47c80cb1b1a
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-11 07:53:46 +0100

    feat(options): sync with system clipboard

--- nvim-fredrik/lua/utils/options.lua
+++ nvim-fredrik/lua/utils/options.lua
@@ -1,7 +1,18 @@
 vim.g.mapleader = " "
 
-
-
 -- project specific settings (see lazyrc.lua for .lazy.lua support)
 vim.opt.exrc = true -- allow local .nvim.lua .vimrc .exrc files
 vim.opt.secure = true -- disable shell and write commands in local .nvim.lua .vimrc .exrc files
+
+-- sync with system clipboard
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.has("wsl") == 1 then
+  vim.api.nvim_create_autocmd("TextYankPost", {
+
+    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+
+    callback = function()
+      vim.fn.system("clip.exe", vim.fn.getreg('"'))
+    end,
+  })
+end
