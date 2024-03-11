ad24b8125d8d87a17c8d4ffffbc8ea032ac67fd7
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-11 11:56:35 +0100

    feat(nvim): dap setup

--- nvim-fredrik/lua/lang/go.lua
+++ nvim-fredrik/lua/lang/go.lua
@@ -140,4 +140,22 @@ return {
     },
   },
 
+  {
+    "mfussenegger/nvim-dap",
+    ft = { "go" },
+    optional = true,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "delve" })
+        end,
+      },
+      {
+        "leoluz/nvim-dap-go",
+        config = true,
+      },
+    },
+  },
 }
