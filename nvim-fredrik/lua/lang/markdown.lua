5290214b1976e4b58becbd4f520d665d445621c2
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-11 08:28:41 +0100

    refactor(nvim): move markdown preview to its own plugin

--- nvim-fredrik/lua/lang/markdown.lua
+++ nvim-fredrik/lua/lang/markdown.lua
@@ -37,13 +37,4 @@ return {
       },
     },
   },
-
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
 }
