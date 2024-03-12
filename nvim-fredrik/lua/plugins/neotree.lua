12f16ef018904410e882cf17bce66b893ffed893
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-12 22:28:32 +0100

    fix(nvim): neotree follow file

--- nvim-fredrik/lua/plugins/neotree.lua
+++ nvim-fredrik/lua/plugins/neotree.lua
@@ -7,16 +7,21 @@ return {
     "MunifTanjim/nui.nvim",
   },
   opts = {
+    -- sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    -- open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
     filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+
+      -- This will use the OS level file watchers to detect changes
+      -- instead of relying on nvim autocmd events.
+      use_libuv_file_watcher = true,
+
       filtered_items = {
         visible = true, -- when true, they will just be displayed differently than normal items
         hide_dotfiles = false,
         hide_gitignored = true,
       },
-
-      -- This will use the OS level file watchers to detect changes
-      -- instead of relying on nvim autocmd events.
-      use_libuv_file_watcher = true,
     },
   },
   config = function(_, opts)
