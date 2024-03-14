fc5fa8a6004e0ca627eb29597c383fde3bd38707
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-15 00:17:05 +0100

    fix(nvim): read neovim library in lua lsp

--- nvim-fredrik/lua/lang/lua.lua
+++ nvim-fredrik/lua/lang/lua.lua
@@ -38,8 +38,34 @@ return {
     ft = { "lua" },
     opts = {
       servers = {
-        -- run :LspInfo and hit tab for more info
         lua_ls = {
+          on_init = function(client)
+            -- run :LspInfo and hit tab for more info
+            local path = client.workspace_folders[1].name
+            if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+              return
+            end
+
+            client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+              runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+              },
+              -- Make the server aware of Neovim runtime files
+              workspace = {
+                checkThirdParty = false,
+                -- library = {
+                --   vim.env.VIMRUNTIME,
+                --   -- Depending on the usage, you might want to add additional paths here.
+                --   -- "${3rd}/luv/library"
+                --   -- "${3rd}/busted/library",
+                -- },
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+            })
+          end,
           settings = {
             Lua = {
               workspace = {
