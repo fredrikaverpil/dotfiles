5a74c4f2c00f9a2c2c5a2620deb53ca8aa1f2a1e
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-15 00:16:26 +0100

    fix(nvim): update python setup

--- nvim-fredrik/lua/lang/python.lua
+++ nvim-fredrik/lua/lang/python.lua
@@ -1,3 +1,28 @@
+-- https://www.lazyvim.org/plugins/lsp
+-- NOTE: don't forget to update treesitter for languages
+-- NOTE: see lazy.lua for extras that configure LSPs, formatters, linters and code actions.
+
+local function prefer_bin_from_venv(executable_name)
+  -- Return the path to the executable if $VIRTUAL_ENV is set and the binary exists somewhere beneath the $VIRTUAL_ENV path, otherwise get it from Mason
+  if vim.env.VIRTUAL_ENV then
+    local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. "/**/bin/" .. executable_name, true, true)
+    local executable_path = table.concat(paths, ", ")
+    if executable_path ~= "" then
+      vim.notify = require("notify")
+      vim.notify("Using path for " .. executable_name .. ": " .. executable_path, vim.log.levels.INFO)
+      return executable_path
+    end
+  end
+
+  -- NOTE: this can probably be removed, as mason is puttiing stuff on $PATH for us:
+  -- local mason_registry = require("mason-registry")
+  -- local mason_path = mason_registry.get_package(executable_name):get_install_path() .. "/venv/bin/" .. executable_name
+  -- vim.notify("Using path for " .. executable_name .. ": " .. mason_path, vim.log.levels.WARN)
+  -- return mason_path
+
+  return executable_name
+end
+
 return {
 
   {
@@ -7,16 +32,17 @@ return {
         "williamboman/mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "black", "isort" })
+          vim.list_extend(opts.ensure_installed, { "ruff" })
         end,
       },
     },
     ft = { "python" },
-    opts = {
-      formatters_by_ft = {
-        python = { "black", "isort" },
-      },
-    },
+    opts = function(_, opts)
+      opts.formatters_by_ft.python = { "ruff_format" }
+
+      local formatters = require("conform.formatters")
+      formatters.ruff_format.command = prefer_bin_from_venv("ruff")
+    end,
   },
 
   {
@@ -33,7 +59,12 @@ return {
     ft = { "python" },
     opts = {
       linters_by_ft = {
-        markdown = { "ruff", "mypy" },
+        python = { "mypy" },
+      },
+      linters = {
+        mypy = {
+          cmd = prefer_bin_from_venv("mypy"),
+        },
       },
     },
   },
@@ -50,7 +81,7 @@ return {
         },
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "pyright" })
+          vim.list_extend(opts.ensure_installed, { "pyright", "ruff_lsp" })
         end,
       },
     },
@@ -58,6 +89,14 @@ return {
     opts = {
       servers = {
         pyright = {},
+        ruff_lsp = {
+          on_attach = function(client, bufnr)
+            if client.name == "ruff_lsp" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end,
+        },
       },
     },
   },
