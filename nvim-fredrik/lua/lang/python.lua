9b13ed1e051ee7821148495d220e526f20bfd78c
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-14 18:31:01 +0100

    fix(nvim): until ruff is configured

--- nvim-fredrik/lua/lang/python.lua
+++ nvim-fredrik/lua/lang/python.lua
@@ -7,14 +7,14 @@ return {
         "williamboman/mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "black" })
+          vim.list_extend(opts.ensure_installed, { "black", "isort" })
         end,
       },
     },
     ft = { "python" },
     opts = {
       formatters_by_ft = {
-        python = { "black" },
+        python = { "black", "isort" },
       },
     },
   },
