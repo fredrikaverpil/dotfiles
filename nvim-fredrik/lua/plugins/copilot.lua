fb9407d4c8d9f3608702cde9757aca063cc1e97b
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-16 21:38:02 +0100

    fix(nvim): remove deprecated function

--- nvim-fredrik/lua/plugins/copilot.lua
+++ nvim-fredrik/lua/plugins/copilot.lua
@@ -12,7 +12,7 @@ return {
           end
 
           local function fgcolor(name)
-            local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name, link = false }) or vim.api.nvim_get_hl_by_name(name, true)
+            local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name, link = false })
             local fg = hl and (hl.fg or hl.foreground)
             return fg and { fg = string.format("#%06x", fg) } or nil
           end
