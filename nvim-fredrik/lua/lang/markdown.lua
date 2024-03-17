cbff60f2c14c515ea3bb8433a5478fec3cae3426
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-17 23:31:20 +0100

    fix(nvim): default to conceallevel 2

--- nvim-fredrik/lua/lang/markdown.lua
+++ nvim-fredrik/lua/lang/markdown.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   group = vim.api.nvim_create_augroup("markdown_conceal", { clear = true }),
   pattern = { "markdown" },
   callback = function()
-    vim.opt_local.conceallevel = 0
+    vim.opt_local.conceallevel = 2
   end,
 })
 
