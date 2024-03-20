6d898f74d63deb50541bc503787b55c0e784a0c3
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-20 09:35:34 +0100

    feat(gh): add actions cache extension

--- installers/gh_extensions.sh
+++ installers/gh_extensions.sh
@@ -5,3 +5,6 @@ gh extension install einride/gh-dependabot
 
 # https://github.com/dlvhdr/gh-dash
 gh extension install dlvhdr/gh-dash
+
+# https://github.com/actions/gh-actions-cache
+gh extension install actions/gh-actions-cache
