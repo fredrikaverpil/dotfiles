345c563bbb264945ce7e439fe224ba42e0723678
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-21 15:13:33 +0100

    feat(nvim): window picker

--- nvim-fredrik/lua/plugins/window_picker.lua
+++ nvim-fredrik/lua/plugins/window_picker.lua
@@ -4,8 +4,21 @@ return {
     name = "window-picker",
     event = "VeryLazy",
     version = "2.*",
-    config = function()
-      require("window-picker").setup()
+    opts = {
+      filter_rules = {
+        include_current_win = false,
+        autoselect_one = true,
+        -- filter using buffer options
+        bo = {
+          -- if the file type is one of following, the window will be ignored
+          filetype = { "neo-tree", "neo-tree-popup", "notify" },
+          -- if the buffer type is one of following, the window will be ignored
+          buftype = { "terminal", "quickfix" },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("window-picker").setup(opts)
     end,
   },
 }
