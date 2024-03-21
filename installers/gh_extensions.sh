89b3cdb2f27876e2bae6cb0d2b8be595b6ab2a77
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-21 20:48:27 +0100

    refactor(copilot-cli): use official one

--- installers/gh_extensions.sh
+++ installers/gh_extensions.sh
@@ -6,3 +6,5 @@ gh extension install einride/gh-dependabot
 # https://github.com/dlvhdr/gh-dash
 gh extension install dlvhdr/gh-dash
 
+# https://docs.github.com/en/copilot/github-copilot-in-the-cli
+gh extension install github/gh-copilot
