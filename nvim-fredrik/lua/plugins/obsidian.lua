d001de84e5127c937131d98bbce3f379aac4be32
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-24 14:50:18 +0100

    fix(nvim): foldername

--- nvim-fredrik/lua/plugins/obsidian.lua
+++ nvim-fredrik/lua/plugins/obsidian.lua
@@ -58,7 +58,7 @@ return {
     },
 
     templates = {
-      subdir = "templates",
+      subdir = "Templates",
       date_format = "%Y-%m-%d",
       time_format = "%H:%M",
       -- A map for custom variables, the key should be the variable and the value a function
