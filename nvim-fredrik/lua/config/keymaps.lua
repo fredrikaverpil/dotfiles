f8a14df21ae089e5e2f709bb9b16f8a2182ef10f
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-25 11:51:27 +0100

    refactor(nvim): keymaps for diffview

--- nvim-fredrik/lua/config/keymaps.lua
+++ nvim-fredrik/lua/config/keymaps.lua
@@ -533,4 +533,23 @@ function M.setup_yanky_keymaps()
   end, "Yanky history")
 end
 
+function M.setup_diffview_keymaps()
+  return {
+    -- use [c and [c to navigate diffs (vim built in), see :h jumpto-diffs
+    -- use ]x and [x to navigate conflicts
+    { "<leader>gdc", ":DiffviewOpen origin/main...HEAD", desc = "Compare commits" },
+    { "<leader>gdd", ":DiffviewClose<CR>", desc = "Close Diffview tab" },
+    { "<leader>gdh", ":DiffviewFileHistory %<CR>", desc = "File history" },
+    { "<leader>gdH", ":DiffviewFileHistory<CR>", desc = "Repo history" },
+    { "<leader>gdm", ":DiffviewOpen<CR>", desc = "Solve merge conflicts" },
+    { "<leader>gdo", ":DiffviewOpen main", desc = "DiffviewOpen" },
+    { "<leader>gdp", ":DiffviewOpen origin/main...HEAD --imply-local", desc = "Review current PR" },
+    {
+      "<leader>gdP",
+      ":DiffviewFileHistory --range=origin/main...HEAD --right-only --no-merges --reverse",
+      desc = "Review current PR (per commit)",
+    },
+  }
+end
+
 return M
--- nvim-fredrik/lua/plugins/diffview.lua
+++ nvim-fredrik/lua/plugins/diffview.lua
@@ -34,21 +34,6 @@ return {
         },
       })
     end,
-    keys = {
-      -- use [c and [c to navigate diffs (vim built in), see :h jumpto-diffs
-      -- use ]x and [x to navigate conflicts
-      { "<leader>gdc", ":DiffviewOpen origin/main...HEAD", desc = "Compare commits" },
-      { "<leader>gdd", ":DiffviewClose<CR>", desc = "Close Diffview tab" },
-      { "<leader>gdh", ":DiffviewFileHistory %<CR>", desc = "File history" },
-      { "<leader>gdH", ":DiffviewFileHistory<CR>", desc = "Repo history" },
-      { "<leader>gdm", ":DiffviewOpen<CR>", desc = "Solve merge conflicts" },
-      { "<leader>gdo", ":DiffviewOpen main", desc = "DiffviewOpen" },
-      { "<leader>gdp", ":DiffviewOpen origin/main...HEAD --imply-local", desc = "Review current PR" },
-      {
-        "<leader>gdP",
-        ":DiffviewFileHistory --range=origin/main...HEAD --right-only --no-merges",
-        desc = "Review current PR (per commit)",
-      },
-    },
+    keys = require("config.keymaps").setup_diffview_keymaps(),
   },
 }
