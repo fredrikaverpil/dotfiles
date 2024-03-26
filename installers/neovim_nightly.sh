ebf6c87e024e6d48fff9dc271969c06bbda96fbb
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-26 22:42:16 +0100

    fix(nvim): download script, support macos-arm64

--- installers/neovim_nightly.sh
+++ installers/neovim_nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-curl -L -o ~/Downloads/nvim-macos.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+curl -L -o ~/Downloads/nvim-macos.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
 rm -rf ~/nvim-nightly
 tar -xvf ~/Downloads/nvim-macos.tar.gz -C ~/
-mv ~/nvim-macos ~/nvim-nightly
+mv ~/nvim-macos-arm64 ~/nvim-nightly
