0f8699b113ec2f959d2545ca23cd9463b8b0a5f5
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-04-22 08:53:30 +0200

    feat(nvim): use tab to move in snippet args

--- nvim-fredrik/lua/plugins/cmp.lua
+++ nvim-fredrik/lua/plugins/cmp.lua
@@ -14,6 +14,31 @@ return {
               "saadparwaiz1/cmp_luasnip",
               "rafamadriz/friendly-snippets",
             },
+            keys = {
+              {
+                "<tab>",
+                function()
+                  return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+                end,
+                expr = true,
+                silent = true,
+                mode = "i",
+              },
+              {
+                "<tab>",
+                function()
+                  require("luasnip").jump(1)
+                end,
+                mode = "s",
+              },
+              {
+                "<s-tab>",
+                function()
+                  require("luasnip").jump(-1)
+                end,
+                mode = { "i", "s" },
+              },
+            },
           },
         },
       },
