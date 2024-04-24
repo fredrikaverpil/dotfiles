df798054c9649428721d383537e3339f799816aa
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-04-24 21:20:22 +0200

    feat(nvim): add projects

--- nvim-fredrik/lua/plugins/telescope.lua
+++ nvim-fredrik/lua/plugins/telescope.lua
@@ -16,6 +16,55 @@ return {
         "nvim-telescope/telescope-live-grep-args.nvim",
       },
       { "smartpde/telescope-recent-files" },
+      {
+        "ahmedkhalf/project.nvim",
+        opts = {
+          {
+            -- Manual mode doesn't automatically change your root directory, so you have
+            -- the option to manually do so using `:ProjectRoot` command.
+            manual_mode = true,
+
+            -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+            -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+            -- order matters: if one is not detected, the other is used as fallback. You
+            -- can also delete or rearangne the detection methods.
+            detection_methods = { "lsp", "pattern" },
+
+            -- All the patterns used to detect root dir, when **"pattern"** is in
+            -- detection_methods
+            patterns = { ".git" },
+
+            -- Table of lsp clients to ignore by name
+            -- eg: { "efm", ... }
+            ignore_lsp = {},
+
+            -- Don't calculate root dir on specific directories
+            -- Ex: { "~/.cargo/*", ... }
+            exclude_dirs = {},
+
+            -- Show hidden files in telescope
+            show_hidden = true,
+
+            -- When set to false, you will get a message when project.nvim changes your
+            -- directory.
+            silent_chdir = false,
+
+            -- What scope to change the directory, valid options are
+            -- * global (default)
+            -- * tab
+            -- * win
+            scope_chdir = "win",
+
+            -- Path where project.nvim will store the project history for use in
+            -- telescope
+            datapath = vim.fn.stdpath("data"),
+          },
+        },
+        event = "VeryLazy",
+        config = function(_, opts)
+          require("project_nvim").setup(opts)
+        end,
+      },
       { "rcarriga/nvim-notify" },
       { "folke/trouble.nvim" }, -- for trouble.sources.telescope
     },
@@ -68,6 +117,7 @@ return {
       telescope.load_extension("live_grep_args")
       telescope.load_extension("ui-select")
       telescope.load_extension("recent_files")
+      telescope.load_extension("projects")
       telescope.load_extension("notify")
 
       require("config.keymaps").setup_telescope_keymaps()
