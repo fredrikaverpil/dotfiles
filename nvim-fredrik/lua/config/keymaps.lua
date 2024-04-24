a258b8c7c77711e3e14a6f865d50e83df9679b31
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-04-24 21:59:42 +0200

    feat(nvim): cmp keymaps

--- nvim-fredrik/lua/config/keymaps.lua
+++ nvim-fredrik/lua/config/keymaps.lua
@@ -177,11 +177,14 @@ end
 
 function M.setup_cmp_keymaps(cmp)
   return {
+    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }
 end
 
