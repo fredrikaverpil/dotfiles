136d42f2fa8357d6f895b9585c95c0614529a58a
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-04-30 07:16:52 +0200

    feat: use neovim nightly from brew

--- installers/neovim_nightly.sh
+++ installers/neovim_nightly.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -e
 
-curl -L -o ~/Downloads/nvim-macos.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz
-rm -rf ~/nvim-nightly
-tar -xvf ~/Downloads/nvim-macos.tar.gz -C ~/
-mv ~/nvim-macos-arm64 ~/nvim-nightly
+brew unlink neovim
+brew install nvim --HEAD
