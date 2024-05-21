34f49c917ecbcb5c1fd05e0b27b5f67b0d9b2ea4
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-05-21 08:05:15 +0200

    fix(nvim): use default branch

--- nvim-fredrik/lua/plugins/neogit.lua
+++ nvim-fredrik/lua/plugins/neogit.lua
@@ -1,17 +1,7 @@
-local function get_branch()
-  if require("utils.version").is_neovim_0_10_0() then
-    -- https://github.com/NeogitOrg/neogit/tree/nightly
-    return "nightly"
-  else
-    return "master"
-  end
-end
-
 return {
 
   {
     "NeogitOrg/neogit",
-    branch = get_branch(),
     event = "VeryLazy",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
