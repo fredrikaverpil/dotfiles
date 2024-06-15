cccba0e85fa2c41f2d4a489945f488ff19850973
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-06-16 00:09:00 +0200

    fix(nvim): add missing deps for rest.nvim

--- nvim-fredrik/lua/plugins/rest.lua
+++ nvim-fredrik/lua/plugins/rest.lua
@@ -3,6 +3,14 @@ return {
   {
     "rest-nvim/rest.nvim",
     dependencies = {
+      {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+        opts = {
+          rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+        },
+      },
       "nvim-lua/plenary.nvim",
     },
     ft = { "http" },
