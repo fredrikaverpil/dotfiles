aad785e7e129e553b1a374584f830aaf514df2c1
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-06-19 07:46:28 +0200

    fix(nvim-git-blame): disable at startup

--- nvim-fredrik/lua/plugins/git_blame.lua
+++ nvim-fredrik/lua/plugins/git_blame.lua
@@ -2,6 +2,9 @@ return {
   {
     "f-person/git-blame.nvim",
     event = "VeryLazy",
+    config = function()
+      vim.cmd(":GitBlameToggle") -- disable at startup
+    end,
     keys = require("config.keymaps").setup_git_blame_keymaps(),
   },
 }
