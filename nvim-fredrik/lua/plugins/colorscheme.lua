ea8620d31822cc2101092248f07c15c04732f36c
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-06-23 12:59:00 +0200

    chore(nvim-colorschemes): set dayfox as light theme

--- nvim-fredrik/lua/plugins/colorscheme.lua
+++ nvim-fredrik/lua/plugins/colorscheme.lua
@@ -1,42 +1,52 @@
+local function set_dark()
+  vim.api.nvim_set_option_value("background", "dark", {})
+  vim.cmd.colorscheme("tokyonight-moon")
+end
+
+local function set_light()
+  vim.api.nvim_set_option_value("background", "light", {})
+  vim.cmd.colorscheme("dayfox")
+end
+
 return {
   {
     "folke/tokyonight.nvim",
+    enabled = true,
     lazy = false,
     priority = 1000,
     init = function()
-      -- NOTE: avoid flickering when starting Neovim (in dark mode)
-      vim.cmd.colorscheme("tokyonight-moon")
+      set_dark() -- avoid flickering when starting nvim
     end,
   },
-
   {
     "catppuccin/nvim",
-    enabled = false,
-    name = "catppuccin",
+    enabled = true,
+    name = "catppuccin", -- or Lazy will show the plugin as "nvim"
   },
-
   {
     "rose-pine/neovim",
-    enabled = false,
-    name = "rose-pine",
+    enabled = true,
+    name = "rose-pine", -- or Lazy will show the plugin as "neovim"
   },
-
+  {
+    "EdenEast/nightfox.nvim",
+    enabled = true,
+  },
   {
     "f-person/auto-dark-mode.nvim",
+    enabled = true,
     lazy = false,
     priority = 1000,
     dependencies = {},
-    config = {
+    opts = {
       update_interval = 3000, -- milliseconds
       set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd.colorscheme("tokyonight-moon")
+        set_dark()
         os.execute("tmux source-file ~/.tmux/plugins/tokyonight.nvim/extras/tmux/tokyonight_moon.tmux")
         os.execute("cp $DOTFILES/lazygit_config_dark.yml $DOTFILES/lazygit_config.yml")
       end,
       set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
-        vim.cmd.colorscheme("tokyonight-day")
+        set_light()
         os.execute("tmux source-file ~/.tmux/plugins/tokyonight.nvim/extras/tmux/tokyonight_day.tmux")
         os.execute("cp $DOTFILES/lazygit_config_light.yml $DOTFILES/lazygit_config.yml")
       end,
