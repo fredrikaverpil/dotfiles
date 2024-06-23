9be0d8251c5b2dfe2c3f5ebae0a9361b2e64f3be
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-06-23 10:48:48 +0200

    fix(nvim): nio as dependency

--- nvim-fredrik/lua/plugins/neotest.lua
+++ nvim-fredrik/lua/plugins/neotest.lua
@@ -3,14 +3,13 @@ return {
     "nvim-neotest/neotest",
     event = "VeryLazy",
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
 
       "nvim-neotest/neotest-plenary",
       "nvim-neotest/neotest-vim-test",
-
-      "nvim-neotest/nvim-nio",
 
       {
         "echasnovski/mini.indentscope",
