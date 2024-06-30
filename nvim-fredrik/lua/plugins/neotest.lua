249960685df4d88c73d401abbbdbe16c1af17fed
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-06-30 21:18:16 +0200

    chore(nvim): tweak comments

--- nvim-fredrik/lua/plugins/neotest.lua
+++ nvim-fredrik/lua/plugins/neotest.lua
@@ -24,18 +24,20 @@ return {
     opts = {
       -- See all config options with :h neotest.Config
       discovery = {
-        -- Drastically improve performance in ginormous projects.
+        -- Drastically improve performance in ginormous projects by
+        -- only AST-parsing the currently opened buffer.
         enabled = false,
-        -- Number of workers to parse files concurrently. 0
-        -- automatically assigns number based on CPU. Set to 1 if experiencing lag.
+        -- Number of workers to parse files concurrently.
+        -- A value of 0 automatically assigns number based on CPU.
+        -- Set to 1 if experiencing lag.
         concurrent = 0,
       },
       running = {
-        -- Run tests concurrently when an adapter provides multiple commands to run
+        -- Run tests concurrently when an adapter provides multiple commands to run.
         concurrent = false,
       },
       summary = {
-        -- Enable/disable animation of icons
+        -- Enable/disable animation of icons.
         animated = false,
       },
     },
