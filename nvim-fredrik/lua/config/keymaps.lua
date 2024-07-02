23f9f480afa2c932759b929752d65cfdc9e229c8
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-07-02 16:47:51 +0200

    chore(nvim): update keymap for goto type def

--- nvim-fredrik/lua/config/keymaps.lua
+++ nvim-fredrik/lua/config/keymaps.lua
@@ -151,7 +151,7 @@ function M.setup_lsp_autocmd_keymaps(event)
   -- Jump to the type of the word under your cursor.
   --  Useful when you're not sure what type a variable is and you want to see
   --  the definition of its *type*, not where it was *defined*.
-  map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+  map("gt", require("telescope.builtin").lsp_type_definitions, "[G]oto [t]ype definition")
 
   -- Fuzzy find all the symbols in your current document.
   --  Symbols are things like variables, functions, types, etc.
