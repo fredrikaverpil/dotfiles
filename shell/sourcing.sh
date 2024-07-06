b749fc8fb4b1317bd73f26130aac0c6a4e2a2990
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-07-06 09:58:19 +0200

    feat(shell): add fzf shell extension

--- shell/sourcing.sh
+++ shell/sourcing.sh
@@ -6,57 +6,56 @@
 # ----------------------------
 
 function virtual_env_activate() {
-	if [[ -z "$VIRTUAL_ENV" ]]; then
-		# if .venv folder is found then activate the vitualenv
-		if [ -d ./.venv ] && [ -f ./.venv/bin/activate ]; then
-			source ./.venv/bin/activate
-		fi
-	else
-		# check the current folder belong to earlier VIRTUAL_ENV folder
-		parentdir="$(dirname "$VIRTUAL_ENV")"
-		if [[ "$PWD"/ != "$parentdir"/* ]]; then
-			deactivate
-		fi
-	fi
+  if [[ -z "$VIRTUAL_ENV" ]]; then
+    # if .venv folder is found then activate the vitualenv
+    if [ -d ./.venv ] && [ -f ./.venv/bin/activate ]; then
+      source ./.venv/bin/activate
+    fi
+  else
+    # check the current folder belong to earlier VIRTUAL_ENV folder
+    parentdir="$(dirname "$VIRTUAL_ENV")"
+    if [[ "$PWD"/ != "$parentdir"/* ]]; then
+      deactivate
+    fi
+  fi
 }
 
 function node_version_manager() {
-	if [[ -z "$NVMRC_PATH" ]]; then
-		if [ -f .nvmrc ]; then
-			nvm use
-			export NVMRC_PATH=$PWD/.nvmrc
-		fi
-	else
-		parent_nvmdir="$(dirname "$NVMRC_PATH")"
-		if [[ "$PWD"/ != "$parent_nvmdir"/* ]]; then
-			nvm deactivate
-			export NVMRC_PATH=""
-		fi
-	fi
+  if [[ -z "$NVMRC_PATH" ]]; then
+    if [ -f .nvmrc ]; then
+      nvm use
+      export NVMRC_PATH=$PWD/.nvmrc
+    fi
+  else
+    parent_nvmdir="$(dirname "$NVMRC_PATH")"
+    if [[ "$PWD"/ != "$parent_nvmdir"/* ]]; then
+      nvm deactivate
+      export NVMRC_PATH=""
+    fi
+  fi
 }
 
 function zsh_completion() {
+  if [ -n "$brew_prefix" ]; then
+    export FPATH=$brew_prefix/share/zsh/site-functions:$FPATH
 
-	if [ -n "$brew_prefix" ]; then
-		export FPATH=$brew_prefix/share/zsh/site-functions:$FPATH
+    source "$brew_prefix/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+    source "$brew_prefix/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+  fi
 
-		source "$brew_prefix/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-		source "$brew_prefix/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-	fi
+  if [ -f "$brew_prefix/share/google-cloud-sdk" ]; then
+    source "$brew_prefix/share/google-cloud-sdk/path.zsh.inc"
+    source "$brew_prefix/share/google-cloud-sdk/completion.zsh.inc"
+  fi
 
-	if [ -f "$brew_prefix/share/google-cloud-sdk" ]; then
-		source "$brew_prefix/share/google-cloud-sdk/path.zsh.inc"
-		source "$brew_prefix/share/google-cloud-sdk/completion.zsh.inc"
-	fi
+  export FPATH=$DOTFILES/work/zsh/site-functions:$FPATH
 
-	export FPATH=$DOTFILES/work/zsh/site-functions:$FPATH
+  # Makefile completion
+  zstyle ':completion:*:*:make:*' tag-order 'targets'
+  zstyle ':completion:*:make:*:targets' call-command true
 
-	# Makefile completion
-	zstyle ':completion:*:*:make:*' tag-order 'targets'
-	zstyle ':completion:*:make:*:targets' call-command true
-
-	autoload -Uz compinit
-	compinit
+  autoload -Uz compinit
+  compinit
 }
 
 # ----------------------------
@@ -71,17 +70,21 @@ shell="$DOTFILES_SHELL"
 # ----------------------------------
 
 if [[ $shell == "zsh" ]]; then
-	if [ -n "$brew_prefix" ]; then
-		source <(pkgx --shellcode)
-	elif [[ $shell == "bash" ]]; then
-		eval "$(pkgx --shellcode)"
-	fi
+  if [ -n "$brew_prefix" ]; then
+    source <(fzf --zsh)
+    source <(pkgx --shellcode)
+  fi
+elif [[ $shell == "bash" ]]; then
+  if [ -n "$brew_prefix" ]; then
+    eval "$(fzf --bash)"
+    eval "$(pkgx --shellcode)"
+  fi
 fi
 
 function cd() {
-	builtin cd "$@" || return
-	virtual_env_activate
-	# node_version_manager  # TODO: with pkgx, maybe nvm is no longer needed?
+  builtin cd "$@" || return
+  virtual_env_activate
+  # node_version_manager  # TODO: with pkgx, maybe nvm is no longer needed?
 }
 cd . # trigger cd overrides
 
@@ -90,17 +93,17 @@ cd . # trigger cd overrides
 # ----------------------------
 
 if [ -f ~/.cargo/env ]; then
-	source "$HOME/.cargo/env"
+  source "$HOME/.cargo/env"
 fi
 
 if [ -n "$brew_prefix" ]; then
-	# TODO: evaluate whether pkgx can replace nvm
-	# source "$brew_prefix/opt/nvm/nvm.sh"
+  # TODO: evaluate whether pkgx can replace nvm
+  # source "$brew_prefix/opt/nvm/nvm.sh"
 
-	eval "$(atuin init $shell --disable-up-arrow)"
-	eval "$(direnv hook $shell)"
-	eval "$(zoxide init $shell)"
-	eval "$(starship init $shell)"
+  eval "$(atuin init $shell --disable-up-arrow)"
+  eval "$(direnv hook $shell)"
+  eval "$(zoxide init $shell)"
+  eval "$(starship init $shell)"
 
 fi
 
@@ -110,12 +113,12 @@ fi
 
 if [[ $shell == "zsh" ]]; then
 
-	zsh_completion
+  zsh_completion
 
 elif [[ $shell == "bash" ]]; then
-	if [ -f "$brew_prefix/share/google-cloud-sdk" ]; then
-		source "$brew_prefix/share/google-cloud-sdk/path.bash.inc"
-		source "$brew_prefix/share/google-cloud-sdk/completion.bash.inc"
-	fi
+  if [ -f "$brew_prefix/share/google-cloud-sdk" ]; then
+    source "$brew_prefix/share/google-cloud-sdk/path.bash.inc"
+    source "$brew_prefix/share/google-cloud-sdk/completion.bash.inc"
+  fi
 
 fi
