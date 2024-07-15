2293e1d5734a5b7bc2f69cacb5fa6dccbd668aec
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-07-15 23:47:28 +0200

    chore(nvim): no hjkl yet

--- nvim-fredrik/lua/plugins/learn-neovim-the-hard-way.lua
+++ nvim-fredrik/lua/plugins/learn-neovim-the-hard-way.lua
@@ -2,7 +2,7 @@ return {
 
   {
     "m4xshen/hardtime.nvim",
-    enabled = true,
+    enabled = false,
     event = "VeryLazy",
     dependencies = {
       "MunifTanjim/nui.nvim",
@@ -13,33 +13,8 @@ return {
 
   {
     "tris203/precognition.nvim",
-    enabled = true,
+    enabled = false,
     event = "VeryLazy",
-    opts = {
-      -- startVisible = true,
-      -- showBlankVirtLine = true,
-      -- highlightColor = { link = "Comment" },
-      -- hints = {
-      --      Caret = { text = "^", prio = 2 },
-      --      Dollar = { text = "$", prio = 1 },
-      --      MatchingPair = { text = "%", prio = 5 },
-      --      Zero = { text = "0", prio = 1 },
-      --      w = { text = "w", prio = 10 },
-      --      b = { text = "b", prio = 9 },
-      --      e = { text = "e", prio = 8 },
-      --      W = { text = "W", prio = 7 },
-      --      B = { text = "B", prio = 6 },
-      --      E = { text = "E", prio = 5 },
-      -- },
-      -- gutterHints = {
-      --     G = { text = "G", prio = 10 },
-      --     gg = { text = "gg", prio = 9 },
-      --     PrevParagraph = { text = "{", prio = 8 },
-      --     NextParagraph = { text = "}", prio = 8 },
-      -- },
-      -- disabled_fts = {
-      --     "startify",
-      -- },
-    },
+    opts = {},
   },
 }
