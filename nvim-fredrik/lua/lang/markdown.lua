c35c789546ab8e8a57b5bf66ba6fd7ab1680a7ea
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-08-02 20:27:28 +0200

    refactor(nvim): markdown-preview

--- nvim-fredrik/lua/lang/markdown.lua
+++ nvim-fredrik/lua/lang/markdown.lua
@@ -56,4 +56,13 @@ return {
       },
     },
   },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
