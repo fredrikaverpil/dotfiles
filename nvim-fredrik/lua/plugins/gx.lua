9fcae65c557e1d6418f889605e5f7b1c6d3be0cc
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-08-02 20:53:54 +0200

    chore(nvim): cleanup/reorder

--- nvim-fredrik/lua/plugins/gx.lua
+++ nvim-fredrik/lua/plugins/gx.lua
@@ -3,12 +3,12 @@ return {
   {
     "chrishrb/gx.nvim",
     event = "VeryLazy",
-    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    dependencies = { "nvim-lua/plenary.nvim" },
     cmd = { "Browse" },
     -- init = function()
     --   vim.g.netrw_nogx = 1 -- disable netrw gx
     -- end,
-    dependencies = { "nvim-lua/plenary.nvim" },
     config = true, -- default settings
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
   },
 }
