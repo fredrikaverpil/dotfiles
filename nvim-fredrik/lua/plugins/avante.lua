30b191e65b4e3ad610f3dfb377cac59395cadcb3
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-08-29 09:29:52 +0200

    feat(nvim): avante

---
+++ nvim-fredrik/lua/plugins/avante.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "yetone/avante.nvim",
+    enabled = require("utils.private").enable_ai(),
+    build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+    opts = {
+      claude = {
+        api_key_name = "cmd:op read op://Personal/Anthropic/tokens/neovim --no-newline",
+      },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+    cmd = { "AvanteAsk", "AvanteChat", "AvanteEdit", "AvanteToggle", "AvanteClear", "AvanteFocus", "AvanteRefresh", "AvanteSwitchProvider" },
+    keys = require("config.keymaps").setup_avante_keymaps(),
+  },
+}
