7e0efb916e6d66e398456097ac5c30d48cd1d673
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-09-14 13:33:08 +0200

    fix(macos-defaults): bump InitialKeyrepeat for bluetooth latency

--- _macos/set_defaults.sh
+++ _macos/set_defaults.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -ex
 
+# Based on
+# https://github.com/kevinSuttle/macOS-Defaults/blob/master/.macos
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
 # Set a blazingly fast keyboard repeat rate
+# The defaults for a freshly installed macOS Sierra 10.12.5 (16F73) are: KeyRepeat = 6 and InitialKeyRepeat = 25.
 defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
