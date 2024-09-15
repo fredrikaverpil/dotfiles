69fd463c85858930a2cb4863bf4607562e5d3b93
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-09-15 22:06:14 +0200

    feat(nvim): sync up go lsp settings with gopls capabilities

--- nvim-fredrik/lua/lang/go.lua
+++ nvim-fredrik/lua/lang/go.lua
@@ -158,15 +158,84 @@ return {
 
           settings = {
 
+            -- NOTE: this is not an explicit list. The gopls defaults will apply if not overridden here.
             gopls = {
               analyses = {
-                fieldalignment = false, -- annoying
+                -- https://github.com/golang/tools/blob/master/gopls/internal/settings/analysis.go
+                -- https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
+
+                -- the traditional vet suite
+                appends = true,
+                asmdecl = true,
+                assign = true,
+                atomic = true,
+                bools = true,
+                buildtag = true,
+                cgocall = true,
+                composite = true,
+                copylock = true,
+                defers = true,
+                deprecated = true,
+                directive = true,
+                errorsas = true,
+                framepointer = true,
+                httpresponse = true,
+                ifaceassert = true,
+                loopclosure = true,
+                lostcancel = true,
+                nilfunc = true,
+                printf = true,
+                shift = true,
+                sigchanyzer = true,
+                slog = true,
+                stdmethods = true,
+                stdversion = true,
+                stringintconv = true,
+                structtag = true,
+                testinggoroutine = true,
+                tests = true,
+                timeformat = true,
+                unmarshal = true,
+                unreachable = true,
+                unsafeptr = true,
+                unusedresult = true,
+
+                -- not suitable for vet:
+                -- - some (nilness) use go/ssa; see #59714.
+                -- - others don't meet the "frequency" criterion;
+                --   see GOROOT/src/cmd/vet/README.
+                atomicalign = true,
+                deepequalerrors = true,
                 nilness = true,
+                sortslice = true,
+                embeddirective = true,
+
+                -- disabled due to high false positives
+                shadow = false,
+                useany = false,
+                -- fieldalignment = false, -- annoying and also  NOTE: deprecated in gopls v0.17.0
+
+                -- "simplifiers": analyzers that offer mere style fixes
+                -- gofmt -s suite:
+                simplifycompositelit = true,
+                simplifyrange = true,
+                simplifyslice = true,
+                -- other simplifiers:
+                infertypeargs = true,
                 unusedparams = true,
                 unusedwrite = true,
-                useany = true,
+
+                -- type-error analyzers
+                -- These analyzers enrich go/types errors with suggested fixes.
+                fillreturns = true,
+                nonewvars = true,
+                noresultvalues = true,
+                stubmethods = true,
+                undeclaredname = true,
+                unusedvariable = true,
               },
               codelenses = {
+                -- https://github.com/golang/tools/blob/master/gopls/internal/settings/settings.go
                 gc_details = false,
                 generate = true,
                 regenerate_cgo = true,
@@ -177,6 +246,7 @@ return {
                 vendor = true,
               },
               hints = {
+                -- https://github.com/golang/tools/blob/master/gopls/internal/settings/settings.go
                 assignVariableTypes = true,
                 compositeLiteralFields = true,
                 compositeLiteralTypes = true,
@@ -185,12 +255,26 @@ return {
                 parameterNames = true,
                 rangeVariableTypes = true,
               },
-              completeUnimported = true,
-              directoryFilters = { "-**/node_modules", "-**/.git", "-.vscode", "-.idea", "-.vscode-test" },
-              gofumpt = true,
-              semanticTokens = true,
-              staticcheck = true,
+              -- completion options
+              -- https://github.com/golang/tools/blob/master/gopls/internal/settings/settings.go
               usePlaceholders = true,
+              completeUnimported = true,
+              experimentalPostfixCompletions = true,
+              completeFunctionCalls = true,
+              -- build options
+              -- https://github.com/golang/tools/blob/master/gopls/internal/settings/settings.go
+              directoryFilters = { "-**/node_modules", "-**/.git", "-.vscode", "-.idea", "-.vscode-test" },
+              -- formatting options
+              -- https://github.com/golang/tools/blob/master/gopls/internal/settings/settings.go
+              gofumpt = false, -- handled by conform instead.
+              -- ui options
+              -- https://github.com/golang/tools/blob/master/gopls/internal/settings/settings.go
+              semanticTokens = false, -- disabling this enables treesitter injections (for sql, json etc)
+              -- diagnostic options
+              -- https://github.com/golang/tools/blob/master/gopls/internal/settings/settings.go
+              staticcheck = true,
+              vulncheck = "imports",
+              analysisProgressReporting = true,
             },
           },
         },
