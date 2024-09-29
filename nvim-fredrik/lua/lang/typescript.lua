4f2eb3d9b6cf5e5c4e8c82396f6364acfb568d0a
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-09-29 14:32:59 +0200

    feat(ts_ls): rename lsp due to chaing in lspconfig

--- nvim-fredrik/lua/lang/typescript.lua
+++ nvim-fredrik/lua/lang/typescript.lua
@@ -12,7 +12,7 @@ return {
         },
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "tsserver", "vtsls" })
+          vim.list_extend(opts.ensure_installed, { "ts_ls", "vtsls" })
         end,
       },
       {
