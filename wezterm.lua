d8b969655ed3515b71ff452a6efc15f0c63e2648
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-10-19 12:23:28 +0200

    feat(wezterm): add custom tab titles

--- wezterm.lua
+++ wezterm.lua
@@ -74,6 +74,44 @@ config.text_background_opacity = 1.0 -- 0.9
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 
+local function get_current_working_dir(tab)
+  local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = "" }
+  local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+  return current_dir == HOME_DIR and "." or string.gsub(current_dir.file_path, "(.*[/\\])(.*)", "%2")
+end
+
+-- tab title
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local has_unseen_output = false
+  if not tab.is_active then
+    for _, pane in ipairs(tab.panes) do
+      if pane.has_unseen_output then
+        has_unseen_output = true
+        break
+      end
+    end
+  end
+
+  local cwd = wezterm.format({
+    { Attribute = { Intensity = "Bold" } },
+    { Text = get_current_working_dir(tab) },
+  })
+
+  local title = string.format(" [%s] %s", tab.tab_index + 1, cwd)
+
+  if has_unseen_output then
+    return {
+      { Foreground = { Color = "#8866bb" } },
+      { Text = title },
+    }
+  end
+
+  return {
+    { Text = title },
+  }
+end)
+
 -- ssh hosts from ~./ssh/config
 local ssh_domains = {}
 for host, config_ in pairs(wezterm.enumerate_ssh_hosts()) do
