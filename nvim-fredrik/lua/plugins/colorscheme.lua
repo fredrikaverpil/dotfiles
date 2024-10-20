20b6a7dd5d2e045801453e3e1ab4c10499e326f9
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-10-20 20:59:32 +0200

    fix(nvim): only run 'tmux source-file' if tmux is running

--- nvim-fredrik/lua/plugins/colorscheme.lua
+++ nvim-fredrik/lua/plugins/colorscheme.lua
@@ -6,6 +6,19 @@ local function set_light()
   vim.cmd.colorscheme("dayfox")
 end
 
+local function tmux_is_running()
+  local processes = vim.fn.systemlist("ps -e | grep tmux")
+  local found = false
+  for _, process in ipairs(processes) do
+    if string.find(process, "grep") then
+      -- do nothing, just skip
+    elseif string.find(process, "tmux") then
+      found = true
+    end
+  end
+  return found
+end
+
 return {
   {
     "folke/tokyonight.nvim",
@@ -47,14 +60,18 @@ return {
       update_interval = 3000, -- milliseconds
       set_dark_mode = function()
         set_dark()
-        os.execute("tmux source-file ~/.tmux/plugins/tokyonight.nvim/extras/tmux/tokyonight_moon.tmux")
+        if tmux_is_running() then
+          os.execute("tmux source-file ~/.tmux/plugins/tokyonight.nvim/extras/tmux/tokyonight_moon.tmux")
+        end
         vim.g.lazygit_use_custom_config_file_path = 1
         local dark_theme = vim.fs.normalize("$DOTFILES/lazygit_config_dark.yml")
         vim.g.lazygit_config_file_path = dark_theme
       end,
       set_light_mode = function()
         set_light()
-        os.execute("tmux source-file ~/.local/share/fredrik/lazy/nightfox.nvim/extra/dayfox/dayfox.tmux")
+        if tmux_is_running() then
+          os.execute("tmux source-file ~/.local/share/fredrik/lazy/nightfox.nvim/extra/dayfox/dayfox.tmux")
+        end
         vim.g.lazygit_use_custom_config_file_path = 1
         local light_theme = vim.fs.normalize("$DOTFILES/lazygit_config_light.yml")
         vim.g.lazygit_config_file_path = light_theme
