02d668aa01d5be752e7d2f48af9c9ebe7665a7b0
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-11-14 14:44:14 +0100

    refactor(nvim): define syntax highlighting in lua instead

--- nvim-fredrik/ftdetect/ghostty.lua
+++
@@ -1,8 +0,0 @@
-vim.filetype.add({
-  extension = {
-    ghostty = "ghostty", -- For files ending in .ghostty
-  },
-  pattern = {
-    [".*/ghostty%.conf"] = "ghostty", -- For ghostty.conf files
-  },
-})
