5f1d3b44cf24d5b0bd7d165d259e1b46c420fe08
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-11-17 13:00:55 +0100

    feat(nvim): add mason lock

--- nvim-fredrik/lua/plugins/mason.lua
+++ nvim-fredrik/lua/plugins/mason.lua
@@ -1,20 +1,30 @@
 return {
+  {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonUpdate", "MasonUninstall" },
+    config = function(_, opts)
+      require("mason").setup(opts)
 
-  "williamboman/mason.nvim",
-  cmd = { "Mason", "MasonInstall", "MasonUpdate", "MasonUninstall" },
-  config = function(_, opts)
-    require("mason").setup(opts)
-
-    -- handle opts.ensure_installed
-    local registry = require("mason-registry")
-    registry.refresh(function()
-      for _, pkg_name in ipairs(opts.ensure_installed) do
-        -- print("loading " .. pkg_name)
-        local pkg = registry.get_package(pkg_name)
-        if not pkg:is_installed() then
-          pkg:install()
+      -- handle opts.ensure_installed
+      local registry = require("mason-registry")
+      registry.refresh(function()
+        for _, pkg_name in ipairs(opts.ensure_installed) do
+          -- print("loading " .. pkg_name)
+          local pkg = registry.get_package(pkg_name)
+          if not pkg:is_installed() then
+            pkg:install()
+          end
         end
-      end
-    end)
-  end,
+      end)
+    end,
+  },
+
+  {
+    "zapling/mason-lock.nvim",
+    init = function()
+      require("mason-lock").setup({
+        lockfile_path = os.getenv("DOTFILES") .. "/nvim-fredrik/mason-lock.json",
+      })
+    end,
+  },
 }
