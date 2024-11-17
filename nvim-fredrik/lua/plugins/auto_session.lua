7536cf9c809c5fb5c8f66406dcb8e37c22418979
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-11-17 13:10:56 +0100

    feat(nvim): use auto-session instead of persistence

---
+++ nvim-fredrik/lua/plugins/auto_session.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/code", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
+    config = function(_, opts)
+      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+      require("auto-session").setup(opts)
+    end,
+  },
+}
--- nvim-fredrik/lua/plugins/persistence.lua
+++ nvim-fredrik/lua/plugins/persistence.lua
@@ -1,11 +1,14 @@
--- restore only some things from the last session, to avoid restoring e.g. blank buffers
-vim.opt.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,terminal"
-
 return {
 
   {
     "folke/persistence.nvim",
+    enabled = false,
     event = "BufReadPre", -- this will only start session saving when an actual file was opened
     opts = {},
+    config = function(_, opts)
+      -- restore only some things from the last session, to avoid restoring e.g. blank buffers
+      vim.opt.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,terminal"
+      require("persistence").setup(opts)
+    end,
   },
 }
