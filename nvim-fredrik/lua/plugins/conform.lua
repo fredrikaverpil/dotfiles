30075fb61af40e06ab24e11299baa62ceca2e646
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-11-20 22:46:42 +0100

    chore(nvim): remove todo

--- nvim-fredrik/lua/plugins/conform.lua
+++ nvim-fredrik/lua/plugins/conform.lua
@@ -4,7 +4,6 @@ return {
     "stevearc/conform.nvim",
     event = "BufWritePre",
     config = function(_, opts)
-      -- TODO: add toggle keymap for auto-save on/off by leveraging a vim.g variable and:
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = "*",
         callback = function(args)
