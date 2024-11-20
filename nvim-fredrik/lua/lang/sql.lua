3ee8c71c3e378eb414931714f480d5ad5ac505b9
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-11-20 18:36:04 +0100

    feat(nvim): use blink for sql completion with dadbod

--- nvim-fredrik/lua/lang/sql.lua
+++ nvim-fredrik/lua/lang/sql.lua
@@ -18,31 +18,23 @@ return {
       vim.g.db_ui_use_nerd_fonts = true
       vim.g.db_ui_execute_on_save = false
       vim.g.db_ui_use_nvim_notify = true
-
-      local autocomplete_group = vim.api.nvim_create_augroup("vimrc_autocompletion", { clear = true })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = sql_ft,
-        callback = function()
-          local cmp = require("cmp")
-          local global_sources = cmp.get_config().sources
-          local buffer_sources = {}
-
-          -- add globally defined sources (see separate nvim-cmp config)
-          -- this makes e.g. luasnip snippets available since luasnip is configured globally
-          if global_sources then
-            for _, source in ipairs(global_sources) do
-              table.insert(buffer_sources, { name = source.name })
-            end
-          end
-
-          -- add vim-dadbod-completion source
-          table.insert(buffer_sources, { name = "vim-dadbod-completion" })
-
-          -- update sources for the current buffer
-          cmp.setup.buffer({ sources = buffer_sources })
-        end,
-        group = autocomplete_group,
-      })
     end,
+  },
+
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        completion = {
+          enabled_providers = { "dadbod" },
+        },
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
+    },
+    dependencies = {
+      "kristijanhusak/vim-dadbod-completion",
+    },
   },
 }
