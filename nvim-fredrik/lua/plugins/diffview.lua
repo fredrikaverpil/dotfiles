3f0b160afda4d200db9c4b2e3a15ced77c724a83
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-11-21 11:22:27 +0100

    fix(nvim): change diff character

--- nvim-fredrik/lua/plugins/diffview.lua
+++ nvim-fredrik/lua/plugins/diffview.lua
@@ -7,32 +7,40 @@ return {
       { "nvim-lua/plenary.nvim" },
       { "nvim-tree/nvim-web-devicons" },
     },
-    config = function()
+
+    opts = {
+
+      -- file_panel = {
+      --   win_config = {
+      --     position = "bottom",
+      --   },
+      -- },
+
+      default = {
+        disable_diagnostics = false,
+      },
+      view = {
+        merge_tool = {
+          disable_diagnostics = false,
+          winbar_info = true,
+        },
+      },
+      enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+      hooks = {
+        -- do not fold
+        diff_buf_win_enter = function(bufnr)
+          vim.opt_local.foldenable = false
+        end,
+
+        -- TODO: jump to first diff: https://github.com/sindrets/diffview.nvim/issues/440
+        -- TODO: enable diagnostics in diffview
+      },
+    },
+
+    config = function(_, opts)
       local actions = require("diffview.actions")
 
-      require("diffview").setup({
-        -- file_panel = {
-        --   win_config = {
-        --     position = "bottom",
-        --   },
-        -- },
-        view = {
-          merge_tool = {
-            disable_diagnostics = false,
-            winbar_info = true,
-          },
-        },
-        enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
-        hooks = {
-          -- do not fold
-          diff_buf_win_enter = function(bufnr)
-            vim.opt_local.foldenable = false
-          end,
-
-          -- TODO: jump to first diff: https://github.com/sindrets/diffview.nvim/issues/440
-          -- TODO: enable diagnostics in diffview
-        },
-      })
+      require("diffview").setup(opts)
     end,
     keys = require("config.keymaps").setup_diffview_keymaps(),
   },
