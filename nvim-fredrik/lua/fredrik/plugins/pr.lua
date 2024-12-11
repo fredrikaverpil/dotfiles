b893158f6d408c4db091377ec4ce76c04970d68f
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-11 21:39:09 +0100

    fix(nvim): use cli to get token for pr.nvim

--- nvim-fredrik/lua/fredrik/plugins/pr.lua
+++ nvim-fredrik/lua/fredrik/plugins/pr.lua
@@ -1,20 +1,34 @@
 return {
   {
     "fredrikaverpil/pr.nvim",
-    lazy = false,
+    lazy = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
     dir = "~/code/public/pr.nvim",
-    opts = {},
+
+    ---@type PR.Config
+    opts = {
+      token = function()
+        local cmd = { "op", "read", "op://Personal/github.com/tokens/pr.nvim", "--no-newline" }
+        local obj = vim.system(cmd, { text = true }):wait()
+        if obj.code ~= 0 then
+          vim.notify("Failed to get token from 1Password", vim.log.levels.ERROR)
+          return nil
+        end
+        return obj.stdout
+      end,
+    },
+
     keys = {
       {
-        "<leader>o",
+        "<leader>gbp",
         function()
           require("pr").open()
         end,
-        desc = "Open PR",
+        desc = "View PR in browser",
       },
     },
+    cmd = { "PRView" },
   },
 }
