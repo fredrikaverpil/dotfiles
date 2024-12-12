9ba7c17364646a4593fac38673a37e7b22670492
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-12 12:36:16 +0100

    chore(nvim): remove opts from pr.nvim

--- nvim-fredrik/lua/fredrik/plugins/pr.lua
+++ nvim-fredrik/lua/fredrik/plugins/pr.lua
@@ -8,17 +8,17 @@ return {
     dir = "~/code/public/pr.nvim",
 
     ---@type PR.Config
-    -- opts = {
-    --   github_token = function()
-    --     local cmd = { "op", "read", "op://Personal/github.com/tokens/pr.nvim", "--no-newline" }
-    --     local obj = vim.system(cmd, { text = true }):wait()
-    --     if obj.code ~= 0 then
-    --       vim.notify("Failed to get token from 1Password", vim.log.levels.ERROR)
-    --       return nil
-    --     end
-    --     return obj.stdout
-    --   end,
-    -- },
+    opts = {
+      --   github_token = function()
+      --     local cmd = { "op", "read", "op://Personal/github.com/tokens/pr.nvim", "--no-newline" }
+      --     local obj = vim.system(cmd, { text = true }):wait()
+      --     if obj.code ~= 0 then
+      --       vim.notify("Failed to get token from 1Password", vim.log.levels.ERROR)
+      --       return nil
+      --     end
+      --     return obj.stdout
+      --   end,
+    },
 
     keys = {
       {
