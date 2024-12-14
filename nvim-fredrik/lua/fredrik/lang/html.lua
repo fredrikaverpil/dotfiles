8bb227d0f1790a8872ec4a972a2845241287d71d
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-14 20:53:59 +0100

    feat(nvim): add html

---
+++ nvim-fredrik/lua/fredrik/lang/html.lua
@@ -0,0 +1,42 @@
+return {
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = true,
+    ft = { "html" },
+    opts = {
+      ensure_installed = { "html" },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    ft = { "html", "templ" },
+    dependencies = {
+      {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+          {
+            "williamboman/mason.nvim",
+          },
+        },
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "html-lsp" })
+        end,
+      },
+    },
+    opts = {
+      servers = {
+        -- https://github.com/microsoft/vscode-html-languageservice
+        html = {
+          filetypes = { "html" },
+          settings = {
+            html = {},
+          },
+        },
+      },
+    },
+  },
+}
