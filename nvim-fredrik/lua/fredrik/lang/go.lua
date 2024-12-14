310a737fd2cc8c30192bab0f4e6f5c7dfb5f1c32
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-14 16:53:26 +0100

    fix(nvim): get bufnr from callback args

--- nvim-fredrik/lua/fredrik/lang/go.lua
+++ nvim-fredrik/lua/fredrik/lang/go.lua
@@ -10,10 +10,9 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
   pattern = "*.gohtml,*.gotmpl",
-  callback = function()
+  callback = function(args)
     if vim.fn.search("{{.\\+}}", "nw") ~= 0 then
-      local buf = vim.api.nvim_get_current_buf()
-      vim.api.nvim_set_option_value("filetype", "gotmpl", { buf = buf })
+      vim.api.nvim_set_option_value("filetype", "gotmpl", { buf = args.buf })
     end
   end,
 })
