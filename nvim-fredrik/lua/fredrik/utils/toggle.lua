092e7410f510b66a2ebc0163f9ecf06dc5a74433
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-14 21:48:01 +0100

    feat(nvim): toggle lsp-provided formatting

--- nvim-fredrik/lua/fredrik/utils/toggle.lua
+++ nvim-fredrik/lua/fredrik/utils/toggle.lua
@@ -1,12 +1,20 @@
 M = {}
 
+local orig_fmt_func = vim.lsp.handlers["textDocument/formatting"]
+
 function M.toggle_formatting()
   vim.g.auto_format = not vim.g.auto_format -- reverse the value
 
   if vim.g.auto_format then
-    print("Auto-formatting enabled", vim.log.levels.INFO)
+    vim.lsp.handlers["textDocument/formatting"] = orig_fmt_func
   else
-    print("Auto-formatting disabled", vim.log.levels.INFO)
+    vim.lsp.handlers["textDocument/formatting"] = function() end
+  end
+
+  if vim.g.auto_format then
+    vim.notify("Auto-formatting enabled", vim.log.levels.INFO)
+  else
+    vim.notify("Auto-formatting disabled", vim.log.levels.INFO)
   end
 end
 
