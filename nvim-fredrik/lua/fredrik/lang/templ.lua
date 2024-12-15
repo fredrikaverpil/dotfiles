33adda32b6d4c2cdd81159efd3d6e4465681f569
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-15 07:04:15 +0100

    feat(nvim): add templ support

---
+++ nvim-fredrik/lua/fredrik/lang/templ.lua
@@ -0,0 +1,84 @@
+local filetypes = { "templ" }
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = filetypes,
+  callback = function()
+    -- set go specific options
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.colorcolumn = "120"
+  end,
+})
+
+return {
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = true,
+    ft = filetypes,
+    opts = {
+      ensure_installed = { "templ" },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    ft = filetypes,
+    dependencies = {
+      {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+          {
+            "williamboman/mason.nvim",
+          },
+        },
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "templ" })
+        end,
+      },
+    },
+    opts = {
+      servers = {
+        templ = {
+          filetypes = filetypes,
+          settings = {
+            templ = {},
+          },
+        },
+      },
+      extends = {
+        -- extendee: templ
+        templ = {
+          -- extends: go.lua, html.lua
+          servers = {
+            gopls = {
+              filetypes = filetypes,
+              settings = {
+                gopls = {
+                  templateExtensions = filetypes, -- make sure this filetype is set in the buffer
+                },
+              },
+            },
+            html = {
+              filetypes = filetypes,
+              settings = {
+                html = {
+                  -- FIXME: disable formatting?
+                },
+              },
+            },
+            -- TODO: add htmx
+            -- htmx = {
+            --   filetypes = filetypes,
+            --   settings = {
+            --     htmx = {},
+            --   },
+            -- },
+          },
+        },
+      },
+    },
+  },
+}
