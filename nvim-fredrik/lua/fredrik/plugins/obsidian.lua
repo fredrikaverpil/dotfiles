baf5af27f6c50a49bc87d411d2e557ee8783eb9b
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-20 19:48:32 +0100

    feat: add blink compat to obsidian

--- nvim-fredrik/lua/fredrik/plugins/obsidian.lua
+++ nvim-fredrik/lua/fredrik/plugins/obsidian.lua
@@ -13,6 +13,40 @@ return {
     -- optional
     "nvim-telescope/telescope.nvim",
     "nvim-treesitter/nvim-treesitter",
+
+    { "saghen/blink.compat" },
+    {
+      "saghen/blink.cmp",
+      dependencies = {
+        { "epwalsh/obsidian.nvim" },
+      },
+      opts_extend = { "sources.completion.enabled_providers" },
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
+      opts = {
+        sources = {
+          default = {
+            "obsidian",
+            "obsidian_new",
+            "obsidian_tags",
+          },
+          providers = {
+            obsidian = {
+              name = "obsidian",
+              module = "blink.compat.source",
+            },
+            obsidian_new = {
+              name = "obsidian_new",
+              module = "blink.compat.source",
+            },
+            obsidian_tags = {
+              name = "obsidian_tags",
+              module = "blink.compat.source",
+            },
+          },
+        },
+      },
+    },
   },
   enabled = function()
     -- only enable on macOS for now, and if vault_path exists
