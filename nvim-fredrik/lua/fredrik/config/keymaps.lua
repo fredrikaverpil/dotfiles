ca1f5cdf250df91b8c47538b9d233f36e6b0bd37
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-21 11:46:39 +0100

    feat(nvim): show dialogue before opening lazygit

--- nvim-fredrik/lua/fredrik/config/keymaps.lua
+++ nvim-fredrik/lua/fredrik/config/keymaps.lua
@@ -463,7 +463,20 @@ function M.setup_snacks_keymaps()
     {
       "<leader>gg",
       function()
-        require("snacks").lazygit.open()
+        -- see https://oldbytes.space/@thelastpsion/113684780429048846
+        local unsaved_table = {}
+        for _, buf_id in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.api.nvim_get_option_value("modified", { buf = buf_id }) then
+            table.insert(unsaved_table, string.format("%3d", buf_id) .. ": " .. vim.api.nvim_buf_get_name(buf_id))
+          end
+        end
+        if
+          #unsaved_table == 0
+          or vim.fn.confirm("There are unsaved buffers:\n\n" .. table.concat(unsaved_table, "\n") .. "\n\nDo you still want to run lazygit?", "&Yes\n&No", 2)
+            == 1
+        then
+          require("snacks").lazygit.open()
+        end
       end,
       desc = "LazyGit",
     },
