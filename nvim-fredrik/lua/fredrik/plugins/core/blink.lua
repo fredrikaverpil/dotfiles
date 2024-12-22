10d9ce6ff4351aa09ec5cd5d2d4fd67aee2171cf
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-22 09:30:03 +0100

    fix(nvim): remove v1 api ref

--- nvim-fredrik/lua/fredrik/plugins/core/blink.lua
+++ nvim-fredrik/lua/fredrik/plugins/core/blink.lua
@@ -48,7 +48,6 @@ return {
     -- allows extending the enabled_providers array elsewhere in your config
     -- without having to redefine it
     opts_extend = {
-      "sources.completion.enabled_providers",
       "sources.default",
     },
   },
