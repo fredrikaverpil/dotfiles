4e4487ae65768587f760de0d293ba6183ab889e5
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-23 10:30:26 +0100

    fix(nvim): move markdown-toc to formatters

--- nvim-fredrik/lua/fredrik/plugins/lang/markdown.lua
+++ nvim-fredrik/lua/fredrik/plugins/lang/markdown.lua
@@ -18,7 +18,7 @@ return {
         "williamboman/mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "prettier", "mdformat" })
+          vim.list_extend(opts.ensure_installed, { "prettier", "mdformat", "markdown-toc" })
         end,
       },
     },
@@ -48,7 +48,7 @@ return {
         "williamboman/mason.nvim",
         opts = function(_, opts)
           opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "markdownlint", "markdown-toc" })
+          vim.list_extend(opts.ensure_installed, { "markdownlint" })
         end,
       },
     },
