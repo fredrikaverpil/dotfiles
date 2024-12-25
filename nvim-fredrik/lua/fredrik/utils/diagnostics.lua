ffa25be5cf083361ba86c1cfb194f0f0eea3de68
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-25 12:15:11 +0100

    refactor(nvim): simplify diagnostics

--- nvim-fredrik/lua/fredrik/utils/diagnostics.lua
+++ nvim-fredrik/lua/fredrik/utils/diagnostics.lua
@@ -1,5 +1,15 @@
 M = {}
 
+---@param diagnostic table
+local function prefix(diagnostic)
+  local icons = require("fredrik.utils.icons").icons.diagnostics
+  for d, icon in pairs(icons) do
+    if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+      return icon
+    end
+  end
+end
+
 function M.setup_diagnostics()
   local diagnostics = {
     underline = true,
@@ -7,10 +17,7 @@ function M.setup_diagnostics()
     virtual_text = {
       spacing = 4,
       source = "if_many",
-      prefix = "●",
-      -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-      -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-      -- prefix = "icons",
+      prefix = prefix,
     },
     severity_sort = true,
     signs = {
@@ -27,19 +34,6 @@ function M.setup_diagnostics()
   for name, icon in pairs(require("fredrik.utils.icons").icons.diagnostics) do
     name = "DiagnosticSign" .. name
     vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
-  end
-
-  local version = require("fredrik.utils.version")
-  if type(diagnostics.virtual_text) == "table" and diagnostics.virtual_text.prefix == "icons" then
-    diagnostics.virtual_text.prefix = version.is_neovim_0_10_0() == 0 and "●"
-        or function(diagnostic)
-          local icons = require("fredrik.utils.icons").icons.diagnostics
-          for d, icon in pairs(icons) do
-            if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
-              return icon
-            end
-          end
-        end
   end
 
   vim.diagnostic.config(vim.deepcopy(diagnostics))
