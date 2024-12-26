5576c0bc733d22e3976bb5cc3cf07e4bdca4e916
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-26 11:19:06 +0100

    feat(nvim): set larger notification history window

--- nvim-fredrik/lua/fredrik/plugins/core/snacks.lua
+++ nvim-fredrik/lua/fredrik/plugins/core/snacks.lua
@@ -3,21 +3,19 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+
+    ---@type snacks.Config
     opts = {
-      -- visuals
+      styles = {
+        notification_history = {
+          width = 0.9,
+          height = 0.9,
+        },
+      },
       notifier = { enabled = true, timeout = 2000 },
       statuscolumn = { enabled = true },
       indent = {
         enabled = true,
-        -- animate scopes. Enabled by default for Neovim >= 0.10
-        -- Works on older versions but has to trigger redraws during animation.
-        ---@class snacks.indent.animate: snacks.animate.Config
-        ---@field enabled? boolean
-        --- * out: animate outwards from the cursor
-        --- * up: animate upwards from the cursor
-        --- * down: animate downwards from the cursor
-        --- * up_down: animate up or down based on the cursor position
-        ---@field style? "out"|"up_down"|"down"|"up"
         animate = {
           enabled = vim.fn.has("nvim-0.10") == 1,
           style = "out",
