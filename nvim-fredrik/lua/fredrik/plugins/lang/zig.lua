8a7a1240cbba9dda9713a13f9c02b27dd91e9612
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-26 15:47:39 +0100

    feat(nvim): add zig

---
+++ nvim-fredrik/lua/fredrik/plugins/lang/zig.lua
@@ -0,0 +1,70 @@
+return {
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = true,
+    ft = { "zig" },
+    opts = {
+      ensure_installed = { "zig" },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    dependencies = {
+      {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+          {
+            "williamboman/mason.nvim",
+          },
+        },
+        opts = function(_, opts)
+          opts.ensure_installed = opts.ensure_installed or {}
+          vim.list_extend(opts.ensure_installed, { "zls" })
+        end,
+      },
+    },
+    opts = {
+      servers = {
+        zls = {
+          filetypes = { "zig" },
+          settings = {
+            zls = {},
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "nvim-neotest/neotest",
+    lazy = true,
+    ft = { "zig" },
+    dependencies = {
+      "lawrence-laz/neotest-zig",
+    },
+    opts = {
+      adapters = {
+        ["neotest-zig"] = {
+          dap = {
+            adapter = "lldb",
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "CRAG666/code_runner.nvim",
+    lazy = true,
+    opts = {
+      filetype = {
+        zig = {
+          "zig run",
+        },
+      },
+    },
+  },
+}
