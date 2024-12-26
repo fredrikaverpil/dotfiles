10f8d668534a0c75da30b9e4f1e776baa0f59cd6
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-12-26 11:03:26 +0100

    fix(nvim): use taplo LSP only

--- nvim-fredrik/lua/fredrik/plugins/lang/toml.lua
+++ nvim-fredrik/lua/fredrik/plugins/lang/toml.lua
@@ -1,26 +1,6 @@
 return {
 
   {
-    "stevearc/conform.nvim",
-    lazy = true,
-    ft = { "toml" },
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = function(_, opts)
-          opts.ensure_installed = opts.ensure_installed or {}
-          vim.list_extend(opts.ensure_installed, { "taplo" })
-        end,
-      },
-    },
-    opts = {
-      formatters_by_ft = {
-        toml = { "taplo" },
-      },
-    },
-  },
-
-  {
     "neovim/nvim-lspconfig",
     lazy = true,
     dependencies = {
@@ -41,11 +21,6 @@ return {
       servers = {
         taplo = {
           filetypes = { "toml" },
-          -- Disable formatting for taplo LSP, delegate to conform.nvim
-          on_attach = function(client, _)
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentRangeFormattingProvider = false
-          end,
           settings = {
             taplo = {},
           },
