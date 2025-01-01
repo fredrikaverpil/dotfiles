68692485d28f3d7bb5e004ca0eb0e8ab928ae365
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-01-01 22:38:30 +0100

    feat(nvim): use noice for search input

--- nvim-fredrik/lua/fredrik/plugins/core/noice.lua
+++ nvim-fredrik/lua/fredrik/plugins/core/noice.lua
@@ -44,7 +44,7 @@ return {
         },
         -- you can enable a preset for easier configuration
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
+          bottom_search = false, -- use a classic bottom cmdline for search
           command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
