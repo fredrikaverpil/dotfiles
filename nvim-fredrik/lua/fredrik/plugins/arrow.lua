a74c23af5ed6dad30b23e9d488efa50bdcdd2150
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-01-06 09:32:10 +0100

    feat(nvim): add 'm' keymap for buffer leader key

--- nvim-fredrik/lua/fredrik/plugins/arrow.lua
+++ nvim-fredrik/lua/fredrik/plugins/arrow.lua
@@ -6,6 +6,7 @@ return {
     opts = {
       show_icons = true,
       leader_key = ";", -- Recommended to be a single key
+      buffer_leader_key = "m",
       always_show_path = true,
     },
   },
