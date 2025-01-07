bc5e97371a26b7da3074fad7b03f7f90543ff60e
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-01-07 14:09:23 +0100

    fix(nvim): remove unused cmp code

--- nvim-fredrik/lua/fredrik/plugins/copilot.lua
+++ nvim-fredrik/lua/fredrik/plugins/copilot.lua
@@ -79,18 +79,6 @@ return {
       -- Make sure not to enable copilot in private projects
       require("fredrik.utils.private").toggle_copilot()
 
-      -- hide copilot suggestions when cmp menu is open
-      -- to prevent odd behavior/garbled up suggestions
-      local cmp_status_ok, cmp = pcall(require, "cmp")
-      if cmp_status_ok then
-        cmp.event:on("menu_opened", function()
-          vim.b.copilot_suggestion_hidden = true
-        end)
-
-        cmp.event:on("menu_closed", function()
-          vim.b.copilot_suggestion_hidden = false
-        end)
-      end
     end,
     keys = require("fredrik.config.keymaps").setup_copilot_keymaps(),
   },
