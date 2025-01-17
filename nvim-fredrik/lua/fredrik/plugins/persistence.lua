1ce8561c6b4957d896e12860a13258daeceef327
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-01-17 15:59:08 +0100

    feat(nvim): update sessionoptions

--- nvim-fredrik/lua/fredrik/plugins/persistence.lua
+++ nvim-fredrik/lua/fredrik/plugins/persistence.lua
@@ -29,7 +29,8 @@ return {
     "folke/persistence.nvim",
     event = "VimEnter",
     init = function()
-      vim.opt.sessionoptions = "buffers,curdir,help,tabpages,winsize,winpos,terminal,localoptions"
+      -- https://neovim.io/doc/user/options.html#'sessionoptions'
+      vim.opt.sessionoptions = "curdir,folds,help,winsize,winpos,localoptions"
     end,
     keys = require("fredrik.config.keymaps").setup_auto_session_keymaps(),
     config = function(_, opts)
