cff9e3f614f10d33de44a7febbbca1acd9c4761c
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-01-30 22:28:58 +0100

    chore(nvim): move into dap-go section

--- nvim-fredrik/lua/fredrik/plugins/lang/go.lua
+++ nvim-fredrik/lua/fredrik/plugins/lang/go.lua
@@ -316,11 +316,22 @@ return {
           dap_configurations = {
             {
               type = "go",
-              name = "Debug opened file's cmd/cli",
+              name = "Delve: debug opened file's cmd/cli",
               request = "launch",
-              cwd = "${fileDirname}", -- FIXME:  should work from  repo root
+              cwd = "${fileDirname}", -- FIXME: should work from repo root
               program = "./${relativeFileDirname}",
               args = {},
+            },
+            {
+              type = "go",
+              name = "Delve: debug test (manually enter test name)",
+              request = "launch",
+              mode = "test",
+              program = "./${relativeFileDirname}",
+              args = function()
+                local testname = vim.fn.input("Test name (^regexp$ ok): ")
+                return { "-test.run", testname }
+              end,
             },
           },
         },
@@ -330,22 +341,9 @@ return {
       },
     },
     opts = {
-      configurations = {
-        go = {
-          -- See require("dap-go") source for full dlv setup.
-          {
-            type = "go",
-            name = "Debug test (manually enter test name)",
-            request = "launch",
-            mode = "test",
-            program = "./${relativeFileDirname}",
-            args = function()
-              local testname = vim.fn.input("Test name (^regexp$ ok): ")
-              return { "-test.run", testname }
-            end,
-          },
-        },
-      },
+      -- configurations = {
+      --   go = {},
+      -- },
     },
   },
 
