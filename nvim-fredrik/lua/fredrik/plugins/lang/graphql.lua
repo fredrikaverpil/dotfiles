19404ffe24cd70ef32ba5eaf219c1ffd694bb056
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-01-31 08:28:56 +0100

    docs(nvim): add link to lsp

--- nvim-fredrik/lua/fredrik/plugins/lang/graphql.lua
+++ nvim-fredrik/lua/fredrik/plugins/lang/graphql.lua
@@ -22,6 +22,7 @@ return {
       servers = {
         ---@type vim.lsp.Config
         graphql = {
+          -- cli: https://github.com/graphql/graphiql/blob/main/packages/graphql-language-service-server/README.md
           -- lsp: https://www.npmjs.com/package/graphql-language-service-cli
           -- ref: https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/graphql.lua
           cmd = { "graphql-lsp", "server", "-m", "stream" },
