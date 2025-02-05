a439f23d3bfaa5774ff3c7dfbdcfa00f1ce0aadb
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-02-05 09:17:23 +0100

    fix(nvim): improve detection of sql injections into go

--- nvim-fredrik/after/queries/go/injections.scm
+++ nvim-fredrik/after/queries/go/injections.scm
@@ -9,12 +9,45 @@
 
 ; neovim 0.10
 ([
-  (interpreted_string_literal)
-  (raw_string_literal)
+  (interpreted_string_literal_content)
+  (raw_string_literal_content)
   ] @injection.content
  (#match? @injection.content "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
  (#offset! @injection.content 0 1 0 -1)
 (#set! injection.language "sql"))
+
+; a general query injection
+([
+   (interpreted_string_literal_content)
+   (raw_string_literal_content)
+ ] @sql
+ (#match? @sql "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
+ (#offset! @sql 0 1 0 -1))
+
+; ----------------------------------------------------------------
+; fallback keyword and comment based injection
+
+([
+  (interpreted_string_literal_content)
+  (raw_string_literal_content)
+ ] @sql
+ (#contains? @sql "-- sql" "--sql" "ADD CONSTRAINT" "ALTER TABLE" "ALTER COLUMN"
+                  "DATABASE" "FOREIGN KEY" "GROUP BY" "HAVING" "CREATE INDEX" "INSERT INTO"
+                  "NOT NULL" "PRIMARY KEY" "UPDATE SET" "TRUNCATE TABLE" "LEFT JOIN" "add constraint" "alter table" "alter column" "database" "foreign key" "group by" "having" "create index" "insert into"
+                  "not null" "primary key" "update set" "truncate table" "left join")
+ (#offset! @sql 0 1 0 -1))
+
+; nvim 0.10
+([
+  (interpreted_string_literal_content)
+  (raw_string_literal_content)
+ ] @injection.content
+ (#contains? @injection.content "-- sql" "--sql" "ADD CONSTRAINT" "ALTER TABLE" "ALTER COLUMN"
+                  "DATABASE" "FOREIGN KEY" "GROUP BY" "HAVING" "CREATE INDEX" "INSERT INTO"
+                  "NOT NULL" "PRIMARY KEY" "UPDATE SET" "TRUNCATE TABLE" "LEFT JOIN" "add constraint" "alter table" "alter column" "database" "foreign key" "group by" "having" "create index" "insert into"
+                  "not null" "primary key" "update set" "truncate table" "left join")
+ (#offset! @injection.content 0 1 0 -1)
+ (#set! injection.language "sql"))
 
 
 
