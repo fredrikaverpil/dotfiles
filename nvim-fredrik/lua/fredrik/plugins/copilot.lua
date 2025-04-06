eebef034b596a694dfb04fb1bb044a834313532e
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-04-06 11:29:58 +0200

    chore(nvim): cleanup

--- nvim-fredrik/lua/fredrik/plugins/copilot.lua
+++ nvim-fredrik/lua/fredrik/plugins/copilot.lua
@@ -15,12 +15,6 @@ return {
             return icon
           end
 
-          -- local function fgcolor(name)
-          --   local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name, link = false })
-          --   local fg = hl and (hl.fg or hl.foreground)
-          --   return fg and { fg = string.format("#%06x", fg) } or nil
-          -- end
-
           local colors = {
             -- statuses NOT supported by copilot.lua (see `require("copilot").api.status`)
             ["Offline"] = require("fredrik.utils.colors").fgcolor("Comment"),
