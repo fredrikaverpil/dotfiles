e8d4aeaccd8f5f91b9ae840f7bbc27eea1c4e55b
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-05-01 21:12:03 +0200

    chore(nvim): whitespace

--- nvim-fredrik/lua/fredrik/config/keymaps.lua
+++ nvim-fredrik/lua/fredrik/config/keymaps.lua
@@ -638,6 +638,7 @@ function M.setup_gitsigns_keymaps(bufnr)
     local default_branch = require("fredrik.utils.git").get_default_branch()
     vim.cmd("Gitsigns change_base " .. default_branch)
   end, { buffer = bufnr, silent = false, noremap = true, desc = "change [b]ase to default branch" })
+
   vim.keymap.set(
     { "n", "v" },
     "<leader>ghs",
