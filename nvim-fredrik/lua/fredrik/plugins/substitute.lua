036478979741c65998d01d64c875b8b19adaaefd
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-07-05 18:25:39 +0200

    feat(nvim): integrate with yanky

--- nvim-fredrik/lua/fredrik/plugins/substitute.lua
+++ nvim-fredrik/lua/fredrik/plugins/substitute.lua
@@ -1,7 +1,14 @@
 return {
   {
     "gbprod/substitute.nvim",
+    dependencies = {
+      "gbprod/yanky.nvim",
+    },
     opts = {},
+    config = function(_, opts)
+      opts.on_substitute = require("yanky.integration").substitute()
+      require("substitute").setup(opts)
+    end,
     keys = require("fredrik.config.keymaps").setup_substitute_keymaps(),
   },
 }
