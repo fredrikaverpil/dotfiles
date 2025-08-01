2aca1832fce55e6209a07fb70d1451b12a4709ee
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-08-01 11:02:36 +0200

    fix(restic): gzip path

--- nix/hosts/rpi5-homelab/restic.nix
+++ nix/hosts/rpi5-homelab/restic.nix
@@ -68,7 +68,7 @@
   environment.etc."homelab/scripts/backup-immich.sh" = {
     text = ''
       #!${pkgs.bash}/bin/bash
-      ${builtins.replaceStrings ["docker"] ["${pkgs.docker}/bin/docker"] (builtins.readFile ./scripts/backup-immich.sh)}
+      ${builtins.replaceStrings ["docker" "gzip"] ["${pkgs.docker}/bin/docker" "${pkgs.gzip}/bin/gzip"] (builtins.readFile ./scripts/backup-immich.sh)}
     '';
     mode = "0755";
   };
