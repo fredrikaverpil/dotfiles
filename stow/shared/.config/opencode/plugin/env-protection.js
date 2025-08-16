42c51c667ae5c4816d4287fb2ee15c5a9ff95c28
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-08-16 17:39:13 +0200

    feat(opencode): add plugin for .env

---
+++ stow/shared/.config/opencode/plugin/env-protection.js
@@ -0,0 +1,9 @@
+export const EnvProtection = async ({ client, $ }) => {
+  return {
+    "tool.execute.before": async (input, output) => {
+      if (input.tool === "read" && output.args.filePath.includes(".env")) {
+        throw new Error("Do not read .env files")
+      }
+    }
+  }
+}
