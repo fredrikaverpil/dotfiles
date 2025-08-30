1670212e17a0eb1f4f78eb9f7c91c361a20be780
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-08-30 11:46:46 +0200

    chore(nvim): do not pre-select for copilot chat

--- nvim-fredrik/lua/fredrik/plugins/copilot_chat.lua
+++ nvim-fredrik/lua/fredrik/plugins/copilot_chat.lua
@@ -10,18 +10,17 @@ return {
       { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim" },
     },
-    ---@type CopilotChat.config
     opts = {
-      debug = false, -- Enable debugging
       selection = function(source)
-        if require("fredrik.utils.private").is_code_public() then
-          local select = require("CopilotChat.select")
-          return select.visual(source) or select.buffer(source)
-        else
-          return nil
-        end
+        -- if require("fredrik.utils.private").is_code_public() then
+        --   local select = require("CopilotChat.select")
+        --   return select.visual(source) or select.buffer(source)
+        -- else
+        --   return nil
+        -- end
+
+        return nil -- just disable selection
       end,
-      -- model = "claude-3.7-sonnet", -- NOTE: requires paid subscription
     },
     keys = function()
       return require("fredrik.config.keymaps").setup_copilot_chat_keymaps()
