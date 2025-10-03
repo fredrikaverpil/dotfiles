0aa73f9c2715545c3c5493b0bc65578fb2e9c641
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-10-03 08:00:21 +0200

    fix(nvim): more robust buffer deletion prior to saving state
    Prior to this change, Neovim would crash with an error about not being
    able to delete certain buffers.

--- nvim-fredrik/lua/fredrik/plugins/persistence.lua
+++ nvim-fredrik/lua/fredrik/plugins/persistence.lua
@@ -4,8 +4,12 @@ local function delete_hidden_buffers()
     visible[vim.api.nvim_win_get_buf(win)] = true
   end
   for _, buf in pairs(vim.api.nvim_list_bufs()) do
-    if not visible[buf] then
-      vim.api.nvim_buf_delete(buf, {})
+    if not visible[buf] and vim.api.nvim_buf_is_valid(buf) then
+      local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
+      -- Skip special buffers (nofile, terminal, prompt, etc.)
+      if buftype == "" then
+        pcall(vim.api.nvim_buf_delete, buf, {})
+      end
     end
   end
 end
