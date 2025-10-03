2264b2a290f17ca72f0126f2f22744c7412f3502
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-10-03 07:13:22 +0200

    feat(nvim): add copilot-lsp for NES

--- nvim-fredrik/lua/fredrik/plugins/copilot.lua
+++ nvim-fredrik/lua/fredrik/plugins/copilot.lua
@@ -7,6 +7,46 @@ return {
     enabled = true,
     dependencies = {
       {
+        "copilotlsp-nvim/copilot-lsp",
+        enabled = true,
+        dependencies = {
+          {
+            "mason-org/mason.nvim",
+            opts = function(_, opts)
+              opts.ensure_installed = opts.ensure_installed or {}
+              vim.list_extend(opts.ensure_installed, { "copilot-language-server" })
+            end,
+          },
+        },
+        init = function()
+          vim.g.copilot_nes_debounce = 500
+          vim.lsp.enable("copilot_ls")
+          vim.keymap.set("n", "<tab>", function()
+            local bufnr = vim.api.nvim_get_current_buf()
+            local state = vim.b[bufnr].nes_state
+            if state then
+              -- Try to jump to the start of the suggestion edit.
+              -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+              local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+                or (
+                  require("copilot-lsp.nes").apply_pending_nes()
+                  and require("copilot-lsp.nes").walk_cursor_end_edit()
+                )
+              return nil
+            else
+              -- Resolving the terminal's inability to distinguish between `TAB` and `<C-i>` in normal mode
+              return "<C-i>"
+            end
+          end, { desc = "Accept Copilot NES suggestion", expr = true })
+          -- Clear copilot suggestion with Esc if visible, otherwise preserve default Esc behavior
+          vim.keymap.set("n", "<esc>", function()
+            if not require("copilot-lsp.nes").clear() then
+              -- fallback to other functionality
+            end
+          end, { desc = "Clear Copilot suggestion or fallback" })
+        end,
+      },
+      {
         "nvim-lualine/lualine.nvim",
         event = "VeryLazy",
         opts = function(_, opts)
