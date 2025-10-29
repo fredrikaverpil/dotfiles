559a48809b418340ec087d8b804793c597fdab1d
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-10-29 07:21:42 +0100

    chore(nvim): remove redundant

--- nvim-fredrik/lua/fredrik/plugins/tiny_inline_diagnostics.lua
+++ nvim-fredrik/lua/fredrik/plugins/tiny_inline_diagnostics.lua
@@ -17,13 +17,6 @@ return {
     },
     config = function(_, opts)
       require("tiny-inline-diagnostic").setup(opts)
-
-      -- override the diagnostics settings (see diagnostics.lua)
-      local diagnostics_opts = vim.diagnostic.config()
-      diagnostics_opts.enable = false
-      diagnostics_opts.virtual_lines = false
-      diagnostics_opts.virtual_text = false
-      vim.diagnostic.config(diagnostics_opts)
     end,
   },
 }
