20d0e69b012ab301b2200770cfefa59e6ffd12ae
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-11-28 12:24:51 +0100

    feat(nvim): limit indent noise

--- nvim-fredrik/lua/fredrik/plugins/blink_indent.lua
+++ nvim-fredrik/lua/fredrik/plugins/blink_indent.lua
@@ -4,6 +4,12 @@ return {
     --- @module 'blink.indent'
     --- @type blink.indent.Config
     opts = {
+      blocked = {
+        filetypes = { include_defaults = true, "snacks_picker_preview" },
+      },
+      static = {
+        enabled = false,
+      },
       scope = {
         highlights = { "BlinkIndentScope" }, -- avoid multiple colors
       },
