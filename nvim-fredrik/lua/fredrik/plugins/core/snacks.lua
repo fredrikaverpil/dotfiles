4652384d5f379ca602c69e9a3bcb4843b6b32a07
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-12-01 17:28:09 +0100

    chore(nvim): show lsp progress

--- nvim-fredrik/lua/fredrik/plugins/core/snacks.lua
+++ nvim-fredrik/lua/fredrik/plugins/core/snacks.lua
@@ -137,6 +137,24 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("snacks").setup(opts)
+
+      vim.api.nvim_create_autocmd("LspProgress", {
+        ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+        callback = function(ev)
+          local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+          vim.notify(vim.lsp.status(), "info", {
+            id = "lsp_progress",
+            title = "LSP Progress",
+            opts = function(notif)
+              notif.icon = ev.data.params.value.kind == "end" and " "
+                or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+            end,
+          })
+        end,
+      })
+    end,
     keys = function()
       ---@type table[table]
       local snacks_keymaps = require("fredrik.config.keymaps").setup_snacks_keymaps()
