dbb132b2c73e16ef94fc2a0e340a5a2bae9a6663
Fredrik Averpil <fredrik.averpil@gmail.com>
2025-12-18 08:43:36 +0100

    feat(nvim): set autocmd for tab/shift

--- nvim-fredrik/lua/fredrik/plugins/lang/typescript.lua
+++ nvim-fredrik/lua/fredrik/plugins/lang/typescript.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
 return {
   {
     "stevearc/conform.nvim",
