5ed920983abd36fa905fe49a9003fa72dc73e8d7
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-01-07 16:43:11 +0100

    refactor(nvim): move lsp progress indicator

--- nvim-fredrik/lua/fredrik/plugins/core/lsp.lua
+++ nvim-fredrik/lua/fredrik/plugins/core/lsp.lua
@@ -136,6 +136,22 @@ local function register_lspattach_autocmd()
   })
 end
 
+-- LSP progress spinner
+vim.api.nvim_create_autocmd("LspProgress", {
+  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+  callback = function(ev)
+    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+    vim.notify(vim.lsp.status(), "info", {
+      id = "lsp_progress",
+      title = "LSP Progress",
+      opts = function(notif)
+        notif.icon = ev.data.params.value.kind == "end" and " "
+          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+      end,
+    })
+  end,
+})
+
 return {
   {
     "virtual-lsp-config",
