d68e421ea35a61b7698dd4f8c8b05c8ebfa7ca52
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-01-12 20:17:45 +0100

    chore(nvim): use mdformat

--- nvim-fredrik/lua/fredrik/plugins/lang/markdown.lua
+++ nvim-fredrik/lua/fredrik/plugins/lang/markdown.lua
@@ -22,7 +22,7 @@ return {
     },
     opts = {
       formatters_by_ft = {
-        markdown = { "prettier" },
+        markdown = { "mdformat" }, -- currently, mdformat is used (but prettier is configured too below)
       },
       formatters = {
         prettier = {
