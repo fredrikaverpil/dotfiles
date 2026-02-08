fad43c465c5a3318d2a8d6a2b672f2f759b69a5f
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-02-08 09:01:31 +0100

    chore(nvim): re-enable blink-go-import.nvim

--- nvim-fredrik/lua/fredrik/plugins/lang/go.lua
+++ nvim-fredrik/lua/fredrik/plugins/lang/go.lua
@@ -492,33 +492,32 @@ return {
     },
   },
 
-  -- FIX: https://github.com/edte/blink-go-import.nvim/issues/4
-  -- {
-  --   "saghen/blink.cmp",
-  --   dependencies = {
-  --     "edte/blink-go-import.nvim",
-  --     ft = "go",
-  --     config = function()
-  --       require("blink-go-import").setup()
-  --     end,
-  --   },
-  --   opts = {
-  --     sources = {
-  --       default = {
-  --         "go_pkgs",
-  --       },
-  --       providers = {
-  --         go_pkgs = {
-  --           module = "blink-go-import",
-  --           name = "Import",
-  --         },
-  --       },
-  --     },
-  --   },
-  --   opts_extend = {
-  --     "sources.default",
-  --   },
-  -- },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "edte/blink-go-import.nvim",
+      ft = "go",
+      config = function()
+        require("blink-go-import").setup()
+      end,
+    },
+    opts = {
+      sources = {
+        default = {
+          "go_pkgs",
+        },
+        providers = {
+          go_pkgs = {
+            module = "blink-go-import",
+            name = "Import",
+          },
+        },
+      },
+    },
+    opts_extend = {
+      "sources.default",
+    },
+  },
 
   {
     "saghen/blink.cmp",
