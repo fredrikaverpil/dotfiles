eb1c1f86b12ad57337b1bab2dd37797869383a21
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-02-23 21:56:26 +0100

    chore(nvim): disable

--- nvim-fredrik/lua/fredrik/plugins/colorizer.lua
+++ nvim-fredrik/lua/fredrik/plugins/colorizer.lua
@@ -2,6 +2,7 @@ return {
   {
     "catgoose/nvim-colorizer.lua",
     event = "BufReadPre",
+    enabled = false, -- adds in colors into e.g. Claude Code CLI and messes up stuff
     opts = {},
   },
 }
