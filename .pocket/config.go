2995cc93e9ae459f2e49fc0a8c77444ce662b01e
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-02-23 21:57:01 +0100

    ci: add pocket

---
+++ .pocket/config.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/fredrikaverpil/pocket/pk"
+	"github.com/fredrikaverpil/pocket/tasks/github"
+	"github.com/fredrikaverpil/pocket/tasks/golang"
+	"github.com/fredrikaverpil/pocket/tasks/lua"
+	"github.com/fredrikaverpil/pocket/tasks/markdown"
+)
+
+// Config is the Pocket configuration for this project.
+var Config = &pk.Config{
+	Auto: pk.Parallel(
+		golang.Tasks(),
+		lua.Tasks(),
+		markdown.Format,
+		github.Tasks(),
+	),
+
+	// Plan configuration: shims, directories, and CI settings.
+	Plan: &pk.PlanConfig{
+		Shims: pk.AllShimsConfig(),
+	},
+}
---
+++ .pocket/go.mod
@@ -0,0 +1,11 @@
+module pocket
+
+go 1.26.0
+
+require github.com/fredrikaverpil/pocket v0.0.0-20260223064301-38a7dffe96e7
+
+require (
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/term v0.39.0 // indirect
+)
