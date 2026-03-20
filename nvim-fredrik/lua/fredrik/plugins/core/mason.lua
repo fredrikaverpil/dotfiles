71c87b4ad88eb7980b852724359f0e4a266281b2
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-03-20 06:14:01 +0100

    feat(nvim): add custom "pip_extra_packages" for mason

--- nvim-fredrik/lua/fredrik/plugins/core/mason.lua
+++ nvim-fredrik/lua/fredrik/plugins/core/mason.lua
@@ -31,17 +31,32 @@ return {
       PATH = "append", -- picks tooling from local environment first
     },
     config = function(_, opts)
+      local pip_extra_packages = opts.pip_extra_packages or {}
+      opts.pip_extra_packages = nil -- don't pass custom key to mason.setup
       require("mason").setup(opts)
 
-      -- handle opts.ensure_installed
       local registry = require("mason-registry")
+
+      -- handle opts.ensure_installed and pip_extra_packages after registry refresh
       registry.refresh(function()
+        -- Install extra pip packages into Mason pypi venvs after (re)install.
+        local InstallLocation = require("mason-core.installer.InstallLocation")
+        for pkg_name, extra_pkgs in pairs(pip_extra_packages) do
+          local ok, pkg = pcall(registry.get_package, pkg_name)
+          if ok then
+            pkg:on("install:success", function()
+              local install_path = InstallLocation.global():package(pkg_name)
+              local cmd = vim.list_extend({ install_path .. "/venv/bin/pip", "install" }, extra_pkgs)
+              vim.fn.jobstart(cmd, { detach = true })
+            end)
+          end
+        end
+
         if opts.ensure_installed == nil then
           return
         end
 
         for _, pkg_name in ipairs(opts.ensure_installed) do
-          -- print("loading " .. pkg_name)
           local pkg = registry.get_package(pkg_name)
           if not pkg:is_installed() then
             pkg:install()
