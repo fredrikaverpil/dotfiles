d9d1abfeba64c59f76ff72ec384b80b081de4515
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-04-02 08:07:31 +0200

    fix(nvim): fix duplicate field

--- nvim-fredrik/lua/fredrik/utils/toggle.lua
+++ nvim-fredrik/lua/fredrik/utils/toggle.lua
@@ -5,17 +5,8 @@ local orig_fmt_func = vim.lsp.handlers["textDocument/formatting"]
 function M.toggle_formatting()
   vim.g.auto_format = not vim.g.auto_format -- reverse the value
 
-  if vim.g.auto_format then
-    vim.lsp.handlers["textDocument/formatting"] = orig_fmt_func
-  else
-    vim.lsp.handlers["textDocument/formatting"] = function() end
-  end
-
-  if vim.g.auto_format then
-    vim.notify("Auto-formatting enabled", vim.log.levels.INFO)
-  else
-    vim.notify("Auto-formatting disabled", vim.log.levels.INFO)
-  end
+  vim.lsp.handlers["textDocument/formatting"] = vim.g.auto_format and orig_fmt_func or function() end
+  vim.notify(vim.g.auto_format and "Auto-formatting enabled" or "Auto-formatting disabled", vim.log.levels.INFO)
 end
 
 function M.toggle_inlay_hints()
