57fb833a51f92d8dd0e8eec9ed26e651596b0b7e
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-04-07 23:21:01 +0200

    chore(nvim): fix warnings, apply formatting

--- nvim-fredrik/lua/fredrik/utils/colors.lua
+++ nvim-fredrik/lua/fredrik/utils/colors.lua
@@ -4,7 +4,10 @@ local M = {}
 ---@return number, number, number
 local function hex_to_rgb(hex)
   hex = tostring(hex):gsub("#", "")
-  return tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6))
+  local r = tonumber("0x" .. hex:sub(1, 2)) --[[@as number]]
+  local g = tonumber("0x" .. hex:sub(3, 4)) --[[@as number]]
+  local b = tonumber("0x" .. hex:sub(5, 6)) --[[@as number]]
+  return r, g, b
 end
 
 ---@param r number
@@ -29,9 +32,12 @@ local function rgb_to_hsl(r, g, b)
   else
     local d = max - min
     s = l > 0.5 and d / (2 - max - min) or d / (max + min)
-    if max == r then h = (g - b) / d + (g < b and 6 or 0)
-    elseif max == g then h = (b - r) / d + 2
-    elseif max == b then h = (r - g) / d + 4
+    if max == r then
+      h = (g - b) / d + (g < b and 6 or 0)
+    elseif max == g then
+      h = (b - r) / d + 2
+    elseif max == b then
+      h = (r - g) / d + 4
     end
     h = h / 6
   end
@@ -49,18 +55,28 @@ local function hsl_to_rgb(h, s, l)
     r, g, b = l, l, l
   else
     local function hue2rgb(p, q, t)
-      if t < 0 then t = t + 1 end
-      if t > 1 then t = t - 1 end
-      if t < 1/6 then return p + (q - p) * 6 * t end
-      if t < 1/2 then return q end
-      if t < 2/3 then return p + (q - p) * (2/3 - t) * 6 end
+      if t < 0 then
+        t = t + 1
+      end
+      if t > 1 then
+        t = t - 1
+      end
+      if t < 1 / 6 then
+        return p + (q - p) * 6 * t
+      end
+      if t < 1 / 2 then
+        return q
+      end
+      if t < 2 / 3 then
+        return p + (q - p) * (2 / 3 - t) * 6
+      end
       return p
     end
     local q = l < 0.5 and l * (1 + s) or l + s - l * s
     local p = 2 * l - q
-    r = hue2rgb(p, q, h + 1/3)
+    r = hue2rgb(p, q, h + 1 / 3)
     g = hue2rgb(p, q, h)
-    b = hue2rgb(p, q, h - 1/3)
+    b = hue2rgb(p, q, h - 1 / 3)
   end
   return r * 255, g * 255, b * 255
 end
