9735bf4a7c1ea7224f93436b017e4ce875741e83
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-04-11 17:43:47 +0200

    fix(nvim): clear cursorword

--- nvim-native/plugin/cursorword.lua
+++ nvim-native/plugin/cursorword.lua
@@ -5,8 +5,14 @@ local group = vim.api.nvim_create_augroup("Cursorword", { clear = true })
 vim.api.nvim_create_autocmd("CursorHold", {
   group = group,
   callback = function()
+    -- Clear any previous match to prevent accumulation
+    local prev_id = vim.w.minicursorword_match_id
+    if prev_id then
+      pcall(vim.fn.matchdelete, prev_id)
+    end
     local word = vim.fn.expand("<cword>")
     if word == "" or #word < 2 or not word:match("^[%w_]+$") then
+      vim.w.minicursorword_match_id = nil
       return
     end
     vim.w.minicursorword_match_id = vim.fn.matchadd("Cursorword", "\\<" .. vim.fn.escape(word, "\\") .. "\\>", -1)
