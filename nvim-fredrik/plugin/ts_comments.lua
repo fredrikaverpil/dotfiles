8040006cd2551e6820a24dc4b34825d6baff0aae
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-04-12 14:38:13 +0200

    chore(nvim): wrap in ts check

--- nvim-fredrik/plugin/ts_comments.lua
+++ nvim-fredrik/plugin/ts_comments.lua
@@ -1,7 +1,9 @@
 require("lazyload").on_vim_enter(function()
-  vim.pack.add({
-    { src = "https://github.com/folke/ts-comments.nvim" },
-  })
+  if Config.use_nvim_treesitter then
+    vim.pack.add({
+      { src = "https://github.com/folke/ts-comments.nvim" },
+    })
 
-  require("ts-comments").setup()
+    require("ts-comments").setup()
+  end
 end)
