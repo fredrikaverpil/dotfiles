640fb8b29124579bf46820889d0eb7661099a7ef
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-04-12 16:58:40 +0200

    chore(nvim): consolidate diagnostics settings

--- nvim-fredrik/plugin/diagnostics.lua
+++ nvim-fredrik/plugin/diagnostics.lua
@@ -2,7 +2,21 @@ require("lazyload").on_vim_enter(function()
   local icons = require("icons").diagnostics
 
   vim.diagnostic.config({
+    enable = true,
+
+    virtual_lines = false,
+    -- virtual_lines = {
+    --   -- Only show virtual line diagnostics for the current cursor line
+    --   current_line = false,
+    -- },
+
+    -- NOTE: disabled due to using the tiny-inline-diagnostic.nvim plugin
     virtual_text = false,
+    -- virtual_text = function(_, _)
+    --   ---@class vim.diagnostic.Opts.VirtualText
+    --   return { spacing = 4, source = "if_many", prefix = prefix }
+    -- end,
+
     underline = true,
     update_in_insert = false,
     severity_sort = true,
