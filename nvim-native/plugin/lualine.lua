4dfa26994805412e918d3edbe21f570da60c2b74
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-04-12 13:52:25 +0200

    chore(nvim): add todo

--- nvim-native/plugin/lualine.lua
+++ nvim-native/plugin/lualine.lua
@@ -1,45 +1,49 @@
-require("lazyload").on_vim_enter(function()
-  vim.pack.add({
-    { src = "https://github.com/nvim-lualine/lualine.nvim" },
-  })
+require("lazyload").on_vim_enter(
+  function()
+    vim.pack.add({
+      { src = "https://github.com/nvim-lualine/lualine.nvim" },
+    })
 
-  local function folder()
-    local cwd = vim.fn.getcwd()
-    return cwd:match("([^/]+)$")
-  end
+    local function folder()
+      local cwd = vim.fn.getcwd()
+      return cwd:match("([^/]+)$")
+    end
 
-  require("lualine").setup({
-    options = {
-      theme = "auto",
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      globalstatus = true,
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch", "diagnostics" },
-      lualine_c = {
-        { folder, color = { gui = "bold" }, separator = "/", padding = { left = 1, right = 0 } },
-        { "filename", path = 1, padding = { left = 0, right = 1 } },
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        globalstatus = true,
       },
-      lualine_x = {
-        {
-          function()
-            return require("dap").status()
-          end,
-          cond = function()
-            return package.loaded["dap"] and require("dap").status() ~= ""
-          end,
-          icon = "",
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diagnostics" },
+        lualine_c = {
+          { folder, color = { gui = "bold" }, separator = "/", padding = { left = 1, right = 0 } },
+          { "filename", path = 1, padding = { left = 0, right = 1 } },
         },
-        "encoding",
-        "filetype",
+        lualine_x = {
+          {
+            function()
+              return require("dap").status()
+            end,
+            cond = function()
+              return package.loaded["dap"] and require("dap").status() ~= ""
+            end,
+            icon = "",
+          },
+          "encoding",
+          "filetype",
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
-    },
-    extensions = { "man", "mason", "quickfix" },
-  })
+      extensions = { "man", "mason", "quickfix" },
+    })
 
-  vim.opt.showmode = false
-end, { sync = true })
+    vim.opt.showmode = false
+  end,
+  -- TODO: remove sync behavior, hide lualine until after leaving dashboard
+  { sync = true }
+)
