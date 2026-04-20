5f841ddaf94c01f3edebeca16124f1f1bc6e6b34
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-04-20 18:37:49 +0200

    chore(nvim): do not wrap in nvim-treesitter guard

--- nvim-fredrik/plugin/ts_comments.lua
+++ nvim-fredrik/plugin/ts_comments.lua
@@ -1,9 +1,7 @@
-require("lazyload").on_vim_enter(function()
-  if Config.use_treesitter_parser then
-    vim.pack.add({
-      { src = "https://github.com/folke/ts-comments.nvim" },
-    })
-
-    require("ts-comments").setup()
-  end
-end)
+-- require("lazyload").on_vim_enter(function()
+--   vim.pack.add({
+--     { src = "https://github.com/folke/ts-comments.nvim" },
+--   })
+--
+--   require("ts-comments").setup()
+-- end)
