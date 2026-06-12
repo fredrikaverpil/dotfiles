f6be1d5e2332527a0ae895271250179690f2961e
Fredrik Averpil <fredrik.averpil@gmail.com>
2026-06-12 08:18:27 +0200

    chore(nvim): remove folke/ts_comments

--- nvim-fredrik/plugin/ts_comments.lua
+++
@@ -1,7 +0,0 @@
--- require("lazyload").on_vim_enter(function()
---   vim.pack.add({
---     { src = "https://github.com/folke/ts-comments.nvim" },
---   })
---
---   require("ts-comments").setup()
--- end)
