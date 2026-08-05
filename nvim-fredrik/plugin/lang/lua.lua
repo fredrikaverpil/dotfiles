989d18af2191765e870099b84e8d6a756d2a2928
Fredrik Averpil <fredrik.averpil@proton.me>
2026-08-05 16:55:43 +0200

    fix(nvim): typo

--- nvim-fredrik/plugin/lang/lua.lua
+++ nvim-fredrik/plugin/lang/lua.lua
@@ -9,7 +9,7 @@ require("lazyload").on_vim_enter(function()
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       { path = "snacks.nvim", words = { "Snacks" } },
       "neotest",
-      "plenary",
+      "plenary.nvim",
     },
   })
 end)
