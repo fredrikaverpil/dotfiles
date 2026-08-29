42b8722323a6156abc7b8bc061757223ce9600b3
Fredrik Averpil <fredrik.averpil@proton.me>
2026-08-29 19:48:17 +0200

    feat(nix): add host LLM agent lists

--- nix/hosts/rpi5-homelab/users/fredrik.nix
+++ nix/hosts/rpi5-homelab/users/fredrik.nix
@@ -17,6 +17,7 @@ in
 
   packageTools.npmPackages = [ ];
   packageTools.uvTools = [ ];
+  packageTools.llmAgents = [ ];
 
   home.packages = with pkgs; [ ];
 
--- nix/hosts/wily-vm/users/fredrik.nix
+++ nix/hosts/wily-vm/users/fredrik.nix
@@ -5,4 +5,6 @@
   ];
 
   home.stateVersion = "26.05";
+
+  packageTools.llmAgents = [ ];
 }
