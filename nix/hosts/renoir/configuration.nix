2c1d1ca03a13f5eaf06f2a461bbb248a45fb6180
Fredrik Averpil <fredrik.averpil@proton.me>
2026-09-12 21:23:22 +0200

    chore(nix): bump inputs, handle ente

--- nix/hosts/renoir/configuration.nix
+++ nix/hosts/renoir/configuration.nix
@@ -7,6 +7,8 @@
   networking.hostName = "renoir";
   nixpkgs.hostPlatform = "x86_64-linux";
   nixpkgs.config.allowUnfree = true;
+  # ente-desktop pins EOL electron; drop once nixpkgs bumps it.
+  nixpkgs.config.permittedInsecurePackages = [ "electron-41.10.6" ];
 
   time.timeZone = "Europe/Stockholm";
 
